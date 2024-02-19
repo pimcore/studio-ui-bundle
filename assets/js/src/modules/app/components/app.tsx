62802e95084f0f9c722e26d0a7000d9fcc90fd9b
vin0401 <marvin.bertram@pimcore.com>
2024-02-19 16:08:03 +0100

    Add ant design app wrapper

--- assets/js/src/modules/app/components/app.tsx
+++ assets/js/src/modules/app/components/app.tsx
@@ -1,13 +1,16 @@
 import React, { StrictMode } from 'react'
 import { GlobalProvider } from './global-provider'
 import { BaseLayout } from '@Pimcore/modules/base-layout/components/base-layout'
+import { App as AntApp } from 'antd'
 
 export const App = (): React.JSX.Element => {
   return (
     <>
       <StrictMode>
         <GlobalProvider>
-          <BaseLayout />
+          <AntApp>
+            <BaseLayout />
+          </AntApp>
         </GlobalProvider>
       </StrictMode>
     </>
