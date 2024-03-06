c6a052c27b7f9310b90a71ebe6dcf65749867cea
Corepex <Corepex@users.noreply.github.com>
2024-03-06 10:19:02 +0000

    Apply eslint-fixer changes

--- assets/js/src/components/modal/modal.tsx
+++ assets/js/src/components/modal/modal.tsx
@@ -1,20 +1,20 @@
-import {Modal as AntModal, ModalProps as AntModalProps} from 'antd';
-import React from "react";
-import {useStyle} from "@Pimcore/components/modal/modal.styles";
-import useModal from 'antd/es/modal/useModal';
+import { Modal as AntModal, type ModalProps as AntModalProps } from 'antd'
+import React from 'react'
+import { useStyle } from '@Pimcore/components/modal/modal.styles'
+import type useModal from 'antd/es/modal/useModal'
 
 export interface ModalProps extends AntModalProps {
-    icon?: React.JSX.Element;
-    footer?: React.JSX.Element;
-    useModal?: typeof useModal;
-    children: React.ReactNode;
+  icon?: React.JSX.Element
+  footer?: React.JSX.Element
+  useModal?: typeof useModal
+  children: React.ReactNode
 }
 
 export const Modal = (props: ModalProps): React.JSX.Element => {
-    const { styles } = useStyle();
-    const {children} = props;
+  const { styles } = useStyle()
+  const { children } = props
 
-    return (
+  return (
         <AntModal
             title={(
                 <>
@@ -30,5 +30,5 @@ export const Modal = (props: ModalProps): React.JSX.Element => {
         >
             {children}
         </AntModal>
-    )
+  )
 }
--- assets/js/src/components/modal/useModal/index.tsx
+++ assets/js/src/components/modal/useModal/index.tsx
@@ -1,55 +1,55 @@
-import React, {useState} from "react";
-import {Modal, ModalProps} from "@Pimcore/components/modal/modal";
-import {Icon} from "@Pimcore/components/icon/icon";
+import React, { useState } from 'react'
+import { Modal, type ModalProps } from '@Pimcore/components/modal/modal'
+import { Icon } from '@Pimcore/components/icon/icon'
 
 interface useModalReturnType {
-    renderModal: (props: ModalProps) => React.JSX.Element;
-    showModal: () => void;
-    handleOk: () => void;
-    handleCancel: () => void;
+  renderModal: (props: ModalProps) => React.JSX.Element
+  showModal: () => void
+  handleOk: () => void
+  handleCancel: () => void
 }
 
-export const useModal = (config = {type: "default"}): useModalReturnType => {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+export const useModal = (config = { type: 'default' }): useModalReturnType => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
-    const showModal = (): void => {
-        setIsModalOpen(true);
-    };
+  const showModal = (): void => {
+    setIsModalOpen(true)
+  }
 
-    const handleOk = (): void => {
-        setIsModalOpen(false);
-    };
+  const handleOk = (): void => {
+    setIsModalOpen(false)
+  }
 
-    const handleCancel = (): void => {
-        setIsModalOpen(false);
-    };
+  const handleCancel = (): void => {
+    setIsModalOpen(false)
+  }
 
-    function getModalComponent(type: string): typeof Modal {
-        let component = Modal;
+  function getModalComponent (type: string): typeof Modal {
+    let component = Modal
 
-        switch(type) {
-            case "error":
-                component = withError(Modal);
-                break;
-            case "success":
-                component = withSuccess(Modal);
-                break;
-            case "info":
-                component = withInfo(Modal);
-                break;
-            case "warn":
-                component = withWarn(Modal);
-                break;
-        }
-
-        return component;
+    switch (type) {
+      case 'error':
+        component = withError(Modal)
+        break
+      case 'success':
+        component = withSuccess(Modal)
+        break
+      case 'info':
+        component = withInfo(Modal)
+        break
+      case 'warn':
+        component = withWarn(Modal)
+        break
     }
 
-    function renderModal(props: ModalProps): React.JSX.Element {
-        const { children, ...inlineProps } = props;
-        const ModalComponent = getModalComponent(config.type);
+    return component
+  }
 
-        return (
+  function renderModal (props: ModalProps): React.JSX.Element {
+    const { children, ...inlineProps } = props
+    const ModalComponent = getModalComponent(config.type)
+
+    return (
             <ModalComponent
                 open={isModalOpen}
                 onOk={handleOk}
@@ -58,80 +58,80 @@ export const useModal = (config = {type: "default"}): useModalReturnType => {
             >
                 {children}
             </ModalComponent>
-        );
-    }
+    )
+  }
 
-    return { renderModal, showModal, handleOk, handleCancel };
+  return { renderModal, showModal, handleOk, handleCancel }
 }
 
 export const withError = (Component: typeof Modal): typeof Modal => {
-    const modalWithError = (props: ModalProps): React.JSX.Element => {
-        const {children, ...inlineProps } = props;
+  const modalWithError = (props: ModalProps): React.JSX.Element => {
+    const { children, ...inlineProps } = props
 
-        return (
+    return (
             <Component
-                title={"Error"}
-                icon={<Icon name={'close-circle-filled'} options={{width: 24, height: 24}} />}
+                title={'Error'}
+                icon={<Icon name={'close-circle-filled'} options={{ width: 24, height: 24 }} />}
                 {...inlineProps}
             >
                 {children}
             </Component>
-        );
-    };
+    )
+  }
 
-    return modalWithError;
-};
+  return modalWithError
+}
 
 export const withSuccess = (Component: typeof Modal): typeof Modal => {
-    const modalWithSuccess = (props: ModalProps): React.JSX.Element => {
-        const { children, ...inlineProps } = props;
+  const modalWithSuccess = (props: ModalProps): React.JSX.Element => {
+    const { children, ...inlineProps } = props
 
-        return (
+    return (
             <Component
-                title={"Success"}
-                icon={<Icon name={'check-circle-filled'} options={{width: 24, height: 24}}/>}
+                title={'Success'}
+                icon={<Icon name={'check-circle-filled'} options={{ width: 24, height: 24 }}/>}
                 {...inlineProps}
             >
                 {children}
             </Component>
-        );
-    };
+    )
+  }
 
-    return modalWithSuccess;
-};
+  return modalWithSuccess
+}
 
 export const withInfo = (Component: typeof Modal): typeof Modal => {
-    const modalWithInfo = (props: ModalProps): React.JSX.Element => {
-        const { children, ...inlineProps } = props;
+  const modalWithInfo = (props: ModalProps): React.JSX.Element => {
+    const { children, ...inlineProps } = props
 
-        return (
+    return (
             <Component
-                title={"Info"}
-                icon={<Icon name={'info-circle-filled'} options={{width: 24, height: 24}}/>}
+                title={'Info'}
+                icon={<Icon name={'info-circle-filled'} options={{ width: 24, height: 24 }}/>}
                 {...inlineProps}
             >
                 {children}
             </Component>
-        );
-    };
+    )
+  }
 
-    return modalWithInfo;
-};
+  return modalWithInfo
+}
 
 export const withWarn = (Component: typeof Modal): typeof Modal => {
-    const modalWithWarn = (props: ModalProps): React.JSX.Element => {
-        const { children, ...inlineProps } = props;
+  const modalWithWarn = (props: ModalProps): React.JSX.Element => {
+    const { children, ...inlineProps } = props
 
-        return (
+    return (
             <Component
                 title="Warn"
-                icon={<Icon name={'exclamation-circle-filled'} options={{width: 24, height: 24}}/>}
+                icon={<Icon name={'exclamation-circle-filled'} options={{ width: 24, height: 24 }}/>}
                 {...inlineProps}
             >
                 {children}
             </Component>
-        );
-    };
+    )
+  }
 
-    return modalWithWarn;
-};
+  return modalWithWarn
+}
