82ef805887d68b248321a0bfcba4f107dd28ec38
Corepex <Corepex@users.noreply.github.com>
2024-03-21 14:20:13 +0000

    Apply eslint-fixer changes

--- assets/js/src/components/sidebar/sidebar.stories.tsx
+++ assets/js/src/components/sidebar/sidebar.stories.tsx
@@ -1,35 +1,35 @@
-import {type Meta} from '@storybook/react'
-import {Sidebar} from "@Pimcore/components/sidebar/sidebar";
-import {AssetEditorSidebarDetailsTab} from "@Pimcore/modules/asset/types/image/sidebar/tabs/details/details";
-import {Icon} from "@Pimcore/components/icon/icon";
-import React from 'react';
+import { type Meta } from '@storybook/react'
+import { Sidebar } from '@Pimcore/components/sidebar/sidebar'
+import { AssetEditorSidebarDetailsTab } from '@Pimcore/modules/asset/types/image/sidebar/tabs/details/details'
+import { Icon } from '@Pimcore/components/icon/icon'
+import React from 'react'
 
 const config: Meta = {
-    title: 'Pimcore studio/UI/Sidebar',
-    component: Sidebar,
-    parameters: {
-        layout: 'centered'
-    },
-    tags: ['autodocs'],
-    argTypes: {
-        entries: {
-            table: {
-                disable: true
-            }
-        },
+  title: 'Pimcore studio/UI/Sidebar',
+  component: Sidebar,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    entries: {
+      table: {
+        disable: true
+      }
     }
+  }
 }
 
 export default config
 
 export const _default = {
-    args: {
-        entries: [
-            {
-                key: 'details',
-                icon: <Icon name={'view-details'} options={{ width: '16px', height: '16px' }}/>,
-                component: <AssetEditorSidebarDetailsTab/>
-            }
-        ]
-    }
+  args: {
+    entries: [
+      {
+        key: 'details',
+        icon: <Icon name={'view-details'} options={{ width: '16px', height: '16px' }}/>,
+        component: <AssetEditorSidebarDetailsTab/>
+      }
+    ]
+  }
 }
