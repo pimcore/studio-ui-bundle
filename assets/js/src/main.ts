e55004e35b71480f3c5054d22f254f66b434e81e
Marvin Bertram <marvin.bertram@pimcore.com>
2024-04-10 15:50:28 +0200

    Storybook improvements (#200)
    * Add portotype for accessibility checks in storybook
    
    * Add storybook documentation for modules
    
    * Remove unused react dependency
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <vin0401@users.noreply.github.com>

--- assets/js/src/main.ts
+++ assets/js/src/main.ts
@@ -1,8 +1,4 @@
-import '@Pimcore/app/i18n'
-import '@Pimcore/modules/example'
-import '@Pimcore/modules/asset'
-import 'flexlayout-react/style/light.css'
-import '../../css/globals.css'
+import '@Pimcore/bootstrap'
 import { runApp } from './modules/app/utils/app-runner'
 
 if (module.hot !== undefined) {
