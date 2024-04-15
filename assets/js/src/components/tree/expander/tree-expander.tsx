f74928b6410a04472dc161eb7c874c439c92984f
Marvin Bertram <marvin.bertram@pimcore.com>
2024-04-15 15:46:05 +0200

    Add keyboard support for tree (#212)
    * Add keyboard support for tree
    
    * Add focus style for tree nodes

--- assets/js/src/components/tree/expander/tree-expander.tsx
+++ assets/js/src/components/tree/expander/tree-expander.tsx
@@ -2,6 +2,7 @@ import React, { type MouseEvent, useContext } from 'react'
 import { type TreeNodeProps } from '../node/tree-node'
 import { TreeContext } from '../tree'
 import { Icon } from '@Pimcore/components/icon/icon'
+import { useTranslation } from 'react-i18next'
 
 export interface TreeExpanderProps {
   node: TreeNodeProps
@@ -11,13 +12,14 @@ export interface TreeExpanderProps {
 export const TreeExpander = ({ node, state }: TreeExpanderProps): React.JSX.Element => {
   const { hasChildren, children } = node
   const { onLoad } = useContext(TreeContext)
-  const [isExapanded, setIsExpanded] = state
+  const [isExpanded, setIsExpanded] = state
+  const { t } = useTranslation()
 
   async function onClick (event: MouseEvent): Promise<void> {
     event.stopPropagation()
 
     if (hasChildren === true) {
-      const newExpandedValue = !isExapanded
+      const newExpandedValue = !isExpanded
 
       if (newExpandedValue && onLoad !== undefined && children.length === 0) {
         await onLoad(node)
@@ -33,8 +35,15 @@ export const TreeExpander = ({ node, state }: TreeExpanderProps): React.JSX.Elem
       style={ { minWidth: 16, width: 16, height: 16 } }
     >
       {node.hasChildren === true && (
-        <span onClick={ onClick }>
-          {isExapanded
+        // keyboard navigation is already handled on parent level
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+        <span
+          aria-label={ t('tree.aria.expand-and-collapse') }
+          onClick={ onClick }
+          role='button'
+          tabIndex={ -1 }
+        >
+          {isExpanded
             ? (
               <Icon
                 name="chevron-up"
