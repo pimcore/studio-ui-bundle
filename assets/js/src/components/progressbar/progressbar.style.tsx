fe28ee188975d0cbde9808d7b3d8b533044a31cc
Corepex <christian.feldkirchner@pimcore.com>
2024-04-18 10:56:04 +0200

    added progressbar component

---
+++ assets/js/src/components/progressbar/progressbar.style.tsx
@@ -0,0 +1,49 @@
+import { createStyles } from 'antd-style'
+
+export const useStyle = createStyles(({ token, css }) => {
+  return {
+    progressbar: css`
+        .progressbar-description {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: flex-end;
+            
+          p {
+            color: ${token.colorTextTertiary};
+            margin: 0;
+            font-size: 12px;
+            font-weight: 400;
+            line-height: 22px;
+          }
+            
+          .ant-btn {
+            color: ${token.colorPrimary};
+            height: ${token.controlHeight};
+            display: flex;
+            justify-content: center;
+            padding: 0 ${token.paddingXS}px;
+            align-items: flex-end;
+            
+            &:hover {
+              color: ${token.colorPrimaryHover}
+            }   
+          }
+        }
+        
+        .ant-progress {
+          margin-bottom: 0;
+        }
+
+        .progressbar-status {
+          p {
+            color: ${token.colorTextSecondary}
+            font-size: 12px;
+            font-weight: 400;
+            line-height: 22px;
+            margin: 0;
+          }
+        }
+    `,
+  }
+}, { hashPriority: 'low' })
---
+++ assets/js/src/components/progressbar/progressbar.tsx
@@ -0,0 +1,32 @@
+import {Progress, ProgressProps} from "antd";
+import React from "react";
+import {useStyle} from "@Pimcore/components/progressbar/progressbar.style";
+
+interface IProgressProps extends ProgressProps {
+  description: string;
+  descriptionAction?: React.ReactNode;
+  progressStatus: string;
+}
+
+export const Progressbar = (props: IProgressProps) => {
+  const {styles} = useStyle();
+
+  return (
+    <div className={styles.progressbar}>
+      <div className={'progressbar-description'}>
+        <p id={'progressbarLabel'}>{props.description}</p>
+        <div className={'progressbar-description__actions'}>
+          {props.descriptionAction}
+        </div>
+      </div>
+      <Progress
+        {...props}
+        showInfo={false}
+        aria-labelledby={'progressbarLabel'}
+      />
+      <div className={'progressbar-status'}>
+        <p>{props.progressStatus}</p>
+      </div>
+    </div>
+  )
+}
