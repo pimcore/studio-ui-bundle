e0b312d24bad4b28dc3037c974aadacec2e3220e
vin0401 <marvin.bertram@pimcore.com>
2024-05-02 17:53:46 +0200

    Remove unnecessary white space

--- assets/.eslintrc.js
+++ assets/.eslintrc.js
@@ -99,7 +99,7 @@ module.exports = {
       '* - Pimcore Commercial License (PCL)',
       '* Full copyright and license information is available in',
       '* LICENSE.md which is distributed with this source code.',
-      '* ',
+      '*',
       '*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)',
       '*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL',
       ''
