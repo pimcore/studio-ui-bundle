7a6e014f255131b81a28aae7d00f548d76315103
vin0401 <marvin.bertram@pimcore.com>
2024-05-02 17:03:13 +0200

    Merge eslint configs

--- assets/.eslintrc.js
+++ assets/.eslintrc.js
@@ -53,7 +53,8 @@ module.exports = {
     }
   },
   plugins: [
-    'react'
+    'react',
+    'header'
   ],
   rules: {
     '@typescript-eslint/no-misused-promises': 'off',
@@ -85,6 +86,17 @@ module.exports = {
         logical: 'parens-new-line',
         prop: 'ignore'
       }
-    ]
+    ],
+    'header/header': [2, 'block', [
+      '*',
+      '* Pimcore',
+      '*',
+      '* This source file is available under two different licenses:',
+      '* - Pimcore Open Core License (POCL)',
+      '* - Pimcore Commercial License (PCL)',
+      '* Full copyright and license information is available in',
+      '* LICENSE.md which is distributed with this source code.',
+      ''
+    ], 2]
   }
 }
