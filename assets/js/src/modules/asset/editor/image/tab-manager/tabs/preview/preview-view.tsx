154ed252f151e4043588ce6bc666c4a9e6130604
markus-moser <markus.moser@pimcore.com>
2024-05-03 16:21:47 +0200

    License requirements (#233)

--- assets/js/src/modules/asset/editor/image/tab-manager/tabs/preview/preview-view.tsx
+++ assets/js/src/modules/asset/editor/image/tab-manager/tabs/preview/preview-view.tsx
@@ -1,3 +1,16 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
 import { PimcoreImage } from '@Pimcore/components/pimcore-image/pimcore-image'
 import React from 'react'
 import { useStyle } from './preview-view.styles'
