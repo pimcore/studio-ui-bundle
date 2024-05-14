a52f6f5bb5b899d6a42b122ac654508d4057cb04
Corepex <christian.feldkirchner@pimcore.com>
2024-05-14 14:34:37 +0200

    moved files due to structure change in 1.x

--- assets/js/src/core/app/api/pimcore/index.ts
+++ assets/js/src/core/app/api/pimcore/index.ts
@@ -12,8 +12,20 @@
 */
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {RootState} from "@Pimcore/app/store";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/',
+    prepareHeaders: (headers, { getState }) => {
+      // By default, if we have a token in the store, let's use that for authenticated requests
+      const token = (getState() as RootState).auth?.token
+      if (token !== null) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+
+      return headers
+    }
+  }),
   endpoints: () => ({})
 })
