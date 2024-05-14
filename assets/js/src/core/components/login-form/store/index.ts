e9799c5f8422efb2eae1266ef77cd5fd4d8bc09e
Corepex <Corepex@users.noreply.github.com>
2024-05-14 12:59:47 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/login-form/store/index.ts
+++ assets/js/src/core/components/login-form/store/index.ts
@@ -1,14 +1,27 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
 import { configureStore } from '@reduxjs/toolkit'
-import { authReducer } from '@Pimcore/app/auth/auth-slice';
-import {api} from "@Pimcore/components/login-form/services/auth";
+import { authReducer } from '@Pimcore/app/auth/auth-slice'
+import { api } from '@Pimcore/components/login-form/services/auth'
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
-    auth: authReducer,
+    auth: authReducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
