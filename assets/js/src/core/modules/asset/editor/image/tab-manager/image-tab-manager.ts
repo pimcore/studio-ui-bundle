da57b4ffa537ea4a6311bc0dc0f0ff5b742107dd
Corepex <christian.feldkirchner@pimcore.com>
2024-05-15 15:35:17 +0200

    implemented dependency-injection system for `imageEditorTabs`

--- assets/js/src/core/modules/asset/editor/image/tab-manager/image-tab-manager.ts
+++ assets/js/src/core/modules/asset/editor/image/tab-manager/image-tab-manager.ts
@@ -13,7 +13,9 @@
 
 import { TabManager } from '@Pimcore/modules/element/editor/tab-manager/tab-manager'
 import { type IAssetEditorTabManager } from '@Pimcore/modules/element/editor/tab-manager/interface/IAssetEditorTabManager'
+import { injectable } from 'inversify'
 
+@injectable()
 export class ImageTabManager extends TabManager implements IAssetEditorTabManager {
   constructor () {
     super()
