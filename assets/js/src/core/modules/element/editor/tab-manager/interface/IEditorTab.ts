221f5d2399f906fb7e55a9e39e6acba1ad7ab2b0
Corepex <Corepex@users.noreply.github.com>
2024-05-16 12:09:21 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/element/editor/tab-manager/interface/IEditorTab.ts
+++ assets/js/src/core/modules/element/editor/tab-manager/interface/IEditorTab.ts
@@ -15,7 +15,7 @@ import type React from 'react'
 
 export interface IEditorTab {
   key: string
-  label: string|React.JSX.Element
+  label: string | React.JSX.Element
   children: React.JSX.Element
   icon: React.JSX.Element
   isDetachable?: boolean
