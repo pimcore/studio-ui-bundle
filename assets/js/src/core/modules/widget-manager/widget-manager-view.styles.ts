d646f6dffe98719525fd85f82bca4118093bb66f
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2024-05-16 15:23:12 +0200

    Widget manager improvements (#232)
    * Improve: widget-manager
    
    * Apply eslint-fixer changes
    
    * Fix: add shadow again
    
    ---------
    
    Co-authored-by: vin0401 <marvin.bertram@pimcore.com>
    Co-authored-by: vin0401 <vin0401@users.noreply.github.com>

--- assets/js/src/core/modules/widget-manager/widget-manager-view.styles.ts
+++ assets/js/src/core/modules/widget-manager/widget-manager-view.styles.ts
@@ -47,10 +47,10 @@ export const getTabTokens = (token): TabsToken => {
   tabTokens = {
     ...tabTokens,
     tabsCardPadding:
-    token.cardPadding ??
-    `${
-      (tabTokens.cardHeight - Math.round(token.fontSize * token.lineHeight)) / 2 - token.lineWidth
-    }px ${token.paddingSM}px`,
+      token.cardPadding ??
+      `${
+        (tabTokens.cardHeight - Math.round(token.fontSize * token.lineHeight)) / 2 - token.lineWidth
+      }px ${token.paddingSM}px`,
     dropdownEdgeChildVerticalPadding: token.paddingXXS,
     tabsActiveTextShadow: '0 0 0.25px currentcolor',
     tabsDropdownHeight: 200,
@@ -67,187 +67,196 @@ export const useStlyes = createStyles(({ token, css }) => {
 
   return {
     widgetManager: css`
-      position: absolute;
-      inset: 8px 6px 12px 6px;
-
-      .flexlayout__layout {
-        overflow: visible;
-      }
-
-      &.widget-manager--inner {
-        inset: 0;
-      }
-
-      .flexlayout__tab_button_leading,
-      .flexlayout__border_button_leading {
-        display: none;
-      }
-
-      .flexlayout__tab_button {
-        margin: 0;
-        padding: ${tabToken.tabsCardPadding};
-        background: ${token.Tabs.colorBgUnselectedTab};
-        transition: all ${token.motionDurationSlow} ${token.motionEaseInOut};
-        font-size: ${token.fontSize}px;
-        color: ${tabToken.itemColor};
-        outline: none;
-        gap: ${token.marginXS}px;
-      
-        &:hover {
-          background: ${token.Tabs.colorBgHoverUnselectedTab};
+        position: absolute;
+        inset: 8px 6px 12px 6px;
+  
+        .flexlayout__layout {
+          overflow: visible;
         }
-
-        &_trailing {
+  
+        &.widget-manager--inner {
+          inset: 0;
+        }
+  
+        .flexlayout__tab_button_leading,
+        .flexlayout__border_button_leading {
           display: none;
         }
-
-        &--selected {
-          font-weight: ${token.fontWeightStrong};
-          color: ${tabToken.itemActiveColor};
-          background: ${token.colorBgContainer};
-          border-top: 1px solid ${token.Tabs.colorBorderActiveTab};
-
-          .widget-manager-tab-title {
-            margin-top: -2px;
-          }
-
+  
+        .flexlayout__tab_button {
+          margin: 0;
+          padding: ${tabToken.tabsCardPadding};
+          background: ${token.colorFillAlter};
+          transition: all ${token.motionDurationSlow} ${token.motionEaseInOut};
+          font-size: ${token.fontSize}px;
+          color: ${tabToken.itemColor};
+          outline: none;
+          gap: ${token.marginXS}px;
+        
           &:hover {
+            background: ${token.Tabs.colorBgHoverUnselectedTab};
+          }
+  
+          &_trailing {
+            display: none;
+          }
+  
+          &--selected {
+            font-weight: ${token.fontWeightStrong};
+            color: ${tabToken.itemActiveColor};
             background: ${token.colorBgContainer};
+            border-top: 2px solid ${token.Tabs.colorBorderActiveTab};
+  
+            .widget-manager-tab-title {
+              margin-top: -2px;
+            }
+  
+            &:hover {
+              background: ${token.colorBgContainer};
+            }
+  
+            & .flexlayout__tab_button_trailing {
+              margin-top: -2px;
+              display: flex;
+            }
           }
-
-          & .flexlayout__tab_button_trailing {
-            margin-top: -2px;
-            display: flex;
+  
+          &:focus:not(:focus-visible), &:active {
+            color: ${tabToken.itemActiveColor};
+          }
+  
+          &:first-child {
+            border-left: 1px solid ${token.Tabs.colorBorderContainer}66;
           }
         }
-
-        &:focus:not(:focus-visible), &:active {
-          color: ${tabToken.itemActiveColor};
+  
+        .flexlayout__tabset_tab_divider {
+          width: ${tabToken.cardGutter}px;
         }
-
-        &:first-child {
-          border-left: 1px solid ${token.Tabs.colorBorderContainer}66;
+  
+        .flexlayout__tab_button_top {
+          border-radius: ${token.borderRadius}px ${token.borderRadius}px 0 0;
+          border-bottom: 0;
         }
-      }
-
-      .flexlayout__tabset_tab_divider {
-        width: ${tabToken.cardGutter}px;
-      }
-
-      .flexlayout__tab_button_top {
-        border-radius: ${token.borderRadius}px ${token.borderRadius}px 0 0;
-        border-bottom: 0;
-      }
-
-      .flexlayout__border_inner_tab_container {
-        width: calc(100svh - 12px);
-        justify-content: flex-end;
-      }
-
-      .flexlayout__border_inner_tab_container_left, .flexlayout__border_inner_tab_container_right {
-        .flexlayout__border_tab_divider {
-          width: 0;
+  
+        .flexlayout__border_inner_tab_container {
+          width: calc(100svh - 12px);
+          justify-content: flex-end;
         }
-      }
-
-      .flexlayout__splitter,
-      .flexlayout__border,
-      .flexlayout__tabset_tabbar_outer {
-        background: transparent;
-      }
-
-      .flexlayout__tab {
-        overflow: visible;
-        background: ${token.colorBgContainer};
-        box-shadow: 0px 8px 24px 0px rgba(0, 0, 0, 0.07), 2px 2px 0px 0px rgba(79, 78, 183, 0.05);
-        border-right: 1px solid ${token.Tabs.colorBorderContainer}66;
-        border-bottom: 1px solid ${token.Tabs.colorBorderContainer}66;
-        border-left: 1px solid ${token.Tabs.colorBorderContainer}66;
-        border-radius: 0 8px 8px 8px;
-      }
-
-      .flexlayout__tab_border {
-        box-shadow: 0px 8px 24px 0px rgba(0, 0, 0, 0.07), 2px 2px 0px 0px rgba(79, 78, 183, 0.05);
-        border-top: 1px solid ${token.Tabs.colorBorderContainer}66;
-        border-right: 1px solid ${token.Tabs.colorBorderContainer}66;
-        border-bottom: 1px solid ${token.Tabs.colorBorderContainer}66;
-        border-left: 1px solid ${token.Tabs.colorBorderContainer}66;
-        border-radius: 8px;
-      }
-
-      .widget-manager-inner-container {
-        background: transparent;
-        box-shadow: none;
-        border: 0;
-      }
-
-      .flexlayout__tabset {
-        overflow: visible;
-        border-radius: ${token.borderRadius}px;
-        font-family: ${token.fontFamily};
-
-        &, &-selected {
+  
+        .flexlayout__border_inner_tab_container_left, .flexlayout__border_inner_tab_container_right {
+          .flexlayout__border_tab_divider {
+            width: 0;
+          }
+        }
+  
+        .flexlayout__splitter,
+        .flexlayout__border,
+        .flexlayout__tabset_tabbar_outer {
           background: transparent;
         }
-      }
-
-      .flexlayout__border {
-        font-family: ${token.fontFamily};
-      }
-
-      .flexlayout__border_button {
-        margin: 0 0 6px 0;
-        background: transparent;
-        width: 40px;
-        justify-content: center;
-        border-radius: ${token.borderRadiusSM}px;
-        transition: all ${token.motionDurationSlow} ${token.motionEaseInOut};
-
-        &--selected {
-          color: ${tabToken.itemActiveColor};
-          border-top: 1.5px solid var(--Colors-Base-Purple-7, #531DAB);
-          background: ${token.controlItemBgHover};
+  
+        .flexlayout__tab {
+          overflow: visible;
+          background: ${token.colorBgContainer};
+          box-shadow: 0px 8px 24px 0px rgba(0, 0, 0, 0.07), 2px 2px 0px 0px rgba(79, 78, 183, 0.05);
+          border-right: 1px solid ${token.Tabs.colorBorderContainer}66;
+          border-bottom: 1px solid ${token.Tabs.colorBorderContainer}66;
+          border-left: 1px solid ${token.Tabs.colorBorderContainer}66;
+          border-radius: 0 8px 8px 8px;
         }
-      }
 
-      @media (hover: hover) {
-        .flexlayout__border_button--unselected:hover {
-          color: ${token.colorTextSecondary}; 
-          background: ${token.controlItemBgActiveHover};
+        .flexlayout__tab:not(.widget-manager-inner-container) {
+          overflow: hidden;
         }
-      }
-
-      .flexlayout__border_button_trailing {
-        display: none;
-      }
-
-      .flexlayout__border_left {
-        border-right: 0;
-
-        .flexlayout__border_button_content {
-          transform: rotate(90deg);
+      
+        .flexlayout__tab_border {
+          box-shadow: 0px 8px 24px 0px rgba(0, 0, 0, 0.07), 2px 2px 0px 0px rgba(79, 78, 183, 0.05);
+          border-top: 1px solid ${token.Tabs.colorBorderContainer}66;
+          border-right: 1px solid ${token.Tabs.colorBorderContainer}66;
+          border-bottom: 1px solid ${token.Tabs.colorBorderContainer}66;
+          border-left: 1px solid ${token.Tabs.colorBorderContainer}66;
+          border-radius: 8px;
         }
-      }
-
-      .flexlayout__border_right {
-        border-left: 0;
-
-        .flexlayout__border_button_content {
-          transform: rotate(-90deg);
+  
+        .widget-manager-inner-container {
+          background: transparent;
+          box-shadow: none;
+          border: 0;
         }
-      }
-
-      .flexlayout__tabset_tabbar_outer_top {
-        border: 0;
-      }
-
-      .flexlayout__tabset_tabbar_inner_tab_container {
-        padding-left: 0;
-      }
-
-      .flexlayout__border_toolbar {
-        display: none;
-      }
-    `
+  
+        .flexlayout__tabset {
+          overflow: visible;
+          border-radius: ${token.borderRadius}px;
+          font-family: ${token.fontFamily};
+  
+          &, &-selected {
+            background: transparent;
+          }
+        }
+  
+        .flexlayout__border {
+          font-family: ${token.fontFamily};
+        }
+  
+        .flexlayout__border_button {
+          margin: 0 0 6px 0;
+          background: transparent;
+          width: 40px;
+          justify-content: center;
+          border-radius: ${token.borderRadiusSM}px;
+          transition: all ${token.motionDurationSlow} ${token.motionEaseInOut};
+  
+          &--selected {
+            color: ${tabToken.itemActiveColor};
+            border-top: 1.5px solid ${token.colorBorderActive};
+            background: ${token.controlItemBgHover};
+          }
+        }
+  
+        @media (hover: hover) {
+          .flexlayout__border_button--unselected:hover {
+            color: ${token.colorTextSecondary}; 
+            background: ${token.controlItemBgActiveHover};
+          }
+          
+          .flexlayout__tab_button--selected:hover {
+            color: ${tabToken.itemActiveColor};
+            background: ${token.colorBgContainer};
+          }
+        }
+  
+        .flexlayout__border_button_trailing {
+          display: none;
+        }
+  
+        .flexlayout__border_left {
+          border-right: 0;
+  
+          .flexlayout__border_button_content {
+            transform: rotate(90deg);
+          }
+        }
+  
+        .flexlayout__border_right {
+          border-left: 0;
+  
+          .flexlayout__border_button_content {
+            transform: rotate(-90deg);
+          }
+        }
+  
+        .flexlayout__tabset_tabbar_outer_top {
+          border: 0;
+        }
+  
+        .flexlayout__tabset_tabbar_inner_tab_container {
+          padding-left: 0;
+        }
+  
+        .flexlayout__border_toolbar {
+          display: none;
+        }
+      `
   }
 }, { hashPriority: 'low' })
