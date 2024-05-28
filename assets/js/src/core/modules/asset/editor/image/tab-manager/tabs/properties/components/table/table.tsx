f5eaf85700812d422f16853e3ab6c0508d4a6d4f
Corepex <christian.feldkirchner@pimcore.com>
2024-05-28 08:22:26 +0200

    added `element-toolbar`, splitted properties table into two, added api slice

---
+++ assets/js/src/core/modules/asset/editor/image/tab-manager/tabs/properties/components/table/table.tsx
@@ -0,0 +1,122 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import {
+  type DataProperty,
+  useGetPropertiesForElementByTypeAndIdQuery
+} from '@Pimcore/modules/asset/properties-api-slice.gen'
+import React, { useEffect, useState } from 'react'
+import { Grid } from '@Pimcore/components/grid/grid'
+import { createColumnHelper } from '@tanstack/react-table'
+import { useTranslation } from 'react-i18next'
+import { useGlobalAssetContext } from '@Pimcore/modules/asset/hooks/use-global-asset-context'
+import { Checkbox, Result } from 'antd'
+import { useStyles } from '@Pimcore/modules/asset/editor/image/tab-manager/tabs/properties/components/table/table.styles'
+
+interface ITableProps {
+  propertiesTableTab: string
+}
+
+export const Table = ({ propertiesTableTab }: ITableProps): React.JSX.Element => {
+  const { t } = useTranslation()
+  const { context } = useGlobalAssetContext()
+  const { styles } = useStyles()
+
+  if (context === undefined) {
+    return <Result title="Missing context" />
+  }
+
+  const { data, isLoading } = useGetPropertiesForElementByTypeAndIdQuery({
+    elementType: 'asset',
+    id: context?.config.id
+  })
+
+  const [gridDataOwn, setGridDataOwn] = useState<DataProperty[]>([])
+  const [gridDataAll, setGridDataAll] = useState<DataProperty[]>([])
+  useEffect(() => {
+    if (data !== undefined && Array.isArray(data.items)) {
+      if (propertiesTableTab === 'own') {
+        setGridDataOwn(data?.items.filter((item) => {
+          return item.inherited === false
+        }))
+      }
+
+      setGridDataAll(data.items)
+    }
+  }, [propertiesTableTab, data])
+
+  const columnHelper = createColumnHelper<DataProperty>()
+  const columns = [
+    columnHelper.accessor('type', {
+      header: t('asset.asset-editor-tabs.properties.columns.type')
+    }),
+    columnHelper.accessor('key', {
+      header: t('asset.asset-editor-tabs.properties.columns.key')
+    }),
+    columnHelper.accessor('description', {
+      header: t('asset.asset-editor-tabs.properties.columns.description')
+    }),
+    columnHelper.accessor('data', {
+      header: t('asset.asset-editor-tabs.properties.columns.data')
+    }),
+    columnHelper.accessor('inheritable', {
+      header: t('asset.asset-editor-tabs.properties.columns.inheritable'),
+      cell: (info) => {
+        return <Checkbox checked={ info.row.original.inheritable } />
+      },
+      size: 70,
+      id: 'properties-table--inheritable-column'
+    }),
+    columnHelper.accessor('actions', {
+      header: t('asset.asset-editor-tabs.properties.columns.inheritable'),
+      cell: (info) => {
+        return (
+          <div>
+            <button>edit</button>
+            <button>delete</button>
+          </div>
+        )
+      },
+      id: 'properties-table--actions-column'
+    })
+  ]
+
+  return (
+    <div className={ styles.table }>
+      {isLoading && (
+        <p>Counting Lamas ...</p>
+      )}
+
+      {!isLoading && (
+        <>
+          <Grid
+            columns={ columns }
+            data={ gridDataOwn }
+          />
+
+          {propertiesTableTab === 'all' && (
+            <>
+              <p className={ 'headline' }>
+                {t('asset.asset-editor-tabs.properties.inherited.properties')}
+              </p>
+              <Grid
+                columns={ columns }
+                data={ gridDataAll }
+              />
+            </>
+          )}
+        </>
+      )}
+    </div>
+  )
+}
