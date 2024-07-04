6b659274d8f674b086fcf50039d07224b4c6a9f0
Corepex <christian.feldkirchner@pimcore.com>
2024-07-04 14:14:14 +0200

    added `skeleton` loader to grid

--- assets/js/src/core/components/grid/grid.tsx
+++ assets/js/src/core/components/grid/grid.tsx
@@ -13,12 +13,13 @@
 
 import { useCssComponentHash } from '@Pimcore/modules/ant-design/hooks/use-css-component-hash'
 import { type ColumnDef, flexRender, getCoreRowModel, useReactTable, type ColumnResizeMode, type TableOptions, type RowData, type CellContext } from '@tanstack/react-table'
-import React, { useEffect, useRef, useState } from 'react'
+import React, {useEffect, useMemo, useRef, useState} from 'react'
 import { useStyles } from './grid.styles'
 import { Resizer } from './resizer/resizer'
 import { DefaultCell } from './columns/default-cell'
 import { GridContextProvider } from './grid-context'
 import { useTranslation } from 'react-i18next'
+import {Skeleton} from "antd";
 
 declare module '@tanstack/react-table' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -43,6 +44,7 @@ export interface GridProps {
   resizable?: boolean
   onUpdateCellData?: ({ rowIndex, columnId, value }: { rowIndex: number, columnId: string, value: any }) => void
   modifiedCells?: Array<{ rowIndex: number, columnId: string }>
+  isLoading?: boolean
 }
 
 export const Grid = (props: GridProps): React.JSX.Element => {
@@ -54,13 +56,31 @@ export const Grid = (props: GridProps): React.JSX.Element => {
   const [columnResizeMode] = useState<ColumnResizeMode>('onEnd')
   const tableElement = useRef<HTMLTableElement>(null)
 
-  useEffect(() => {
-    setData(props.data)
-  }, [props.data])
+  const tableData = useMemo(
+    () => (props.isLoading ? Array(5).fill({}) : props.data),
+    [props.isLoading, props.data]
+  );
 
   useEffect(() => {
-    setColumns(props.columns)
-  }, [props.columns])
+    setData(tableData)
+  }, [tableData])
+
+  const tableColumns = useMemo(
+    () =>
+      props.isLoading
+        ? props.columns.map((column) => ({
+          ...column,
+          cell: <Skeleton.Input active={true} size={'small'}/>,
+        }))
+        : props.columns,
+    [props.isLoading, columns]
+  );
+
+  useEffect(() => {
+    setColumns(tableColumns as GridProps['columns'])
+  }, [tableColumns])
+
+
 
   const tableProps: TableOptions<any> = {
     data,
