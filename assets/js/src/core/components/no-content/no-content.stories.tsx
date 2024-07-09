2a9b2fe216e255f19e6af53d8461cd7298ff90b4
Corepex <Corepex@users.noreply.github.com>
2024-07-09 09:31:15 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/no-content/no-content.stories.tsx
+++ assets/js/src/core/components/no-content/no-content.stories.tsx
@@ -1,18 +1,18 @@
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - Pimcore Open Core License (POCL)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
- */
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
 
-import {type Meta} from '@storybook/react'
-import {NoContent} from "@Pimcore/components/no-content/no-content";
+import { type Meta } from '@storybook/react'
+import { NoContent } from '@Pimcore/components/no-content/no-content'
 
 const config: Meta = {
   title: 'Pimcore studio/UI/No Content',
@@ -20,7 +20,7 @@ const config: Meta = {
   parameters: {
     layout: 'centered'
   },
-  tags: ['autodocs'],
+  tags: ['autodocs']
 }
 
 export default config
