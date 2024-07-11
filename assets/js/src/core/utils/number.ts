3b51c03fb79ae9739bf06fd90fc5a4e3e7af8f56
Corepex <Corepex@users.noreply.github.com>
2024-07-11 07:14:16 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/utils/number.ts
+++ assets/js/src/core/utils/number.ts
@@ -11,7 +11,7 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
-import i18n from 'i18next';
+import i18n from 'i18next'
 
 interface IFormatNumberProps {
   value: number
