65cec4063defb44bc448dac3f83a4466f404182a
Corepex <christian.feldkirchner@pimcore.com>
2024-08-21 09:15:36 +0200

    fixed focalpoint story && build files

--- assets/js/src/core/components/focal-point/focal-point.stories.tsx
+++ assets/js/src/core/components/focal-point/focal-point.stories.tsx
@@ -1,14 +1,14 @@
 import type {Meta} from "@storybook/react";
 import {FocalPoint} from "@Pimcore/components/focal-point/focal-point";
 import React from "react";
+import { PimcoreImage } from "../pimcore-image/pimcore-image";
 
 const config: Meta = {
   title: 'Pimcore studio/UI/Focal Point',
   component: () => {
     return (
       <FocalPoint>
-        {/* <PimcoreImage src={ 'https://picsum.photos/800/600.jpg' } /> */}
-        <img src={'https://picsum.photos/800/600.jpg'} />
+        <PimcoreImage src={ 'https://picsum.photos/800/600.jpg' } />
       </FocalPoint>
     )
   },
