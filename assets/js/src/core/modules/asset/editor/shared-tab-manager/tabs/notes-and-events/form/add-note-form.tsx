66bd8b8a8598fb5f3acd89c05862c835a5e600b5
markus-moser <markus.moser@pimcore.com>
2024-08-22 18:56:04 +0200

    Optimize add note modal

---
+++ assets/js/src/core/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/form/add-note-form.tsx
@@ -0,0 +1,71 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import React from 'react'
+import { Form, type FormProps, Input, Select } from 'antd'
+import TextArea from 'antd/es/input/TextArea'
+import { useTranslation } from 'react-i18next'
+
+export interface AddNoteFormValues {
+  type: string
+  title: string
+  description: string
+}
+
+export interface AddNoteFormProps extends FormProps {}
+
+export const AddNoteForm = ({ ...props }: AddNoteFormProps): React.JSX.Element => {
+  const { t } = useTranslation()
+
+  return (
+    <Form
+      layout="vertical"
+      { ...props }
+    >
+      <Form.Item
+        label={ t('type') }
+        name="type"
+      >
+        <Select
+          options={ [
+            {
+              value: 'content', label: t('notes-and-events.content')
+            }, {
+              value: 'seo', label: t('notes-and-events.seo')
+            }, {
+              value: 'warning', label: t('notes-and-events.warning')
+            }, {
+              value: 'notice', label: t('notes-and-events.notice')
+            }
+          ] }
+          placeholder={ t('select') }
+        />
+      </Form.Item>
+      <Form.Item
+        label={ t('title') }
+        name="title"
+        rules={ [{ required: true, message: t('form.validation.required') }] }
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label={ t('description') }
+        name="description"
+      >
+        <TextArea
+          autoSize={ { minRows: 3 } }
+        />
+      </Form.Item>
+    </Form>
+  )
+}
---
+++ assets/js/src/core/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/modal/add-note-modal.tsx
@@ -0,0 +1,92 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import React from 'react'
+import { Button, type FormProps, Modal } from 'antd'
+import { useTranslation } from 'react-i18next'
+import { ModalTitle } from '@Pimcore/components/modal/modal-title/modal-title'
+import { AddNoteForm } from '@Pimcore/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/form/add-note-form'
+import { useForm } from 'antd/es/form/Form'
+import {
+  useNoteElementCreateMutation
+} from '@Pimcore/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/notes-and-events-api-slice.gen'
+
+export interface AddNoteFormValues {
+  type: string
+  title: string
+  description: string
+}
+
+export interface AddNoteFormProps extends FormProps {}
+
+export interface AddNoteModalProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+  elementType: 'asset' | 'document' | 'data-object'
+  elementId: number
+}
+
+export const AddNoteModal = ({ ...props }: AddNoteModalProps): React.JSX.Element => {
+  const { t } = useTranslation()
+  const [form] = useForm()
+  const [createNote] = useNoteElementCreateMutation()
+  const [saveInProgress, setSaveInProgress] = React.useState(false)
+
+  async function addNote (type: string = '', title: string, description: string = ''): Promise<void> {
+    await createNote({
+      elementType: props.elementType,
+      id: props.elementId,
+      createNote: {
+        type,
+        title,
+        description
+      }
+    })
+  }
+
+  async function onFinish (values: AddNoteFormValues): Promise<void> {
+    setSaveInProgress(true)
+    await addNote(values.type, values.title, values.description)
+    props.setOpen(false)
+    form.resetFields()
+    setSaveInProgress(false)
+  }
+
+  return (
+    <Modal
+      footer={
+        <Button
+          loading={ saveInProgress }
+          onClick={ () => { form.submit() } }
+          type='primary'
+        >
+          { t('save') }
+        </Button>
+      }
+      onCancel={ () => {
+        props.setOpen(false)
+        form.resetFields()
+      } }
+      open={ props.open }
+      title={ (
+        <ModalTitle iconName='PlusCircleOutlined'>{ t('notes-and-events.new-note') }</ModalTitle>
+          ) }
+    >
+
+      <AddNoteForm
+        form={ form }
+        onFinish={ onFinish }
+      />
+    </Modal>
+  )
+}
