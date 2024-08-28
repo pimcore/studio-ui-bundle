0a655c561a318edb5fdea503256a278d31827437
xIrusux <christine.altmann@pimcore.com>
2024-08-28 12:49:45 +0200

    merge left over

--- assets/js/src/core/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/notes-and-events-view.tsx
+++ assets/js/src/core/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/notes-and-events-view.tsx
@@ -26,11 +26,12 @@ import { formatDateTime } from '@Pimcore/utils/date-time'
 import { respectLineBreak } from '@Pimcore/utils/helpers'
 import { NoContent } from '@Pimcore/components/no-content/no-content'
 import {
+  AddNoteModal
+} from '@Pimcore/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/modal/add-note-modal'
+import {
   CardHeaderContainer
 } from '@Pimcore/modules/asset/editor/shared-tab-manager/tabs/card-header/card-header-container'
 import { CardContainer } from '@Pimcore/modules/asset/editor/shared-tab-manager/tabs/card-container/card-container'
-  AddNoteModal
-} from '@Pimcore/modules/asset/editor/shared-tab-manager/tabs/notes-and-events/modal/add-note-modal'
 
 interface NotesAndEventsTabViewProps {
   notes: Note[]
@@ -39,7 +40,6 @@ interface NotesAndEventsTabViewProps {
   elementType: 'asset' | 'document' | 'data-object'
   elementId: number
 }
-
 export const NotesAndEventsTabView = ({
   notes,
   pagination,
@@ -49,11 +49,6 @@ export const NotesAndEventsTabView = ({
 }: NotesAndEventsTabViewProps): React.JSX.Element => {
   const { t } = useTranslation()
   const { styles } = useStyles()
-  const { renderModal: RenderModal, showModal, handleOk } = useModal()
-
-  let type = ''
-  let title = ''
-  let description = ''
   const [addNoteModalOpen, setAddNoteModalOpen] = useState<boolean>(false)
 
   const NotesAndEvents = notes.map((note) => {
@@ -80,9 +75,7 @@ export const NotesAndEventsTabView = ({
         date={ formatDateTime({ timestamp: note.date, dateStyle: 'short', timeStyle: 'medium' }) }
         description={ note.description }
         key={ note.id }
-        onClickTrash={ () => {
-          onClickTrash(note.id)
-        } }
+        onClickTrash={ () => { onClickTrash(note.id) } }
         showDetails={ showDetails }
         title={ note.title }
         type={ note.type !== '' ? t(`notes-and-events.${note.type}`) : undefined }
@@ -90,57 +83,6 @@ export const NotesAndEventsTabView = ({
       />
     )
   })
-
-  const modal = (
-    <RenderModal
-      footer={
-        <ModalFooter buttonAlignment={ 'end' }>
-          <Button
-            onClick={ onClickSaveModal }
-            type={ 'primary' }
-          >{t('save')}</Button>
-        </ModalFooter>
-            }
-      title={ t('notes-and-events.new-note') }
-    >
-      <div className={ styles['add-note-modal__section'] }>
-        <label>{t('type')}</label>
-        <Select
-          onChange={ (val) => {
-            type = val
-          } }
-          options={ [
-            {
-              value: 'content', label: t('notes-and-events.content')
-            }, {
-              value: 'seo', label: t('notes-and-events.seo')
-            }, {
-              value: 'warning', label: t('notes-and-events.warning')
-            }, {
-              value: 'notice', label: t('notes-and-events.notice')
-            }
-          ] }
-          placeholder={ t('select') }
-        />
-      </div>
-      <div className={ styles['add-note-modal__section'] }>
-        <label>{t('title')}</label><span className={ 'mandatory' }>*</span>
-        <Input
-          onChange={ (e) => {
-            title = e.target.value
-          } }
-        />
-      </div>
-      <div className={ styles['add-note-modal__section'] }>
-        <label>{t('description')}</label>
-        <TextArea
-          onChange={ (e) => {
-            description = e.target.value
-          } }
-        />
-      </div>
-    </RenderModal>
-  )
 
   return (
     <div className={ styles['notes-and-events'] }>
@@ -158,15 +100,13 @@ export const NotesAndEventsTabView = ({
             >
               {t('add')}
             </Button>
-            {modal}
-          </CardHeaderContainer>
             <AddNoteModal
               elementId={ elementId }
               elementType={ elementType }
               open={ addNoteModalOpen }
               setOpen={ setAddNoteModalOpen }
             />
-          </div>
+          </CardHeaderContainer>
           {notes.length > 0
             ? (
               <CardContainer>
@@ -178,7 +118,7 @@ export const NotesAndEventsTabView = ({
                 <NoContent text={ t('notes-and-events.no-notes-and-events-to-show') } />
               </div>
               )
-                    }
+          }
         </div>
         <div className={ 'notes-container__pagination-container' }>
           <div className={ 'notes-container__pagination' }>
