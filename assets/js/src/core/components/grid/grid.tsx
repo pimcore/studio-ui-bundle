85361ab054374994f0a062b0b7edd05ca374cfe6
xIrusux <christine.altmann@pimcore.com>
2024-08-29 12:27:03 +0200

    ensure type is String instead

--- assets/js/src/core/components/grid/grid.tsx
+++ assets/js/src/core/components/grid/grid.tsx
@@ -293,7 +293,7 @@ export const Grid = ({ enableMultipleRowSelection = false, modifiedCells = [], s
   ), [table, modifiedCells, data, columns, rowSelection, internalSorting])
 
   function getModifiedRow (rowIndex: string | number): GridProps['modifiedCells'] {
-    return memoModifiedCells.filter(({ rowIndex: rIndex }) => Number(rIndex) === Number(rowIndex)) ?? []
+    return memoModifiedCells.filter(({ rowIndex: rIndex }) => String(rIndex) === rowIndex) ?? []
   }
 
   function updateRowSelection (selectedRows: RowSelectionState): void {
