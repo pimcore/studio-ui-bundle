7bc60dcc873ad77d5f40c7d3e37cf6ace0d36bc3
xIrusux <christine.altmann@pimcore.com>
2024-09-04 10:42:12 +0200

    styling

--- assets/js/src/core/components/accordion/accordion.tsx
+++ assets/js/src/core/components/accordion/accordion.tsx
@@ -1,33 +1,31 @@
 /**
-* Pimcore
-*
-* This source file is available under two different licenses:
-* - Pimcore Open Core License (POCL)
-* - Pimcore Commercial License (PCL)
-* Full copyright and license information is available in
-* LICENSE.md which is distributed with this source code.
-*
-*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
-*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
-*/
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - Pimcore Open Core License (POCL)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+ */
 import React from "react";
-import {Collapse, CollapseProps, theme} from "antd";
+import {Collapse, CollapseProps} from "antd";
 import {useStyles} from "@Pimcore/components/accordion/accordion.styles";
 
-export interface AccordionProps extends CollapseProps{
+export interface AccordionProps extends CollapseProps {
 }
 
 export const Accordion = ({items}: AccordionProps): React.JSX.Element => {
-
-    const { styles } = useStyles()
-
+    const {styles} = useStyles()
 
     return (
-      <Collapse
-          className={[styles.accordion, 'accordion'].join(' ')}
-          items={items}
-          defaultActiveKey={['1']}
-          expandIconPosition = 'end'
-      />
-  )
+        <Collapse
+            className={[styles.accordion, 'accordion'].join(' ')}
+            items={items}
+            defaultActiveKey={['1']}
+            expandIconPosition='end'
+        />
+    )
 }
