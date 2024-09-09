5e78c8477de619114cdde7b91b4c2e1bbdf5ee16
xIrusux <christine.altmann@pimcore.com>
2024-09-09 21:46:33 +0200

    custom expand chevron to react to expand and contract

--- assets/js/src/core/components/accordion/accordion.tsx
+++ assets/js/src/core/components/accordion/accordion.tsx
@@ -37,43 +37,55 @@ export const Accordion = ({
   className,
   ...props
 }: AccordionProps): React.JSX.Element => {
-  const { styles } = useStyles()
-  const [isExpanded, setIsExpanded] = useState(false)
 
-  const onClickChevron = (): void => {
-    setIsExpanded(!isExpanded)
+  const { styles } = useStyles()
+  const [expandedIds, setExpandedIds] = useState<string[]>([])
+  const onClickChevron = (id: string): void => {
+    setExpandedIds((prevIds) =>
+      prevIds.includes(id)
+        ? prevIds.filter((expandedId) => expandedId !== id)
+        : [...prevIds, id]
+    )
   }
 
   // use whats given through collapsible and just use the background that expands
   // pass in via each item whether it is empty i.e. text that then transform into showDetails and put into conditional for button
-  const itemsWithCardClassName = items?.map((i) => ({
-    ...i,
-    className: [i?.className, 'card'].filter(Boolean).join(' '),
-    label: <>
-      <Flex
-        align={ 'center' }
-        justify={ 'center' }
-        vertical={ false }
-      >
-        {i.title}
-        {(i.children !== null) && (
-        <Button
-          aria-label={ i18n.t('aria.notes-and-events.expand') }
-          className={ 'card-title__chevron-btn' }
-          icon={ <Icon
-            className={ ['chevron', isExpanded ? 'chevron-up' : ''].join(' ') }
-            name={ 'chevron-up' }
-                 /> }
-          onClick={ onClickChevron }
-          role={ 'button' }
-          size="small"
-          type={ 'text' }
-        />
-        )}
-      </Flex>
-      {i.subtitle}
-    </>
-  })) ?? []
+  const itemsWithCardClassName = items?.map((item) => {
+    const chevronClassName = ['chevron', item.key != null && expandedIds.includes(String(item.key)) ? 'chevron-up' : ''].join(' ')
+
+    return ({
+      ...item,
+      className: [item?.className, 'card'].filter(Boolean).join(' '),
+      label: <>
+        <Flex
+          align={ 'center' }
+          justify={ 'center' }
+          vertical={ false }
+        >
+          {item.title}
+          {(item.children !== null) && (
+          <Button
+            aria-label={ i18n.t('aria.notes-and-events.expand') }
+            className={ 'card-title__chevron-btn' }
+            icon={ <Icon
+              className={ chevronClassName }
+              name={ 'chevron-up' }
+                   /> }
+            onClick={ () => {
+              if (item.id != null) {
+                onClickChevron(item.id)
+              }
+            } }
+            role={ 'button' }
+            size="small"
+            type={ 'text' }
+          />
+          )}
+        </Flex>
+        {item.subtitle}
+      </>
+    })
+  }) ?? []
 
   const allClassNames = [
     'accordion',
@@ -89,9 +101,11 @@ export const Accordion = ({
   return (
     <Collapse
       accordion={ exclusive }
+      activeKey={ expandedIds }
       bordered={ !spaced }
       className={ allClassNames.join(' ') }
       items={ itemsWithCardClassName }
+      onChange={ (keys) => { setExpandedIds(Array.isArray(keys) ? keys : [keys]) } }
       { ...props }
     />
   )
