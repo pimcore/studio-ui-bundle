0dab78f3b0a7175fce517584f6982f7ae2580df4
xIrusux <xIrusux@users.noreply.github.com>
2024-09-19 17:29:03 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/tag/tag.stories.tsx
+++ assets/js/src/core/components/tag/tag.stories.tsx
@@ -1,52 +1,52 @@
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - Pimcore Open Core License (POCL)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
- */
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
 
-import {type Meta} from '@storybook/react'
-import {Tag} from "@Pimcore/components/tag/tag";
+import { type Meta } from '@storybook/react'
+import { Tag } from '@Pimcore/components/tag/tag'
 
 const config: Meta = {
-    title: 'Components/General/Tag',
-    component: Tag,
-    tags: ['autodocs']
+  title: 'Components/General/Tag',
+  component: Tag,
+  tags: ['autodocs']
 }
 
 export default config
 
 export const _default = {
-    args: {
-        tagText: 'default'
-    }
+  args: {
+    tagText: 'default'
+  }
 }
 
 export const SuccessIconTag = {
-    args: {
-        iconName: 'world',
-        color: 'success',
-        tagText: "Published"
-    }
+  args: {
+    iconName: 'world',
+    color: 'success',
+    tagText: 'Published'
+  }
 }
 
 export const OwnDraftIconTag = {
-    args: {
-        iconName: 'user',
-        color: 'blue',
-        tagText: "Own draft"
-    }
+  args: {
+    iconName: 'user',
+    color: 'blue',
+    tagText: 'Own draft'
+  }
 }
 
 export const IDTag = {
-    args: {
-        theme: 'id',
-        tagText: "ID: 150"
-    }
+  args: {
+    theme: 'id',
+    tagText: 'ID: 150'
+  }
 }
