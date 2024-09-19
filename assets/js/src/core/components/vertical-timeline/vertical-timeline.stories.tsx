7ee881124b84985b0f379afeebbd81ee73b299c4
xIrusux <xIrusux@users.noreply.github.com>
2024-09-19 09:25:14 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/vertical-timeline/vertical-timeline.stories.tsx
+++ assets/js/src/core/components/vertical-timeline/vertical-timeline.stories.tsx
@@ -1,37 +1,37 @@
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - Pimcore Open Core License (POCL)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
- */
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
 
 import React from 'react'
-import {type Meta} from '@storybook/react'
-import {VerticalTimeline as VerticalTimelineComponent} from './vertical-timeline'
+import { type Meta } from '@storybook/react'
+import { VerticalTimeline as VerticalTimelineComponent } from './vertical-timeline'
 
 const config: Meta = {
-    title: 'Components/Data Display/VerticalTimeline',
-    component: VerticalTimelineComponent,
-    parameters: {
-        layout: 'centered'
-    },
-    tags: ['autodocs']
+  title: 'Components/Data Display/VerticalTimeline',
+  component: VerticalTimelineComponent,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
 }
 
 export default config
 
 export const _default = {
-    args: {
-        timeStamps: [
-            <div key={1}>Stamp 1</div>,
-            <div key={2}>Stamp 2</div>,
-            <div key={3}>Stamp 3</div>,
-        ]
-    }
+  args: {
+    timeStamps: [
+      <div key={ 1 }>Stamp 1</div>,
+      <div key={ 2 }>Stamp 2</div>,
+      <div key={ 3 }>Stamp 3</div>
+    ]
+  }
 }
