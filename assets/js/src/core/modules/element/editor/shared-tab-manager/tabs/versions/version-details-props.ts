9e60a43c98b09afaa65b8b0b17764079c0ced14d
markus-moser <markus-moser@users.noreply.github.com>
2024-09-26 16:41:42 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/element/editor/shared-tab-manager/tabs/versions/version-details-props.ts
+++ assets/js/src/core/modules/element/editor/shared-tab-manager/tabs/versions/version-details-props.ts
@@ -1,12 +1,25 @@
-import type {VersionIdentifiers} from "@Pimcore/modules/element/editor/shared-tab-manager/tabs/versions/versions-view";
-import type {Version} from "@Pimcore/modules/element/editor/version-api-slice.gen";
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import type { VersionIdentifiers } from '@Pimcore/modules/element/editor/shared-tab-manager/tabs/versions/versions-view'
+import type { Version } from '@Pimcore/modules/element/editor/version-api-slice.gen'
 
 export interface VersionComparisonViewProps {
-    versionIds: VersionIdentifiers[]
+  versionIds: VersionIdentifiers[]
 }
 
 export interface SingleVersionViewProps {
-    versions: Version[]
-    versionId: VersionIdentifiers
-    setDetailedVersions: (vIds: VersionIdentifiers[]) => void
+  versions: Version[]
+  versionId: VersionIdentifiers
+  setDetailedVersions: (vIds: VersionIdentifiers[]) => void
 }
