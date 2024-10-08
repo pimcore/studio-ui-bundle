371523a91048cc5229b05d0a5e67913ab446fdb5
sholzer <sholzer@users.noreply.github.com>
2024-10-08 15:15:15 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/card/card.stories.tsx
+++ assets/js/src/core/components/card/card.stories.tsx
@@ -12,12 +12,12 @@
 */
 
 import { type Meta } from '@storybook/react'
-import {Card} from "./card";
-import {Switch} from "../switch/switch";
-import React, {Fragment} from "react";
-import {IconButton} from "@Pimcore/components/icon-button/icon-button";
-import {IconTextButton} from "@Pimcore/components/icon-text-button/icon-text-button";
-import {Flex} from "antd";
+import { Card } from './card'
+import { Switch } from '../switch/switch'
+import React, { Fragment } from 'react'
+import { IconButton } from '@Pimcore/components/icon-button/icon-button'
+import { IconTextButton } from '@Pimcore/components/icon-text-button/icon-text-button'
+import { Flex } from 'antd'
 
 const config: Meta = {
   title: 'Components/Data Display/Card',
@@ -35,26 +35,29 @@ export const _default = {
 }
 
 export const HeadWithButtons = {
-    args: {
-        ..._default.args,
-        title: <Fragment>
-            Card Title
-            <IconTextButton icon="PlusCircleOutlined" type="default">Add</IconTextButton>
-        </Fragment>,
-        extra: [
-            {
-                icon: 'eye-outlined',
-                title: 'View',
-                type: 'default',
-                onClick: () => console.log('click view button')
-            }
-        ]
-    }
+  args: {
+    ..._default.args,
+    title: <Fragment>
+      Card Title
+      <IconTextButton
+        icon="PlusCircleOutlined"
+        type="default"
+      >Add</IconTextButton>
+    </Fragment>,
+    extra: [
+      {
+        icon: 'eye-outlined',
+        title: 'View',
+        type: 'default',
+        onClick: () => { console.log('click view button') }
+      }
+    ]
+  }
 }
 export const CloseButton = {
   args: {
     ..._default.args,
-    onClose: () => console.log('click close button'),
+    onClose: () => { console.log('click close button') },
     extra: ['20.02.2024 15:13:22']
   }
 }
@@ -69,42 +72,52 @@ export const HeadWithIcon = {
 export const ToggleButton = {
   args: {
     title: 'Manufacturer Uppercase',
-      extra: [<Switch key="toggle-switch" labelLeft={"Toggle"} onChange={() => console.log('change')}></Switch>]
+    extra: [<Switch
+      key="toggle-switch"
+      labelLeft={ 'Toggle' }
+      onChange={ () => { console.log('change') } }
+            ></Switch>]
   }
 }
 
 export const Image = {
   args: {
     image: {
-        src: 'https://pimcore.com/brand/Website-Banners/image-thumb__23862__header-sujet-img__2019--slider/2024-Pimcore-Home-Main.webp',
-        alt: 'Pimcore Logo'
+      src: 'https://pimcore.com/brand/Website-Banners/image-thumb__23862__header-sujet-img__2019--slider/2024-Pimcore-Home-Main.webp',
+      alt: 'Pimcore Logo'
     },
     children:
-        <Fragment>
-            <Flex align="center" justify="space-between">
-                <div>Lorem ipsum dolor</div>
-                <IconButton icon={'eye-outlined'} onClick={() => console.log('click button')} />
-            </Flex>
-        </Fragment>
+  <Fragment>
+    <Flex
+      align="center"
+      justify="space-between"
+    >
+      <div>Lorem ipsum dolor</div>
+      <IconButton
+        icon={ 'eye-outlined' }
+        onClick={ () => { console.log('click button') } }
+      />
+    </Flex>
+  </Fragment>
   }
 }
 
 export const Actions = {
   args: {
     ..._default.args,
-      actions: [
-          <IconButton
-              icon="trash"
-              key={ 'icon-button-01' }
-              type='link'
-              onClick={ () => console.log('click trash button') }
-          />,
-          <IconButton
-              icon="refresh"
-              key={ 'icon-button-02' }
-              type='link'
-                onClick={ () => console.log('click refresh button') }
-          />
-      ]
+    actions: [
+      <IconButton
+        icon="trash"
+        key={ 'icon-button-01' }
+        onClick={ () => { console.log('click trash button') } }
+        type='link'
+      />,
+      <IconButton
+        icon="refresh"
+        key={ 'icon-button-02' }
+        onClick={ () => { console.log('click refresh button') } }
+        type='link'
+      />
+    ]
   }
 }
