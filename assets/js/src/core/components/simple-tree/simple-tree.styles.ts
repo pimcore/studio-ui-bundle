1f5a8bab45bc24a1f61b96e5e9c9312457463c6f
sholzer <sholzer@users.noreply.github.com>
2024-10-14 05:40:12 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/simple-tree/simple-tree.styles.ts
+++ assets/js/src/core/components/simple-tree/simple-tree.styles.ts
@@ -1,21 +1,21 @@
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - Pimcore Open Core License (POCL)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
- */
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
 
 import { createStyles } from 'antd-style'
 
 export const useStyles = createStyles(({ token, css }) => {
-    return {
-        tree: css`
+  return {
+    tree: css`
             .ant-tree-treenode {
                 padding: 2px ${token.paddingSM}px 2px 0;
                 position: relative;
@@ -95,5 +95,5 @@ export const useStyles = createStyles(({ token, css }) => {
                 pointer-events: none;
             }
         `
-    }
+  }
 }, { hashPriority: 'high' })
--- assets/js/src/core/components/simple-tree/simple-tree.tsx
+++ assets/js/src/core/components/simple-tree/simple-tree.tsx
@@ -1,75 +1,75 @@
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - Pimcore Open Core License (POCL)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
- */
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
 
-import React from 'react';
-import { Tree, type TreeDataNode } from 'antd';
-import { Icon } from '@Pimcore/components/icon/icon';
-import { SimpleTreeItem } from './simple-tree-item';
-import { useStyles } from './simple-tree.styles';
+import React from 'react'
+import { Tree, type TreeDataNode } from 'antd'
+import { Icon } from '@Pimcore/components/icon/icon'
+import { SimpleTreeItem } from './simple-tree-item'
+import { useStyles } from './simple-tree.styles'
 
 interface ExtendedTreeDataNode extends TreeDataNode {
-    actions?: Array<{ key: string; icon: string }>;
+  actions?: Array<{ key: string, icon: string }>
 }
 interface SimpleTreeProps {
-    treeData: ExtendedTreeDataNode[];
-    className?: string;
-    searchable?: boolean;
-    onCheck?: (checkedKeys: any) => void;
-    onContextMenuClick?: (key: any, action: string) => void;
-    onDragAndDrop?: (params: { node: ExtendedTreeDataNode; dragNode: ExtendedTreeDataNode; dropPosition: number }) => void;
-    onSelected?: (key: any) => void;
+  treeData: ExtendedTreeDataNode[]
+  className?: string
+  searchable?: boolean
+  onCheck?: (checkedKeys: any) => void
+  onContextMenuClick?: (key: any, action: string) => void
+  onDragAndDrop?: (params: { node: ExtendedTreeDataNode, dragNode: ExtendedTreeDataNode, dropPosition: number }) => void
+  onSelected?: (key: any) => void
 }
 
 const SimpleTree = ({ className, ...props }: SimpleTreeProps): React.JSX.Element => {
-    const { styles } = useStyles();
-    const classNames = [styles.tree, className];
-    const [selectedKeys, setSelectedKeys] = React.useState<any[]>([]);
+  const { styles } = useStyles()
+  const classNames = [styles.tree, className]
+  const [selectedKeys, setSelectedKeys] = React.useState<any[]>([])
 
-    if (props.searchable != null && props.searchable) {
-        // Handle searchable logic here
-    }
+  if (props.searchable != null && props.searchable) {
+    // Handle searchable logic here
+  }
 
-    return (
-        <Tree
-            blockNode
-            className={classNames.join(' ')}
-            checkable={props.onCheck !== undefined}
-            onCheck={(checkedKeys): void => props.onCheck?.(checkedKeys)}
-            defaultExpandAll
-            draggable
-            onDrop={(evt): void => {
-                props.onDragAndDrop?.({
-                    node: evt.node as ExtendedTreeDataNode,
-                    dragNode: evt.dragNode as ExtendedTreeDataNode,
-                    dropPosition: evt.dropPosition,
-                });
-            }}
-            selectedKeys={selectedKeys}
-            showIcon
-            switcherIcon={<Icon name={'chevron-down-small'} />}
-            titleRender={(node) => (
-                <SimpleTreeItem
-                    onSelected={() => {
-                        setSelectedKeys([node.key]);
-                        props.onSelected?.(node.key);
-                    }}
-                    title={node.title as string}
-                    actions={node.actions}
-                    onContextMenuClick={(action) => props.onContextMenuClick?.(node.key, action)}
-                />
-            )}
-            treeData={props.treeData}
+  return (
+    <Tree
+      blockNode
+      checkable={ props.onCheck !== undefined }
+      className={ classNames.join(' ') }
+      defaultExpandAll
+      draggable
+      onCheck={ (checkedKeys): void => props.onCheck?.(checkedKeys) }
+      onDrop={ (evt): void => {
+        props.onDragAndDrop?.({
+          node: evt.node as ExtendedTreeDataNode,
+          dragNode: evt.dragNode as ExtendedTreeDataNode,
+          dropPosition: evt.dropPosition
+        })
+      } }
+      selectedKeys={ selectedKeys }
+      showIcon
+      switcherIcon={ <Icon name={ 'chevron-down-small' } /> }
+      titleRender={ (node) => (
+        <SimpleTreeItem
+          actions={ node.actions }
+          onContextMenuClick={ (action) => props.onContextMenuClick?.(node.key, action) }
+          onSelected={ () => {
+            setSelectedKeys([node.key])
+            props.onSelected?.(node.key)
+          } }
+          title={ node.title as string }
         />
-    );
-};
+      ) }
+      treeData={ props.treeData }
+    />
+  )
+}
 export { SimpleTree }
