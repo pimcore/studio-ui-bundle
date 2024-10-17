44c27c7e2542e44ab7bbf4c83bb8f4096ac06b73
xIrusux <christine.altmann@pimcore.com>
2024-10-17 15:58:32 +0200

    create pimcore badge

---
+++ assets/js/src/core/components/badge/badge.tsx
@@ -0,0 +1,26 @@
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - Pimcore Open Core License (POCL)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+ */
+
+import React from 'react'
+import { Badge as AntBadge, BadgeProps } from 'antd'
+
+export const Badge = ({ color, children, ...props }: BadgeProps): React.JSX.Element => {
+
+    return (
+        <AntBadge
+            color={ color }
+            styles={{ indicator: { outline: `1px solid ${color}` }, root: {marginRight: '5px'}}}
+            { ...props }
+        />
+    )
+}
