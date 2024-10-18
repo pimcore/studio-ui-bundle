1f21a8b55a56d9174acdcaee4643898d71b53501
Tine <42359615+xIrusux@users.noreply.github.com>
2024-10-18 14:16:03 +0200

    Workflow tags (#621)
    * render badge tags
    
    * create pimcore badge
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    * embed new badge
    
    * remove hug icon and use workflow instead
    
    * style toolbar add dropdown button
    
    * create toolbar dropdown for next actions per workflow
    
    * fix second dropdown menu items
    
    * loading state
    
    * Automatic frontend build
    
    * reformat code
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: xIrusux <xIrusux@users.noreply.github.com>

---
+++ assets/js/src/core/modules/asset/editor/toolbar/workflow-menu/workflow-menu.tsx
@@ -0,0 +1,110 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import React, { useEffect } from 'react'
+import { TagList } from '@Pimcore/components/tag-list/tag-list'
+import {
+  useWorkflowGetDetailsQuery
+} from '@Pimcore/modules/element/editor/shared-tab-manager/tabs/workflow/workflow-api-slice-enhanced'
+import { useElementContext } from '@Pimcore/modules/element/hooks/use-element-context'
+import type { TagProps } from '@Pimcore/components/tag/tag'
+import { Badge } from '@Pimcore/components/badge/badge'
+import { Flex } from '@Pimcore/components/flex/flex'
+import { Dropdown, type DropdownMenuProps, type ItemType } from '@Pimcore/components/dropdown/dropdown'
+import { DropdownButton } from '@Pimcore/components/dropdown-button/dropdown-button'
+import { Icon } from '@Pimcore/components/icon/icon'
+import { useTranslation } from 'react-i18next'
+
+export const EditorToolbarWorkflowMenu = (): React.JSX.Element => {
+  const { t } = useTranslation()
+  const { id, elementType } = useElementContext()
+  const { data, isLoading } = useWorkflowGetDetailsQuery({ elementType, elementId: id })
+  const [items, setItems] = React.useState<DropdownMenuProps['items']>([])
+
+  useEffect(() => {
+    if (data?.items !== undefined && data.items.length > 0) {
+      const workFlowItems = data.items.map((workflow) => {
+        const result: ItemType[] = []
+        const mergedActions = [
+          ...(workflow.allowedTransitions ?? []),
+          ...(workflow.globalActions ?? [])
+        ]
+
+        mergedActions?.forEach((action) => {
+          result.push({
+            key: (result.length + 1).toString(),
+            label: t(`${action.label}`)
+          })
+        })
+
+        return {
+          key: t(`${workflow.workflowName}`),
+          type: 'group',
+          label: t(`${workflow.workflowName}`).toUpperCase(),
+          children: result
+        }
+      })
+      setItems(workFlowItems)
+    }
+  }, [data])
+
+  const getVisibleWorkflowStatus = (): TagProps[][] => {
+    if (data?.items !== undefined && data.items.length > 0) {
+      const formattedStatuses = data.items.reduce((result: Array<{ children: string }>, workflow) => {
+        workflow.workflowStatus.forEach((status) => {
+          if (status.visibleInDetail !== undefined && status.visibleInDetail) {
+            const style = status.colorInverted
+              ? { backgroundColor: `${status.color}33` }
+              : {}
+            const tag =
+                            {
+                              children: t(`${status.label}`),
+                              icon: <Badge
+                                color={ status.color }
+                                    />,
+                              style
+                            }
+            result.push(tag)
+          }
+        })
+        return result
+      }, [])
+      return [formattedStatuses]
+    }
+    return [[]]
+  }
+
+  return (
+    <Flex
+      align={ 'center' }
+      justify={ 'space-between' }
+    >
+      {!isLoading && (
+        <TagList
+          itemGap={ 'extra-small' }
+          list={ getVisibleWorkflowStatus() }
+        />
+      )}
+      <Dropdown
+        menu={ { items } }
+      >
+        <DropdownButton
+          disabled={ isLoading }
+          loading={ isLoading }
+        >
+          <Icon name={ 'workflow' } />
+        </DropdownButton>
+      </Dropdown>
+    </Flex>
+  )
+}
