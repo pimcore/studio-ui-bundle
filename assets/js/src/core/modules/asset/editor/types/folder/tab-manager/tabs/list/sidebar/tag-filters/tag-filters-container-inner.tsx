2eb88cadc913c3cd51b0a7c44c9d449dd1700f76
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2024-10-23 17:37:26 +0200

    updated the  tags-tree-filters-container

--- assets/js/src/core/modules/asset/editor/types/folder/tab-manager/tabs/list/sidebar/tag-filters/tag-filters-container-inner.tsx
+++ assets/js/src/core/modules/asset/editor/types/folder/tab-manager/tabs/list/sidebar/tag-filters/tag-filters-container-inner.tsx
@@ -11,8 +11,8 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
-import React from 'react'
-import { TagsTreeContainer } from '@Pimcore/modules/element/editor/shared-tab-manager/tabs/tags/components/tags-tree/tags-tree-container'
+import React, { useState } from 'react'
+import { TagsTreeFiltersContainer } from './tags-tree-filters-container'
 import { Title } from '@Pimcore/components/title/title'
 import { IconTextButton } from '@Pimcore/components/icon-text-button/icon-text-button'
 import { Button } from '@Pimcore/components/button/button'
@@ -21,23 +21,20 @@ import {
 } from '@Pimcore/components/content-toolbar-sidebar-layout/content-toolbar-sidebar-layout'
 import { Toolbar } from '@Pimcore/components/toolbar/toolbar'
 import { Content } from '@Pimcore/components/content/content'
-import { useTagGetCollectionForElementByTypeAndIdQuery } from '@Pimcore/modules/element/editor/shared-tab-manager/tabs/tags/tags-api-slice.gen'
-import { useElementContext } from '@Pimcore/modules/element/hooks/use-element-context'
 import { useTagFilters } from './hooks/use-tag-filters'
 import { useListFilterOptions } from '@Pimcore/modules/asset/editor/types/folder/tab-manager/tabs/list/hooks/use-list'
 
 export const TagFiltersContainerInner = (): React.JSX.Element => {
-  const { id, elementType } = useElementContext()
-
-  const { data, isLoading } = useTagGetCollectionForElementByTypeAndIdQuery({
-    elementType,
-    id
-  })
+  const [checkedTagList, setCheckedTagList] = useState<string[]>([])
 
   const { setFilterOptions } = useListFilterOptions()
   const { resetFilters, filterOptions } = useTagFilters()
 
-  const handleApplyClick = (): void => { setFilterOptions(filterOptions) }
+  const handleApplyClick = (): void => {
+    console.log('------ checkedTagList: ', checkedTagList)
+
+    setFilterOptions(filterOptions)
+  }
 
   const handleResetAllFiltersClick = (): void => { resetFilters() }
 
@@ -68,10 +65,7 @@ export const TagFiltersContainerInner = (): React.JSX.Element => {
       >
         <Title>Tag Filters</Title>
 
-        <TagsTreeContainer
-          isLoading={ isLoading }
-          tags={ data?.items ?? [] }
-        />
+        <TagsTreeFiltersContainer setCheckedTagList={ setCheckedTagList } />
       </Content>
 
     </ContentToolbarSidebarLayout>
---
+++ assets/js/src/core/modules/asset/editor/types/folder/tab-manager/tabs/list/sidebar/tag-filters/tags-tree-filters-container.tsx
@@ -0,0 +1,62 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import { useTagGetCollectionQuery } from '@Pimcore/modules/element/editor/shared-tab-manager/tabs/tags/tags-api-slice.gen'
+import { Tree } from 'antd'
+import React, { type Key } from 'react'
+import { Content } from '@Pimcore/components/content/content'
+import { Flex } from '@Pimcore/components/flex/flex'
+import {
+  useCreateTreeStructure
+} from '@Pimcore/modules/element/editor/shared-tab-manager/tabs/tags/components/tags-tree/hooks/use-create-tree-structure'
+
+export const TagsTreeFiltersContainer = ({ setCheckedTagList }: { setCheckedTagList: any }): React.JSX.Element => {
+  const { data: tags, isLoading: tagsLoading } = useTagGetCollectionQuery({
+    page: 1,
+    pageSize: 9999
+  })
+
+  const { createTreeStructure } = useCreateTreeStructure()
+
+  if (tagsLoading) {
+    return <Content loading />
+  }
+
+  if (tags?.items === undefined) {
+    return <div>Failed to load tags</div>
+  }
+
+  const treeData = createTreeStructure({ tags: tags.items })
+
+  const handleCheck = (checkedKeys: { checked: Key[], halfChecked: Key[] }): void => {
+    setCheckedTagList(checkedKeys.checked)
+  }
+
+  return (
+    <>
+      <Flex
+        gap={ 'small' }
+        vertical
+      >
+        <Tree
+          checkStrictly
+          checkable
+          defaultExpandedKeys={ ['root'] }
+          onCheck={ handleCheck }
+          showIcon
+          treeData={ treeData }
+        />
+      </Flex>
+    </>
+  )
+}
