83ecf9115a407d3b822d5f871c2907f926fe3ce1
markus-moser <markus-moser@users.noreply.github.com>
2024-11-11 09:26:10 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/slider/slider.stories.tsx
+++ assets/js/src/core/components/slider/slider.stories.tsx
@@ -1,23 +1,36 @@
-import { Meta } from '@storybook/react'
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import { type Meta } from '@storybook/react'
 import { Slider } from './slider'
 
-const config: Meta =  {
-    title: 'Components/Data Entry/Slider',
-    component: Slider,
-    argTypes: {
-        value: { control: 'array' },
-        onChange: { action: 'changed' }
-    }
+const config: Meta = {
+  title: 'Components/Data Entry/Slider',
+  component: Slider,
+  argTypes: {
+    value: { control: 'array' },
+    onChange: { action: 'changed' }
+  }
 }
 
 export default config
 
 export const _default = {
-    args: {
-        value: 35,
-        min: 0,
-        max: 100,
-        allowClear: true,
-        showValue: true,
-    }
+  args: {
+    value: 35,
+    min: 0,
+    max: 100,
+    allowClear: true,
+    showValue: true
+  }
 }
