e6e7a946880cdbac6b813d5369f263fe763eeb84
vin0401 <marvin.bertram@pimcore.com>
2024-11-20 09:07:20 +0100

    Improve render performance of widget content

--- assets/js/src/core/utils/hooks/use-css-container/use-css-container.tsx
+++ assets/js/src/core/utils/hooks/use-css-container/use-css-container.tsx
@@ -11,6 +11,7 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
+import { useMemo } from 'react'
 import { useCssContainerStyles } from './use-css-container.styles'
 
 export interface UseCssContainerProps {
@@ -24,8 +25,11 @@ export interface UseCssContainerReturn {
 
 export const useCssContainer = ({ name, type = 'size' }: UseCssContainerProps): UseCssContainerReturn => {
   const styleDefinition = useCssContainerStyles({ name, type })
+  console.log({ styleDefinition })
 
-  return {
-    styleDefinition
-  }
+  return useMemo(() => {
+    return {
+      styleDefinition
+    }
+  }, [])
 }
