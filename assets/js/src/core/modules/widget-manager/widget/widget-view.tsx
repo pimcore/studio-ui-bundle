60c7783e7c34056139be94c1e6b3864d0dc56d9c
Corepex <christian.feldkirchner@pimcore.com>
2024-11-21 14:16:48 +0100

    fixed tree icon

--- assets/js/src/core/modules/widget-manager/widget/widget-view.tsx
+++ assets/js/src/core/modules/widget-manager/widget/widget-view.tsx
@@ -16,11 +16,12 @@ import { TitleView } from './title/title-view'
 import { useStyles } from './widget-view.styles'
 import { useTranslation } from 'react-i18next'
 import { useCssContainer, type UseCssContainerProps } from '@Pimcore/utils/hooks/use-css-container/use-css-container'
+import { type ElementIcon } from '@Pimcore/modules/asset/asset-api-slice.gen'
 
 interface WidgetViewProps {
   title: string
   showTitle?: boolean
-  icon: string
+  icon: ElementIcon
   children: React.ReactNode
 }
 
