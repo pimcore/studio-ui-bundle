4d8a0c8519e056d18d6cd99c101c7077b93fe0d2
Corepex <christian.feldkirchner@pimcore.com>
2024-11-22 13:03:10 +0100

    fixed open issue when clicking dropdown option

--- assets/js/src/core/modules/asset/editor/types/folder/tab-manager/tabs/preview/flex-container.tsx
+++ assets/js/src/core/modules/asset/editor/types/folder/tab-manager/tabs/preview/flex-container.tsx
@@ -21,6 +21,9 @@ import { useAssetHelper } from '@Pimcore/modules/asset/hooks/use-asset-helper'
 import { type AssetGetTreeApiResponse } from '@Pimcore/modules/asset/asset-api-slice-enhanced'
 import { type DropdownProps } from '@Pimcore/components/dropdown/dropdown'
 import { Icon } from '@Pimcore/components/icon/icon'
+import { useRename } from '@Pimcore/modules/element/actions/rename/use-rename'
+import { type TreeNodeProps } from '@Pimcore/components/element-tree/node/tree-node'
+import { useDelete } from '@Pimcore/modules/element/actions/delete/use-delete'
 
 interface FlexContainerProps {
   assets: AssetGetTreeApiResponse
@@ -30,34 +33,8 @@ const FlexContainer = (props: FlexContainerProps): React.JSX.Element => {
   const { assets } = props
   const { t } = useTranslation()
   const { openAsset } = useAssetHelper()
-
-  const dropdownItems: DropdownProps['menu']['items'] = [
-    {
-      key: 'locate-in-tree',
-      icon: <Icon value="target" />,
-      label: t('preview-card.locate-in-tree')
-    },
-    {
-      key: 'info',
-      icon: <Icon value="info-circle-outlined" />,
-      label: t('info')
-    },
-    {
-      key: 'rename',
-      icon: <Icon value="rich-edit" />,
-      label: t('preview-card.rename')
-    },
-    {
-      key: 'download-zip',
-      icon: <Icon value="download-02" />,
-      label: t('preview-card.download-zip')
-    },
-    {
-      key: 'delete',
-      icon: <Icon value="trash" />,
-      label: t('preview-card.delete')
-    }
-  ]
+  const { renameContextMenuItem } = useRename('asset')
+  const { deleteContextMenuItem } = useDelete('asset')
 
   const cards: ReactNode[] = []
   assets.items.forEach((asset) => {
@@ -68,6 +45,26 @@ const FlexContainer = (props: FlexContainerProps): React.JSX.Element => {
         }
       })
     }
+
+    const dropdownItems: DropdownProps['menu']['items'] = [
+      {
+        key: 'locate-in-tree',
+        icon: <Icon value="target" />,
+        label: t('preview-card.locate-in-tree')
+      },
+      {
+        key: 'info',
+        icon: <Icon value="info-circle-outlined" />,
+        label: t('info')
+      },
+      renameContextMenuItem(asset as any as TreeNodeProps),
+      {
+        key: 'download-zip',
+        icon: <Icon value="download-02" />,
+        label: t('preview-card.download-zip')
+      },
+      deleteContextMenuItem(asset as any as TreeNodeProps)
+    ]
 
     if ('imageThumbnailPath' in asset && asset.imageThumbnailPath !== undefined && asset.imageThumbnailPath !== null) {
       cards.push(
