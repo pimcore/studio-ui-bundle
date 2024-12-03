0e4ab00333abe28ed7cfb4652b23f02259c9560a
xIrusux <christine.altmann@pimcore.com>
2024-12-03 16:05:58 +0100

    update tags as it seems like we dont need to be caring for tag ids in rtk tags

--- assets/js/src/core/modules/element/editor/shared-tab-manager/tabs/tags/tags-api-slice-enhanced.ts
+++ assets/js/src/core/modules/element/editor/shared-tab-manager/tabs/tags/tags-api-slice-enhanced.ts
@@ -33,28 +33,17 @@ export const api = baseApi.enhanceEndpoints({
       }
     },
     tagAssignToElement: {
-      invalidatesTags: (result, error, args) => invalidatingTags.ELEMENT_TAGS_ID(args.id)
+      invalidatesTags: (result, error, args) => invalidatingTags.ELEMENT_TAGS(args.elementType, args.id)
     },
     tagUnassignFromElement: {
-      invalidatesTags: (result, error, args) => invalidatingTags.ELEMENT_TAGS_ID(args.id)
+      invalidatesTags: (result, error, args) => invalidatingTags.ELEMENT_TAGS(args.elementType, args.id)
     },
     tagBatchOperationToElementsByTypeAndId: {
       // when tags from folder are applied to children assets or data objects
       invalidatesTags: (result, error, args) => invalidatingTags.ELEMENT_TAGS(args.elementType, args.id)
     },
     tagGetCollectionForElementByTypeAndId: {
-      providesTags: (result, error, args) => {
-        const tags: Tag[] = []
-        // all tags that are applied to an element
-        const items = Array.isArray(result?.items) ? result.items : []
-        items.forEach((assignedTag) => {
-          if (assignedTag.id !== undefined) {
-            tags.push(...providingTags.ELEMENT_TAGS(args.elementType, assignedTag.id))
-          }
-        })
-
-        return tags
-      }
+      providesTags: (result, error, args) => providingTags.ELEMENT_TAGS(args.elementType, args.id)
     }
   }
 })
