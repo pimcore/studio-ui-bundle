92945ea13adcc1287227f6e5e07184d3ea33a675
Corepex <christian.feldkirchner@pimcore.com>
2024-12-09 16:34:47 +0100

    added manual cache update

--- assets/js/src/core/modules/element/hooks/use-element-api.ts
+++ assets/js/src/core/modules/element/hooks/use-element-api.ts
@@ -11,9 +11,10 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
-import { useAssetPatchByIdMutation } from '@Pimcore/modules/asset/asset-api-slice-enhanced'
-import { useDataObjectPatchByIdMutation } from '@Pimcore/modules/data-object/data-object-api-slice-enhanced'
+import { api as assetApi, type Asset, useAssetPatchByIdMutation } from '@Pimcore/modules/asset/asset-api-slice-enhanced'
+import { api as dataObjectApi, useDataObjectPatchByIdMutation } from '@Pimcore/modules/data-object/data-object-api-slice-enhanced'
 import { type ElementType } from 'types/element-type.d'
+import { store, useAppDispatch } from '@Pimcore/app/store'
 
 /**
  * Abstracts the logic for some basic API calls across element types (assets, data objects, documents)
@@ -37,12 +38,40 @@ interface UseElementApiReturn {
 export const useElementApi = (elementType: ElementType): UseElementApiReturn => {
   const [assetPatch] = useAssetPatchByIdMutation()
   const [dataObjectPatch] = useDataObjectPatchByIdMutation()
+  const dispatch = useAppDispatch()
 
   const elementPatch = async (args: ElementPatchArgs): Promise<void> => {
     if (elementType === 'asset') {
       await assetPatch(args)
+
+      /* eslint-disable */
+      const entries = assetApi.util.selectInvalidatedBy(
+        store.getState() as any,
+        ['ASSET_TREE']
+      )
+
+      entries.forEach((queryKeys) => {
+        console.log(queryKeys)
+
+        dispatch(assetApi.util.updateQueryData(
+          // @ts-ignore typescript, constants, u know? - i dont - @daniel
+          queryKeys.endpointName,
+          queryKeys.originalArgs,
+          (draft) => {
+            if('items' in draft && typeof draft.items === 'object') {
+              const entries = draft.items as Array<Asset>
+              const indexToUpdate = entries.findIndex((entry) => entry.id === args.body.data[0].id)
+
+              if(indexToUpdate !== -1) {
+                draft.items![indexToUpdate].filename = args.body.data[0].key as string
+              }
+            }
+          }
+        ))
+      })
     } else if (elementType === 'data-object') {
       await dataObjectPatch(args)
+      console.log(dataObjectApi)
     }
   }
 
