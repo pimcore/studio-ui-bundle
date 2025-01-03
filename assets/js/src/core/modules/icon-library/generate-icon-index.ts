ad78e613b7f3435d246309e027fb1142aeeceaa3
xIrusux <christine.altmann@pimcore.com>
2025-01-03 11:34:44 +0100

    adjust to make lint tests pass

--- assets/js/src/core/modules/icon-library/generate-icon-index.ts
+++ assets/js/src/core/modules/icon-library/generate-icon-index.ts
@@ -12,25 +12,26 @@
  *
  * HOW TO USE:
  * 1. Place all the SVG files in the folder `./js/src/core/assets/icons`
- * 2. Please remove any duplicates and add SVG files that use a protected word to the `protectedNames` array, the script will append 'Icon' to the variable name.
+ * 2. Please remove any duplicates and add SVG file names that use a protected word to the `protectedNames` array,
+ * the script will append 'Icon' to the variable name.
  * 3. Run the script using `npm run generate-icons`
- * 4. The script will generate the index file at `./js/src/core/modules/icon-library/index.ts`
- * 5. The script will also update the SVG files to use `currentColor` for the stroke and fill color.
+ * 4. The script will generate/replace the index file at `./js/src/core/modules/icon-library/index.ts`
+ * 5. The script will also update the SVG files to use `currentColor` as the stroke color.
  */
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const SVG_FOLDER = path.resolve('./js/src/core/assets/icons');
 const OUTPUT_FILE = path.resolve('./js/src/core/modules/icon-library/index.ts');
 const protectedNames = new Set(['new', 'package', 'import', 'export']);
 
-if (!fs.existsSync(SVG_FOLDER)) {
+if (!fs.existsSync(SVG_FOLDER as string)) {
     console.error(`Error: Directory ${SVG_FOLDER} does not exist.`);
     process.exit(1);
 }
 
-const files = fs.readdirSync(SVG_FOLDER).filter(file => file.endsWith('.inline.svg'));
+const files: string[] = fs.readdirSync(SVG_FOLDER as string).filter(file => file.endsWith('.inline.svg'));
 
 if (files.length === 0) {
     console.log(`No SVG files found in ${SVG_FOLDER}`);
@@ -62,7 +63,7 @@ const generateIconEntry = (fileName: string): string => {
 };
 
 const modifySvgStroke = (filePath: string): void => {
-    let svgContent = fs.readFileSync(filePath, 'utf-8');
+    let svgContent: string = fs.readFileSync(filePath, 'utf-8');
 
     svgContent = svgContent.replace(/stroke="[^"]*"/g, 'stroke="currentColor"');
     fs.writeFileSync(filePath, svgContent, 'utf-8');
@@ -102,11 +103,11 @@ import { serviceIds } from '@Pimcore/app/config/services/service-ids';
 import { type IconLibrary } from './services/icon-library';
 `;
 
-files.forEach(file => {
-    const filePath = path.join(SVG_FOLDER, file);
+files.forEach((file: string) => {
+    const filePath: string = path.join(SVG_FOLDER as string, file);
     modifySvgStroke(filePath);
 
-    const variableName = generateVariableName(file);
+    const variableName: string = generateVariableName(file);
     content += `
 import ${variableName} from '@Pimcore/assets/icons/${file}';`;
 });
