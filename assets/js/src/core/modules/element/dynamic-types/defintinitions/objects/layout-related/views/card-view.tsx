7ba70811d2c3103ca6bcc0ffd0f74fc9d701702e
vin0401 <marvin.bertram@pimcore.com>
2025-01-06 16:25:14 +0100

    Implement card based blocks and field collections

--- assets/js/src/core/modules/element/dynamic-types/defintinitions/objects/layout-related/views/card-view.tsx
+++ assets/js/src/core/modules/element/dynamic-types/defintinitions/objects/layout-related/views/card-view.tsx
@@ -11,21 +11,25 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
-import React from 'react'
+import React, { type ReactNode } from 'react'
 import { isEmpty } from 'lodash'
 import { Card } from '@Pimcore/components/card/card'
+import { type BoxProps } from '@Pimcore/components/box/box'
 
 export interface CardViewProps {
-  title?: string
+  title?: ReactNode
   children?: React.ReactNode
+  collapsible?: false
   bordered?: boolean
-  theme?: 'fieldset' | 'card-with-highlight'
+  theme?: 'fieldset' | 'card-with-highlight' | 'default'
+  contentPadding?: BoxProps['padding']
 }
 
 export const CardView = (props: CardViewProps): React.JSX.Element => {
   return (
     <Card
       bordered={ props.bordered === true }
+      contentPadding={ props.contentPadding }
       theme={ props.theme }
       title={ isEmpty(props.title) ? undefined : props.title }
     >
