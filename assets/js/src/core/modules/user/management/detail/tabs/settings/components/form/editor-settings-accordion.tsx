3551eeecda178b545f99761bc51963b19c42994f
Sarah Holzer <sarah.holzer@valantic.at>
2025-01-07 11:28:27 +0100

    User config (#688)

---
+++ assets/js/src/core/modules/user/management/detail/tabs/settings/components/form/editor-settings-accordion.tsx
@@ -0,0 +1,48 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import React from 'react'
+import { Accordion } from '@Pimcore/components/accordion/accordion'
+import { useTranslation } from 'react-i18next'
+import { LanguageTable } from '@Pimcore/modules/user/management/detail/tabs/settings/components/table/language-table'
+
+interface IEditorSettingsAccordionProps {
+  data: any
+  onChange: (data: any) => void
+}
+const EditorSettingsAccordion = ({ data, onChange, ...props }: IEditorSettingsAccordionProps): React.JSX.Element => {
+  const { t } = useTranslation()
+
+  const content = [
+    {
+      key: '1',
+      title: <>{ t('user-management.editor-settings') }</>,
+      children: (
+        <LanguageTable
+          data={ data }
+          onChangeOrder={ (languages) => { onChange(languages) } }
+        />
+      )
+    }
+  ]
+  return (
+    <Accordion
+      activeKey={ '1' }
+      bordered
+      items={ content }
+      size={ 'small' }
+      table
+    />
+  )
+}
+export { EditorSettingsAccordion }
