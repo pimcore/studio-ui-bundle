63925c3f9122eec6eb9f25321f4c6372bae72bf8
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-01-08 16:23:20 +0100

    updated the AlertModal stories

--- assets/js/src/core/components/modal/alert-modal/alert-modal.stories.tsx
+++ assets/js/src/core/components/modal/alert-modal/alert-modal.stories.tsx
@@ -11,40 +11,42 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
+import React from 'react'
 import type { Meta } from '@storybook/react'
 import { useAlertModal } from '@Pimcore/components/modal/alert-modal/hooks/use-alert-modal'
 import { Button } from '@Pimcore/components/button/button'
-import React from 'react'
+
+const AlertModalComponent = (args: { type: string, content: string }): React.JSX.Element => {
+  const modal = useAlertModal()
+
+  const callbackManager = (): void => {
+    switch (args.type) {
+      case 'info':
+        modal.info({
+          content: args.content
+        })
+        break
+      case 'error':
+        modal.error({
+          content: args.content
+        })
+        break
+      case 'warn':
+        modal.warn({
+          content: args.content
+        })
+        break
+    }
+  }
+
+  return (
+    <Button onClick={ callbackManager }>Open modal</Button>
+  )
+}
 
 const config: Meta = {
   title: 'Components/Feedback/AlertModal',
-  component: (args) => {
-    const modal = useAlertModal()
-
-    const callbackManager = (): void => {
-      switch (args.type) {
-        case 'info':
-          modal.info({
-            content: args.content
-          })
-          break
-        case 'error':
-          modal.error({
-            content: args.content
-          })
-          break
-        case 'warn':
-          modal.warn({
-            content: args.content
-          })
-          break
-      }
-    }
-
-    return (
-      <Button onClick={ callbackManager }>Open modal</Button>
-    )
-  },
+  component: AlertModalComponent,
   parameters: {
     layout: 'centered'
   },
