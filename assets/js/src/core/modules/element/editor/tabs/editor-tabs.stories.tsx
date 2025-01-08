780bf44954e1a0ca8305dc6093e955f74d58cb85
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-01-08 16:55:34 +0100

    updated the EditorTab stories

--- assets/js/src/core/modules/element/editor/tabs/editor-tabs.stories.tsx
+++ assets/js/src/core/modules/element/editor/tabs/editor-tabs.stories.tsx
@@ -25,6 +25,34 @@ interface IEditorTabsStory extends IEditorTabsProps {
   loading: boolean
 }
 
+const EditorTabComponent = ({ elementId, elementType, loading, defaultActiveKey, showLabelIfActive, items }: IEditorTabsStory): React.JSX.Element => {
+  const { setContext } = useGlobalAssetContext()
+
+  if (loading) {
+    return (
+      <div style={ { minWidth: 1024 } }>
+        <EditorTabsSkeleton />
+      </div>
+    )
+  }
+
+  if (elementId === undefined || elementType === undefined) {
+    return (<p>Please fill elementId and elementType argument</p>)
+  }
+
+  setContext({ id: elementId })
+
+  return (
+    <div style={ { minWidth: 1024 } }>
+      <EditorTabs
+        defaultActiveKey={ defaultActiveKey }
+        items={ items }
+        showLabelIfActive={ showLabelIfActive }
+      />
+    </div>
+  )
+}
+
 // TODO: Component needs refactoring because it contains business logic
 const config: Meta = {
   title: 'Components/__Refactor__/EditorTabs',
@@ -46,33 +74,7 @@ const config: Meta = {
     items: { table: { disable: true } }
   },
   tags: ['autodocs'],
-  render: ({ elementId, elementType, loading, defaultActiveKey, showLabelIfActive, items }: IEditorTabsStory) => {
-    const { setContext } = useGlobalAssetContext()
-
-    if (loading) {
-      return (
-        <div style={ { minWidth: 1024 } }>
-          <EditorTabsSkeleton />
-        </div>
-      )
-    }
-
-    if (elementId === undefined || elementType === undefined) {
-      return (<p>Please fill elementId and elementType argument</p>)
-    }
-
-    setContext({ id: elementId })
-
-    return (
-      <div style={ { minWidth: 1024 } }>
-        <EditorTabs
-          defaultActiveKey={ defaultActiveKey }
-          items={ items }
-          showLabelIfActive={ showLabelIfActive }
-        />
-      </div>
-    )
-  }
+  render: EditorTabComponent
 }
 
 export default config
