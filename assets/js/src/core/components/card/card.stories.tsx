1737e890a7aedd3bb3736f5487ee09beb86951f0
vin0401 <marvin.bertram@pimcore.com>
2025-01-13 10:55:13 +0100

    Remove unused references

--- assets/js/src/core/components/card/card.stories.tsx
+++ assets/js/src/core/components/card/card.stories.tsx
@@ -20,8 +20,6 @@ import { IconTextButton } from '@Pimcore/components/icon-text-button/icon-text-b
 import { Flex } from 'antd'
 import Input from 'antd/es/input/Input'
 import { Button } from '@Pimcore/components/button/button'
-import { ExtraPosition } from '../collapse/item/collapse-item.stories'
-import { extractComponentProps } from 'storybook/internal/docs-tools'
 
 const config: Meta = {
   title: 'Components/Data Display/Card',
