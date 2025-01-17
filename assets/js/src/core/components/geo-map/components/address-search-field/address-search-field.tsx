bef401e21c72fb8f606ba7a00aaeab7f09a26d82
markus-moser <markus.moser@pimcore.com>
2025-01-17 09:33:33 +0100

    Use map settings from settings endpoint

--- assets/js/src/core/components/geo-map/components/address-search-field/address-search-field.tsx
+++ assets/js/src/core/components/geo-map/components/address-search-field/address-search-field.tsx
@@ -17,6 +17,7 @@ import { useTranslation } from 'react-i18next'
 import { ERROR_ADDRESS_NOT_FOUND, geoCode } from '@Pimcore/components/geo-map/utils/geocode'
 import { useAlertModal } from '@Pimcore/components/modal/alert-modal/hooks/use-alert-modal'
 import { type GeoPoint } from '@Pimcore/components/geo-map/types/geo-types'
+import { useSettings } from '@Pimcore/modules/app/settings/hooks/use-settings'
 
 interface AddressSearchFieldProps {
   onSearch: (geoPoint?: GeoPoint) => void
@@ -26,13 +27,14 @@ interface AddressSearchFieldProps {
 export const AddressSearchField = (props: AddressSearchFieldProps): React.JSX.Element => {
   const { t } = useTranslation()
   const alertModal = useAlertModal()
+  const settings = useSettings()
 
   const onSearch = async (value: string): Promise<void> => {
     if (value === '') {
       props.onSearch(undefined); return
     }
 
-    await geoCode(value)
+    await geoCode(value, settings.maps.geocoding_url_template as string)
       .then(props.onSearch)
       .catch((error: Error) => {
         if (error.message === ERROR_ADDRESS_NOT_FOUND) {
