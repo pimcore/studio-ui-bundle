aa5e9fbd9c944f3b50f359df78720b8c09e52f23
markus-moser <markus.moser@pimcore.com>
2025-01-20 11:40:04 +0100

    Add many to many object relation data type

---
+++ assets/js/src/core/modules/element/dynamic-types/defintinitions/objects/data-related/components/many-to-many-object-relation/many-to-many-object-relation.tsx
@@ -0,0 +1,20 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import React from 'react'
+import { ManyToManyRelation } from '@Pimcore/modules/element/dynamic-types/defintinitions/objects/data-related/components/many-to-many-relation/many-to-many-relation'
+import type { ManyToManyRelationProps } from '@Pimcore/modules/element/dynamic-types/defintinitions/objects/data-related/components/many-to-many-relation/many-to-many-relation'
+
+export const ManyToManyObjectRelation = (props: ManyToManyRelationProps): React.JSX.Element => {
+  return <ManyToManyRelation { ...props } />
+}
