4c81a759ef749f9016ec0b5cf1ca9fc44e4f2bd9
xIrusux <christine.altmann@pimcore.com>
2025-01-20 17:55:11 +0100

    adjust alert stories

--- assets/js/src/core/components/modal/alert-modal/hooks/use-alert-modal.tsx
+++ assets/js/src/core/components/modal/alert-modal/hooks/use-alert-modal.tsx
@@ -13,7 +13,8 @@
 
 import { App, type ModalFuncProps } from 'antd'
 import { Icon } from '@Pimcore/components/icon/icon'
-import React, { useMemo } from 'react'
+import type React from 'react'
+import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
 type ConfigUpdate = ModalFuncProps | ((prevConfig: ModalFuncProps) => ModalFuncProps)
