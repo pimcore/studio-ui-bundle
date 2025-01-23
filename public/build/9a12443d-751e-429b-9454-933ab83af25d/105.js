21ef8c1d2a692d2decb8e604f7f5097d260e3bcb
ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>
2025-01-23 09:05:49 +0000

    Automatic frontend build

--- public/build/9a12443d-751e-429b-9454-933ab83af25d/105.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 105.js.LICENSE.txt */
-"use strict";(self.webpackChunkstudio_core=self.webpackChunkstudio_core||[]).push([[105],{78105:(e,s,c)=>{c.r(s),c.d(s,{Pimcore:()=>t});var i=c(81690),r=c(19387),n=c(54088),t={container:i.container,serviceIds:n.serviceIds,pluginSystem:r.pluginSystem}}}]);
