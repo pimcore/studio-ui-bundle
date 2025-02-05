eedabcf234832aeec7548565bd8ed6964d835a99
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-02-05 12:22:34 +0100

    [API] Update the behavior for 401 error (#944)
    * added the rtkQueryErrorLogger middleware to detect the 401 error
    
    * updated the logic of the isLoading state for login button
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

--- assets/js/src/core/app/store/index.ts
+++ assets/js/src/core/app/store/index.ts
@@ -14,6 +14,7 @@
 import { type Reducer, combineSlices, configureStore, type CombinedSliceReducer } from '@reduxjs/toolkit'
 import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { api as pimcoreApi } from '@Pimcore/app/api/pimcore'
+import { rtkQueryErrorLogger } from './middleware/rtkQueryErrorLogger'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface LazyloadedSlices {};
@@ -45,7 +46,7 @@ export const store = configureStore({
         ignoredActionPaths: ['execution-engine', 'meta'],
         ignoredPaths: ['execution-engine', 'meta']
       }
-    }).concat(pimcoreApi.middleware)
+    }).concat(pimcoreApi.middleware, rtkQueryErrorLogger)
 })
 
 export const injectSliceWithState = (newSlice: AnySliceLike): CombinedSliceReducer<Record<string, any>, Record<string, any>> => {
