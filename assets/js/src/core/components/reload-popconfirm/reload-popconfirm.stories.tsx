52832694df36e0f0d6c57457d9a51d2f4611379a
markus-moser <markus.moser@pimcore.com>
2025-02-07 13:41:18 +0100

    Extract reload popconfirm logic to separate component

---
+++ assets/js/src/core/components/reload-popconfirm/reload-popconfirm.stories.tsx
@@ -0,0 +1,93 @@
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - Pimcore Open Core License (POCL)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license  https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+ */
+
+import { type Meta, type StoryObj } from '@storybook/react';
+import React, { useRef } from 'react';
+import { ReloadPopconfirm, ReloadPopconfirmHandle } from './reload-popconfirm';
+import { Button } from 'antd';
+
+const config: Meta<typeof ReloadPopconfirm> = {
+  title: 'Components/Feedback/ReloadPopconfirm',
+  component: ReloadPopconfirm,
+  args: {
+    title: 'Are you sure you want to reload?',
+  },
+};
+
+export default config;
+
+
+export const _default: StoryObj<typeof ReloadPopconfirm> = {
+  render: (args) => (
+    <ReloadPopconfirm
+      {...args}
+      onReload={() => alert('Reloading automatically...')}
+      hasDataChanged={() => true}
+    >
+      <Button>Click me (auto-trigger)</Button>
+    </ReloadPopconfirm>
+  ),
+};
+
+
+export const WithDataChange: StoryObj<typeof ReloadPopconfirm> = {
+  render: (args) => {
+    const popconfirmRef = useRef<ReloadPopconfirmHandle>(null);
+
+    const triggerPopconfirm = () => {
+      if (popconfirmRef.current) {
+        popconfirmRef.current.refresh();
+      }
+    };
+
+    return (
+      <div>
+        <Button onClick={triggerPopconfirm}>Trigger ReloadPopconfirm</Button>
+        <ReloadPopconfirm
+          ref={popconfirmRef}
+          {...args}
+          onReload={() => alert('Reloading...')}
+          hasDataChanged={() => true}
+        >
+          <Button>Click me</Button>
+        </ReloadPopconfirm>
+      </div>
+    );
+  },
+};
+
+export const WithoutDataChange: StoryObj<typeof ReloadPopconfirm> = {
+  render: (args) => {
+    const popconfirmRef = useRef<ReloadPopconfirmHandle>(null);
+
+    const triggerPopconfirm = () => {
+      if (popconfirmRef.current) {
+        popconfirmRef.current.refresh();
+      }
+    };
+
+    return (
+      <div>
+        <Button onClick={triggerPopconfirm}>Trigger ReloadPopconfirm</Button>
+        <ReloadPopconfirm
+          ref={popconfirmRef}
+          {...args}
+          onReload={() => alert('Reloading without confirmation...')}
+          hasDataChanged={() => false}
+        >
+          <Button>Click me</Button>
+        </ReloadPopconfirm>
+      </div>
+    );
+  },
+};
--- assets/js/src/core/modules/data-object/editor/toolbar/context-menu/components/reload-button/reload-button.tsx
+++ assets/js/src/core/modules/data-object/editor/toolbar/context-menu/components/reload-button/reload-button.tsx
@@ -11,8 +11,7 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
-import React, { useState, useContext } from 'react'
-import { Popconfirm } from 'antd'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { IconButton } from '@Pimcore/components/icon-button/icon-button'
 import { useTranslation } from 'react-i18next'
 import { useDataObjectDraft } from '@Pimcore/modules/data-object/hooks/use-data-object-draft'
@@ -24,77 +23,94 @@ import {
 import { Dropdown, type ItemType } from '@Pimcore/components/dropdown/dropdown'
 import { useCustomLayouts } from '@Pimcore/modules/data-object/hooks/use-custom-layouts'
 import { Text } from '@Pimcore/components/text/text'
+import { ReloadPopconfirm, type ReloadPopconfirmHandle } from '@Pimcore/components/reload-popconfirm/reload-popconfirm'
+import { isString } from 'lodash'
 
 export const ReloadButton = (): React.JSX.Element => {
   const { t } = useTranslation()
   const { id } = useContext(DataObjectContext)
   const { dataObject } = useDataObjectDraft(id)
-  const [popConfirmOpen, setPopConfirmOpen] = useState<boolean>(false)
   const { refreshElement } = useElementRefresh('data-object')
   const { isLoading, layouts } = useCustomLayouts(id)
   const { setCurrentLayout, currentLayout } = useLayoutSelection()
+  const [selectedLayout, setSelectedLayout] = useState<string | null>()
+  const popconfirmRef = useRef<ReloadPopconfirmHandle>(null)
+
+  useEffect(() => {
+    if (isString(selectedLayout)) {
+      popconfirmRef.current?.refresh()
+    }
+  }, [selectedLayout])
 
   if (isLoading) {
     return <></>
   }
-  const onOpenChange = (newOpen: boolean): void => {
-    if (!newOpen) {
-      setPopConfirmOpen(false)
-      return
-    }
 
-    if (Object.keys(dataObject?.changes ?? {}).length > 0) {
-      setPopConfirmOpen(true)
-    } else {
-      refreshElement(id)
-    }
+  const hasDataChanged = (): boolean => {
+    return Object.keys(dataObject?.changes ?? {}).length > 0
   }
 
-  const onConfirm = (): void => {
-    setPopConfirmOpen(false)
+  const onReload = (): void => {
     refreshElement(id)
   }
 
-  const onCancel = (): void => {
-    setPopConfirmOpen(false)
+  const onReloadLayout = (): void => {
+    if (isString(selectedLayout)) {
+      setCurrentLayout(selectedLayout)
+    }
+
+    refreshElement(id)
+  }
+
+  const onCancelLayoutChange = (): void => {
+    setSelectedLayout(null)
   }
 
   const items: ItemType[] = (layouts ?? []).map(layout => ({
     key: `reload-${layout.id}`,
     label: <Text strong={ currentLayout === layout.id }>{ t(layout.name) }</Text>,
     onClick: (): void => {
-      setCurrentLayout(layout.id)
-      refreshElement(id)
+      setSelectedLayout(layout.id)
     }
   }))
 
   return (
-    <Popconfirm
-      key="reload"
-      onCancel={ onCancel }
-      onConfirm={ onConfirm }
-      onOpenChange={ onOpenChange }
-      open={ popConfirmOpen }
-      title={ t('toolbar.reload.confirmation') }
-    >
-      <IconButton
-        icon={ { value: 'refresh' } }
-      >
-        {t('toolbar.reload')}
-      </IconButton>
-
-      { items.length > 1 && (
-      <Dropdown
-        key="switch-layout"
-        menu={ { items } }
+    <>
+      <ReloadPopconfirm
+        hasDataChanged={ hasDataChanged }
+        key="reload"
+        onReload={ onReload }
+        title={ t('toolbar.reload.confirmation') }
       >
         <IconButton
-          icon={ { value: 'chevron-down' } }
+          icon={ { value: 'refresh' } }
         >
-          {t('toolbar.switch-layout')}
+          {t('toolbar.reload')}
         </IconButton>
-      </Dropdown>
-      ) }
-    </Popconfirm>
+
+      </ReloadPopconfirm>
+
+      <ReloadPopconfirm
+        hasDataChanged={ hasDataChanged }
+        key="reload"
+        onCancel={ onCancelLayoutChange }
+        onReload={ onReloadLayout }
+        ref={ popconfirmRef }
+        title={ t('toolbar.reload.confirmation') }
+      >
+        { items.length > 1 && (
+          <Dropdown
+            key="switch-layout"
+            menu={ { items } }
+          >
+            <IconButton
+              icon={ { value: 'chevron-down' } }
+            >
+              {t('toolbar.switch-layout')}
+            </IconButton>
+          </Dropdown>
+        ) }
+      </ReloadPopconfirm>
+    </>
   )
 }
