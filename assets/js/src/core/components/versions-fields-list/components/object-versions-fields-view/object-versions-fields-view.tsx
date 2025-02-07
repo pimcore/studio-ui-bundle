19b6b9914f03c1a77dc330867edb9977b4cf3e1f
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-02-07 10:10:46 +0100

    created the ObjectVersionsFieldsView component

---
+++ assets/js/src/core/components/versions-fields-list/components/object-versions-fields-view/object-versions-fields-view.tsx
@@ -0,0 +1,95 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import cn from 'classnames'
+import { Flex } from '@Pimcore/components/flex/flex'
+import { Text } from '@Pimcore/components/text/text'
+import {
+  type CategoriesList,
+  type IVersionsFieldsList,
+  type VersionKeysList
+} from '@Pimcore/components/versions-fields-list/types'
+import { useStyles } from './object-versions-fields-view.styles'
+import { VersionCategoryName } from '@Pimcore/constants/versionConstants'
+
+interface IAssetVersionsFieldsViewProps {
+  categoriesList?: CategoriesList
+  versionViewData: IVersionsFieldsList['data']
+  versionKeysList: VersionKeysList
+  modifiedFields: string[]
+}
+
+const CATEGORIES_WITHOUT_TRANSLATION = [VersionCategoryName.META]
+
+export const ObjectVersionsFieldsView = ({ categoriesList, versionViewData, versionKeysList, modifiedFields }: IAssetVersionsFieldsViewProps): React.JSX.Element => {
+  const { styles } = useStyles()
+  const { t } = useTranslation()
+
+  const renderFieldTitle = ({ key, value, categoryName }: { key: string, value: string, categoryName: VersionCategoryName }): React.JSX.Element => {
+    const isShowValueWithoutTranslation = CATEGORIES_WITHOUT_TRANSLATION.includes(categoryName)
+    const textValue = isShowValueWithoutTranslation ? value : t(`version.${key}`)
+
+    return (
+      <Text className={ styles.fieldTitle }>{textValue}</Text>
+    )
+  }
+
+  return (
+    <>
+      {categoriesList?.map((category, index) => (
+        <div key={ `${index}-${category.key}` }>
+          <Text
+            className={ styles.categoryTitle }
+            strong
+          >
+            {t(`version.category.title.${category.key}`)}
+          </Text>
+          <Flex
+            className={ styles.categoryFields }
+            gap="extra-small"
+            vertical
+          >
+            {versionViewData.map((fieldItem, fieldIndex) =>
+              category.fieldKeys.includes(fieldItem.Field.key) && (
+              <div key={ `${fieldIndex}-${fieldItem.Field.key}` }>
+                {renderFieldTitle({ categoryName: category.key, key: fieldItem.Field.key, value: fieldItem.Field.field })}
+                <Flex gap="mini">
+                  {versionKeysList.map((key, index) => {
+                    const isModifiedField = modifiedFields.includes(fieldItem.Field.key)
+                    const isSecondItem = index === 1
+
+                    return (
+                      <div
+                        className={
+                            cn(styles.categoryFieldItem, {
+                              [styles.categoryFieldItemHighlight]: isModifiedField && isSecondItem
+                            })
+                          }
+                        key={ `${index}-${key}` }
+                      >
+                        <Text>{fieldItem[key]}</Text>
+                      </div>
+                    )
+                  })}
+                </Flex>
+              </div>
+              )
+            )}
+          </Flex>
+        </div>
+      ))}
+    </>
+  )
+}
