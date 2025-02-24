0e6a22113a8edbf1e26e0facd507b96d8b3af57b
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-02-24 17:31:40 +0100

    updated the structure

--- assets/js/src/core/modules/data-object/editor/shared-tab-manager/tabs/versions/details-functions.ts
+++ assets/js/src/core/modules/data-object/editor/shared-tab-manager/tabs/versions/details-functions.ts
@@ -137,6 +137,9 @@ export const versionsDataToTableData = (data: IFormattedDataStructureData[][]): 
     const versionItem = mainVersionData[index]
     const compareVersionItem = compareVersionData[index]
 
+    console.log('------>>>>>> versionItem: ', versionItem)
+    console.log('------>>>>>> compareVersionItem: ', compareVersionItem)
+
     const hasCompareVersion = !isEmpty(compareVersionItem)
 
     const field: IObjectVersionField = {
@@ -154,49 +157,51 @@ export const versionsDataToTableData = (data: IFormattedDataStructureData[][]): 
 
     if (hasCompareVersion) {
       field[`Version ${compareVersionItem.versionCount}`] = compareVersionItem.fieldValue ?? null
+    }
 
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      if (COMPLEX_DATA_OBJECT_TYPES.includes(versionItem.fieldData.fieldtype as DynamicTypesList)) {
-        const allFieldKeys = new Set([
-          ...Object.keys(versionItem?.fieldValue as object),
-          ...(!isEmpty(compareVersionItem?.fieldValue as object) ? Object.keys(compareVersionItem?.fieldValue as object) : [])
-        ])
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    if (COMPLEX_DATA_OBJECT_TYPES.includes(versionItem.fieldData.fieldtype as DynamicTypesList)) {
+      const allFieldKeys = new Set([
+        ...Object.keys(versionItem?.fieldValue as object),
+        ...(!isEmpty(compareVersionItem?.fieldValue as object) ? Object.keys(compareVersionItem?.fieldValue as object) : [])
+      ])
 
-        const list: Array<{ key: string, localesList: string[] }> = []
-        const list2: Array<{ key: string, localesList: string[] }> = []
+      const list: Array<{ key: string, localesList: string[] }> = []
+      const list2: Array<{ key: string, localesList: string[] }> = []
 
-        allFieldKeys.forEach(key => {
-          if (JSON.stringify(versionItem.fieldValue?.[key]) !== JSON.stringify(compareVersionItem.fieldValue?.[key])) {
-            if (!isEmpty(compareVersionItem.fieldValue?.[key])) {
-              const getNonNullValues = (obj: object): any => Object.keys(obj).filter(key => obj[key] !== null)
+      allFieldKeys.forEach(key => {
+        if (JSON.stringify(versionItem?.fieldValue?.[key]) !== JSON.stringify(compareVersionItem?.fieldValue?.[key])) {
+          if (!isEmpty(compareVersionItem?.fieldValue?.[key])) {
+            const getNonNullValues = (obj: object): any => Object.keys(obj).filter(key => obj[key] !== null)
 
-              const result1 = isObject(versionItem.fieldValue?.[key]) ? getNonNullValues((versionItem.fieldValue?.[key])) : []
-              const result2 = isObject(compareVersionItem.fieldValue?.[key]) ? getNonNullValues((compareVersionItem.fieldValue?.[key])) : []
+            const result1 = isObject(versionItem.fieldValue?.[key]) ? getNonNullValues((versionItem.fieldValue?.[key])) : []
+            const result2 = isObject(compareVersionItem.fieldValue?.[key]) ? getNonNullValues((compareVersionItem.fieldValue?.[key])) : []
 
-              const mergedResult = [...result1, ...result2]
+            const mergedResult = [...result1, ...result2]
 
-              uniq(mergedResult)?.forEach((item: string) => {
-                if (versionItem.fieldValue?.[key]?.[item] !== compareVersionItem.fieldValue?.[key]?.[item]) {
-                  const existingItem = list.find(entry => entry.key === key)
+            uniq(mergedResult)?.forEach((item: string) => {
+              if (versionItem.fieldValue?.[key]?.[item] !== compareVersionItem.fieldValue?.[key]?.[item]) {
+                const existingItem = list.find(entry => entry.key === key)
 
-                  if (!isEmpty(existingItem)) {
-                    existingItem?.localesList.push(...existingItem.localesList, item)
-                  } else {
-                    list.push({ key, localesList: [item] })
-                  }
+                if (!isEmpty(existingItem)) {
+                  existingItem?.localesList.push(...existingItem.localesList, item)
+                } else {
+                  list.push({ key, localesList: [item] })
                 }
-              })
+              }
+            })
 
-              list2.push({ key, localesList: uniq(mergedResult) })
-              field.isModifiedValue = true
-            }
+            list2.push({ key, localesList: uniq(mergedResult) })
+            field.isModifiedValue = true
           }
-        })
+        }
+      })
 
-        field.listModifiedFields = list
-        field.listAllFieldsWithoutNull = list2
-      } else {
+      field.listModifiedFields = list
+      field.listAllFieldsWithoutNull = list2
+    } else {
+      if (hasCompareVersion) {
         if (!isEqual(versionItem.fieldValue, compareVersionItem.fieldValue)) {
           field.isModifiedValue = true
         }
