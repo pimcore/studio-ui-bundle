16d34cd81d2b1c2b64ec8aa0a63fb2e363ec1c1a
markus-moser <4639428+markus-moser@users.noreply.github.com>
2025-03-04 17:10:57 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/element-tree/types/node-api-hook.ts
+++ assets/js/src/core/components/element-tree/types/node-api-hook.ts
@@ -1,11 +1,24 @@
-import { TreeNode } from "../element-tree-slice"
-import { NodeState } from "../hooks/use-element-tree-node"
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
+
+import { type TreeNode } from '../element-tree-slice'
+import { type NodeState } from '../hooks/use-element-tree-node'
 
 export type NodeApiHook = () => NodeApiHookReturnType
 
 export interface DataTransformerSourceNode {
-    id: string
-    internalKey: string
+  id: string
+  internalKey: string
 }
 
 export interface DataTransformerReturnType {
@@ -14,5 +27,5 @@ export interface DataTransformerReturnType {
 }
 
 export interface NodeApiHookReturnType {
-    fetchApiHookResult: (node: DataTransformerSourceNode, nodeState: NodeState) => Promise<DataTransformerReturnType | undefined>
-  }
+  fetchApiHookResult: (node: DataTransformerSourceNode, nodeState: NodeState) => Promise<DataTransformerReturnType | undefined>
+}
