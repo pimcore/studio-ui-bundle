d06abdca2054e57500a3285541f6aaee30ad604f
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-03-13 18:27:06 +0100

    updated the ObjectVersionsFieldsView component

--- assets/js/src/core/modules/data-object/editor/shared-tab-manager/tabs/versions/components/object-versions-fields-view/object-versions-fields-view.tsx
+++ assets/js/src/core/modules/data-object/editor/shared-tab-manager/tabs/versions/components/object-versions-fields-view/object-versions-fields-view.tsx
@@ -88,11 +88,16 @@ export const ObjectVersionsFieldsView = ({ breadcrumbsList, versionViewData, ver
                 const isBreadcrumbKeyMatch = breadcrumb.key === fieldItem.Field.fieldBreadcrumbTitle
                 const isFieldInBreadcrumbList = breadcrumb.fieldKeys.includes(fieldItem.Field.name as string)
 
+                const isGroupedType = fieldItem.Field.fieldtype === 'fieldcollections'
+
                 return (
                   isBreadcrumbKeyMatch && isFieldInBreadcrumbList && (
                     <div key={ `${fieldIndex}-${fieldItem.Field.name}` }>
                       {renderFieldTitle({ key: fieldItem.Field.title, locale: fieldItem.Field?.locale, isCommonSection })}
-                      <Flex gap="mini">
+                      <Flex
+                        className={ cn({ [styles.sectionFields]: isGroupedType }) }
+                        gap="mini"
+                      >
                         {versionKeysList.map((key, index) => {
                           const isModifiedField = fieldItem?.isModifiedValue === true
                           const isSecondItem = index === 1
