1a54278274d7d3673a565d30d7adcb8474584a15
vin0401 <26813978+vin0401@users.noreply.github.com>
2025-03-20 11:43:51 +0000

    Apply eslint-fixer changes

--- assets/js/src/sdk/modules/user/index.ts
+++ assets/js/src/sdk/modules/user/index.ts
@@ -1,7 +1,20 @@
-export * from '@Pimcore/modules/user/hooks/use-global-user-context';
-export * from '@Pimcore/modules/user/hooks/use-user-draft';
-export * from '@Pimcore/modules/user/hooks/use-user-helper';
-export * from '@Pimcore/modules/user/hooks/use-user-trackable-changes';
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Open Core License (POCL)
+* - Pimcore Commercial License (PCL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
+*/
 
-export * from '@Pimcore/modules/user/roles/hooks/use-roles-draft';
-export * from '@Pimcore/modules/user/roles/hooks/use-roles-helper';
+export * from '@Pimcore/modules/user/hooks/use-global-user-context'
+export * from '@Pimcore/modules/user/hooks/use-user-draft'
+export * from '@Pimcore/modules/user/hooks/use-user-helper'
+export * from '@Pimcore/modules/user/hooks/use-user-trackable-changes'
+
+export * from '@Pimcore/modules/user/roles/hooks/use-roles-draft'
+export * from '@Pimcore/modules/user/roles/hooks/use-roles-helper'
