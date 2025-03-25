fcb64efde914f698a6e757d7b3f2d77e6780927d
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-03-25 10:14:08 +0100

    [Versions Tab] Fix the data handling logic (#1172)
    * deleted the comparison statement
    
    * added functionality of caching endpoints
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

--- assets/js/src/core/modules/element/dynamic-types/definitions/objects/data-related/types/dynamic-type-object-data-object-brick.tsx
+++ assets/js/src/core/modules/element/dynamic-types/definitions/objects/data-related/types/dynamic-type-object-data-object-brick.tsx
@@ -24,6 +24,9 @@ import {
   type IFormattedDataStructureData,
   type IProcessVersionFieldDataProps
 } from '@Pimcore/modules/data-object/editor/shared-tab-manager/tabs/versions/types'
+import {
+  DynamicTypesList
+} from '@Pimcore/modules/element/dynamic-types/definitions/objects/data-related/constants/typesList'
 
 export class DynamicTypeObjectDataObjectBrick extends DynamicTypeObjectDataAbstract {
   id: string = 'objectbricks'
@@ -41,7 +44,7 @@ export class DynamicTypeObjectDataObjectBrick extends DynamicTypeObjectDataAbstr
   }
 
   async processVersionFieldData (props: IProcessVersionFieldDataProps): Promise<IFormattedDataStructureData[]> {
-    const { objectId, item, fieldBreadcrumbTitle, fieldValueByName, versionId, versionCount } = props
+    const { objectId, item, fieldBreadcrumbTitle, fieldValueByName, versionId, versionCount, layoutsList, setLayoutsList } = props
 
     let currentBrickSection: null | string = null
 
@@ -88,9 +91,17 @@ export class DynamicTypeObjectDataObjectBrick extends DynamicTypeObjectDataAbstr
 
     async function handleObjectBrickData (): Promise<IFormattedDataStructureData[] | []> {
       try {
+        const bricksLayout = layoutsList.filter((layout) => layout.type === DynamicTypesList.OBJECT_BRICKS)
+
+        if (!isEmpty(bricksLayout)) {
+          return processObjectBrickData({ data: bricksLayout[0]?.data as ObjectBrickLayoutDefinition[] })
+        }
+
         const data: ClassObjectBrickObjectLayoutApiResponse | null = await loadLayoutById()
 
         if (!isEmpty(data)) {
+          setLayoutsList([...layoutsList, { type: DynamicTypesList.OBJECT_BRICKS, data: data?.items }])
+
           return processObjectBrickData({ data: data?.items })
         } else {
           return []
