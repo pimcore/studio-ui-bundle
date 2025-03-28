a63344c15448d90c396e1acec982dcfcfa59aa40
fashxp <8792145+fashxp@users.noreply.github.com>
2025-03-28 10:30:24 +0000

    Apply php-cs-fixer changes

--- src/Installer.php
+++ src/Installer.php
@@ -1,12 +1,24 @@
 <?php
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace Pimcore\Bundle\StudioUiBundle;
 
 use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
 
 class Installer extends SettingsStoreAwareInstaller
 {
-
     public function install(): void
     {
         //currently nothing to do
@@ -18,5 +30,4 @@ class Installer extends SettingsStoreAwareInstaller
         //currently nothing to do
         $this->markUninstalled();
     }
-
 }
