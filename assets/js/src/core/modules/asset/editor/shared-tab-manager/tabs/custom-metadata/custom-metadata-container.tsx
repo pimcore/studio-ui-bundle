6f6235ed005ddda1b9e286e8c0b8cb50dd1046fe
markus-moser <markus.moser@pimcore.com>
2025-04-09 13:13:17 +0200

    [Asset Editor] custom metadata can be added with wrong permission (#1325)

--- assets/js/src/core/modules/asset/editor/shared-tab-manager/tabs/custom-metadata/custom-metadata-container.tsx
+++ assets/js/src/core/modules/asset/editor/shared-tab-manager/tabs/custom-metadata/custom-metadata-container.tsx
@@ -41,6 +41,7 @@ import {
   useLazyMetadataGetCollectionQuery
 } from '@Pimcore/modules/asset/editor/shared-tab-manager/tabs/custom-metadata/metadata-api-slice-enhanced'
 import trackError, { ApiError } from '@Pimcore/modules/app/error-handler'
+import { checkElementPermission } from '@Pimcore/modules/element/permissions/permission-helper'
 
 export interface CustomMetadataTabContainerProps {
   disableHeaderTitle?: boolean
@@ -52,7 +53,7 @@ export const CustomMetadataTabContainer = ({ disableHeaderTitle = false, disable
   const [editmode, setEditMode] = useState<boolean>(false)
   const settings = useSettings()
   const { id } = useContext(AssetContext)
-  const { addCustomMetadata, customMetadata } = useAssetDraft(id)
+  const { asset, addCustomMetadata, customMetadata } = useAssetDraft(id)
   const [predefinedMetadata, { isFetching, isError, error }] = useLazyMetadataGetCollectionQuery()
 
   const {
@@ -72,6 +73,7 @@ export const CustomMetadataTabContainer = ({ disableHeaderTitle = false, disable
     }
   }, [isError])
 
+  const isEditable = checkElementPermission(asset?.permissions, 'publish')
   const nameInputValue = useRef<string>('')
   const nameInputRef = useRef<InputRef>(null)
   const typeSelectValue = useRef<string>('input')
@@ -270,7 +272,7 @@ export const CustomMetadataTabContainer = ({ disableHeaderTitle = false, disable
             </>
             )}
 
-            {!editmode && (
+            {!editmode && isEditable && (
             <ButtonGroup items={ buttons } />
             )}
           </Space>
