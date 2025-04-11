41646569d7da8ac718b05133c3da362a1b5bb650
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-04-11 10:46:21 +0200

    fixed styles

--- assets/js/src/core/components/focal-point/focal-point.styles.ts
+++ assets/js/src/core/components/focal-point/focal-point.styles.ts
@@ -40,7 +40,7 @@ export const useStyles = createStyles(({ token, css }) => {
       transform: translate(-50%, -50%);
       
 
-        &:hover {
+        &:hover, &:active {
           color: ${token.colorPrimary};
           background: ${token.Colors.Neutral.Fill.colorFill} !important;
       }
