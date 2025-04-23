b5d02c90e00e1b334f212b72a421d6a8f7b8db5b
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-04-23 15:49:03 +0200

    updated the DynamicTypeFieldFilterObjectAdapterComponent

--- assets/js/src/core/modules/element/dynamic-types/definitions/field-filters/components/dynamic-type-field-filter-object-adapter.tsx
+++ assets/js/src/core/modules/element/dynamic-types/definitions/field-filters/components/dynamic-type-field-filter-object-adapter.tsx
@@ -12,12 +12,36 @@
 */
 
 import React from 'react'
+import { Alert } from 'antd'
 import { type AbstractFieldFilterDefinition } from '../dynamic-type-field-filter-abstract'
+import { useDynamicFilter } from '@Pimcore/components/dynamic-filter/provider/use-dynamic-filter'
+import { useInjection } from '@Pimcore/app/depency-injection'
+import type { DynamicTypeFieldFilterRegistry } from '@Pimcore/modules/element/dynamic-types/definitions/field-filters/dynamic-type-field-filter-registry'
+import { serviceIds } from '@Pimcore/app/config/services/service-ids'
 
 export interface DynamicTypeFieldFilterObjectAdapterProps extends AbstractFieldFilterDefinition {}
 
 export const DynamicTypeFieldFilterObjectAdapterComponent = (): React.JSX.Element => {
-  return (
-    <div>Adapter</div>
-  )
+  const { config } = useDynamicFilter()
+
+  if (!('fieldDefinition' in config)) {
+    throw new Error('Field definition is missing in config')
+  }
+
+  const { fieldDefinition } = config
+  const currentFieldType: string = fieldDefinition?.fieldType ?? fieldDefinition?.fieldtype ?? 'unknown'
+  const objectDataRegistry = useInjection<DynamicTypeFieldFilterRegistry>(serviceIds['DynamicTypes/FieldFilterRegistry'])
+
+  if (!objectDataRegistry.hasDynamicType(currentFieldType)) {
+    return (
+      <Alert
+        message={ `Unknown data type: ${currentFieldType}` }
+        type="warning"
+      />
+    )
+  }
+
+  const objectDataType = objectDataRegistry.getDynamicType(currentFieldType)
+
+  return objectDataType.getFieldFilterComponent(fieldDefinition as AbstractFieldFilterDefinition)
 }
