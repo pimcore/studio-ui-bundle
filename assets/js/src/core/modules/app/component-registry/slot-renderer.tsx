f44fa7906b35b6054f1a0a3de3244d5ce23ca008
markus-moser <markus.moser@pimcore.com>
2025-04-25 14:19:58 +0200

    Add additional component registry slots (#1398)

--- assets/js/src/core/modules/app/component-registry/slot-renderer.tsx
+++ assets/js/src/core/modules/app/component-registry/slot-renderer.tsx
@@ -11,7 +11,7 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
-import React, { useEffect } from 'react'
+import React, { useRef } from 'react'
 import { type ComponentRegistryEntry, useComponentRegistry } from './component-registry'
 import { isUndefined } from 'lodash'
 
@@ -21,33 +21,39 @@ interface SlotRendererProps {
     defaultEntries?: Array<ComponentRegistryEntry<any>>
   }
   props?: Record<string, any>
+  onRenderComponent?: (Component: React.JSX.Element, context: { name?: string, index?: number, props?: Record<string, any> }) => React.JSX.Element
 }
 
-export const SlotRenderer = ({ slot, props }: SlotRendererProps): React.JSX.Element => {
+export const SlotRenderer = ({ slot, props, onRenderComponent }: SlotRendererProps): React.JSX.Element => {
   const { name, defaultEntries } = slot
   const ComponentRegistry = useComponentRegistry()
 
-  useEffect(() => {
-    if (!isUndefined(defaultEntries)) {
-      const existingEntries = ComponentRegistry.getSlotComponents(name).map(({ name }) => name)
-      Object.entries(defaultEntries).forEach(([key, entry]) => {
-        if (!existingEntries.includes(entry.name)) {
-          ComponentRegistry.registerToSlot(name, entry)
-        }
-      })
-    }
-  }, [name, defaultEntries, ComponentRegistry])
+  const hasRegisteredDefaults = useRef(false)
+
+  if (!hasRegisteredDefaults.current && !isUndefined(defaultEntries)) {
+    const existingEntries = ComponentRegistry.getSlotComponents(name).map(({ name }) => name)
+    defaultEntries.forEach((entry) => {
+      if (!existingEntries.includes(entry.name)) {
+        ComponentRegistry.registerToSlot(name, entry)
+      }
+    })
+    hasRegisteredDefaults.current = true
+  }
 
   const components = ComponentRegistry.getSlotComponents(name)
 
   return (
     <>
-      {components.map(({ component: Component, name }, index) => (
-        <Component
-          key={ name }
-          { ...props }
-        />
-      ))}
+      {components.map(({ component: Component, name }, index) => {
+        const renderedComponent = (
+          <Component
+            key={ `component-${name}` }
+            { ...props }
+          />
+        )
+        const context = { name, index, props }
+        return !isUndefined(onRenderComponent) ? onRenderComponent(renderedComponent, context) : renderedComponent
+      })}
     </>
   )
 }
--- assets/js/src/core/modules/data-object/editor/toolbar/toolbar.tsx
+++ assets/js/src/core/modules/data-object/editor/toolbar/toolbar.tsx
@@ -11,41 +11,23 @@
 *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
 */
 
-import React, { useContext } from 'react'
+import React from 'react'
 import { Toolbar as ToolbarView } from '@Pimcore/components/toolbar/toolbar'
-import { useDataObjectDraft } from '../../hooks/use-data-object-draft'
-import { DataObjectContext } from '../../data-object-provider'
 
-import { type ComponentRegistry } from '@Pimcore/modules/app/component-registry/component-registry'
-import { serviceIds } from '@Pimcore/app/config/services/service-ids'
-import { container } from '@Pimcore/app/depency-injection'
 import { Flex } from '@Pimcore/components/flex/flex'
-import { TAB_EDIT } from '../types/object/tab-manager/tabs/edit/edit-container'
-import { LanguageSelection } from './language-selection/language-selection'
 import { WorkflowLogModal } from '@Pimcore/modules/asset/editor/toolbar/workflow-log-modal/workflow-log-modal'
-import { EditorToolbarWorkflowMenu } from '@Pimcore/modules/asset/editor/toolbar/workflow-menu/workflow-menu'
 import { WorkFlowProvider } from '@Pimcore/modules/asset/editor/toolbar/workflow-log-modal/workflow-provider'
-
-import {
-  EditorToolbarSaveButtons
-} from '@Pimcore/modules/data-object/editor/toolbar/save-buttons/save-buttons'
 import { componentConfig } from '@Pimcore/modules/app/component-registry/component-config'
+import { SlotRenderer } from '@Pimcore/modules/app/component-registry/slot-renderer'
 
 export const Toolbar = (): React.JSX.Element => {
-  const { id } = useContext(DataObjectContext)
-  const { activeTab } = useDataObjectDraft(id)
-  const componentRegistry = container.get<ComponentRegistry>(serviceIds['App/ComponentRegistry/ComponentRegistry'])
-  const ContextMenu = componentRegistry.get(componentConfig.dataObject.editor.toolbar.contextMenu.name)
-
   return (
     <ToolbarView>
       <WorkFlowProvider>
         <Flex>
-          <ContextMenu />
-
-          {activeTab === TAB_EDIT.key && (
-            <LanguageSelection />
-          )}
+          <SlotRenderer
+            slot={ componentConfig.dataObject.editor.toolbar.slots.left }
+          />
         </Flex>
 
         <Flex
@@ -54,8 +36,9 @@ export const Toolbar = (): React.JSX.Element => {
           style={ { height: '32px' } }
           vertical={ false }
         >
-          <EditorToolbarWorkflowMenu />
-          <EditorToolbarSaveButtons />
+          <SlotRenderer
+            slot={ componentConfig.dataObject.editor.toolbar.slots.right }
+          />
         </Flex>
         <WorkflowLogModal />
       </WorkFlowProvider>
