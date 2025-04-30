0a7e117b50d4c7dff1b84e2befc03e0fef76c3db
Christian Fasching <fashxp@users.noreply.github.com>
2025-04-30 10:06:44 +0200

    License Switch to POCL (#1427)

--- assets/js/src/core/utils/css.tsx
+++ assets/js/src/core/utils/css.tsx
@@ -1,15 +1,12 @@
 /**
-* Pimcore
-*
-* This source file is available under two different licenses:
-* - Pimcore Open Core License (POCL)
-* - Pimcore Commercial License (PCL)
-* Full copyright and license information is available in
-* LICENSE.md which is distributed with this source code.
-*
-*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
-*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
-*/
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
 
 export const toCssDimension = (value?: number | string | null, fallback?: number | string): string | undefined => {
   if (value === null || value === undefined || value === '' || value === 0) {
