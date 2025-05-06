caac72353f5aa21b9c134b6223763950362a75f8
Christian F. <christian.feldkirchner@pimcore.com>
2025-05-06 15:14:06 +0200

    Added alert to edit tab (#1438)
    * added alert to indicate error
    
    * Automatic frontend build
    
    * added translation value
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: Corepex <16717695+Corepex@users.noreply.github.com>

--- assets/js/src/core/modules/asset/editor/types/text/tab-manager/tabs/edit/edit-container.tsx
+++ assets/js/src/core/modules/asset/editor/types/text/tab-manager/tabs/edit/edit-container.tsx
@@ -8,23 +8,25 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
-import React, { useContext } from 'react'
-import { isString } from 'lodash'
-import { EditView } from './edit-view'
+import { detectLanguageFromFilename, type SupportedLanguage } from '@Pimcore/components/text-editor/detect-language'
 import { useAssetGetTextDataByIdQuery } from '@Pimcore/modules/asset/asset-api-slice-enhanced'
 import { AssetContext } from '@Pimcore/modules/asset/asset-provider'
-import { useStyle } from './edit-container.styles'
-import { isSet } from '@Pimcore/utils/helpers'
-import { detectLanguageFromFilename, type SupportedLanguage } from '@Pimcore/components/text-editor/detect-language'
 import { useAssetDraft } from '@Pimcore/modules/asset/hooks/use-asset-draft'
+import { isSet } from '@Pimcore/utils/helpers'
+import { isString } from 'lodash'
+import React, { useContext } from 'react'
+import { useStyle } from './edit-container.styles'
+import { EditView } from './edit-view'
+import { Box } from '@Pimcore/components/box/box'
+import { Alert } from '@Pimcore/components/alert/alert'
+import { useTranslation } from 'react-i18next'
 
 const EditContainer = (): React.JSX.Element => {
   const assetContext = useContext(AssetContext)
-
   const { asset, updateTextData } = useAssetDraft(assetContext.id)
-  const { data } = useAssetGetTextDataByIdQuery({ id: assetContext.id })
-
+  const { data, isError } = useAssetGetTextDataByIdQuery({ id: assetContext.id })
   const { styles } = useStyle()
+  const { t } = useTranslation()
 
   let language: SupportedLanguage = null
 
@@ -36,15 +38,27 @@ const EditContainer = (): React.JSX.Element => {
     updateTextData(data)
   }
 
+  if (isError) {
+    return (
+      <Box padding="extra-small">
+        <Alert
+          message={ t('preview-not-available') }
+          showIcon
+          type="info"
+        />
+      </Box>
+    )
+  }
+
   return (
     <div className={ styles.relativeContainer }>
-      { isSet(data) && (
-      <EditView
-        language={ language }
-        src={ data!.data }
-        updateTextData={ handleUpdateAssetStoreData }
-      />
-      ) }
+      {isSet(data) && (
+        <EditView
+          language={ language }
+          src={ data!.data }
+          updateTextData={ handleUpdateAssetStoreData }
+        />
+      )}
     </div>
   )
 }
