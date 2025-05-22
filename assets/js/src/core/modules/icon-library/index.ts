59cdec908c0bf30dfd6c3c06dc84ede1e8aa0dbf
xIrusux <christine.altmann@gmx.com>
2025-05-22 14:49:25 +0200

    add new notification icons

--- assets/js/src/core/modules/icon-library/index.ts
+++ assets/js/src/core/modules/icon-library/index.ts
@@ -1,11 +1,14 @@
 /**
- * This source file is available under the terms of the
- * Pimcore Open Core License (POCL)
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - Pimcore Open Core License (POCL)
+ * - Pimcore Commercial License (PCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
- *  @license    Pimcore Open Core License (POCL)
+ *  @copyright  Copyright (c) Pimcore GmbH
+ *  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PCL
  */
 
 /* eslint-disable max-lines */
@@ -14,7 +17,7 @@ import { container } from '@Pimcore/app/depency-injection'
 import { moduleSystem } from '@Pimcore/app/module-system/module-system'
 import { serviceIds } from '@Pimcore/app/config/services/service-ids'
 import { type IconLibrary } from './services/icon-library'
-import arrowNarrowRight from '@Pimcore/assets/icons/arrow-narrow-right.inline.svg?react'
+
 import accessory from '@Pimcore/assets/icons/accessory.inline.svg?react'
 import addFind from '@Pimcore/assets/icons/add-find.inline.svg?react'
 import addFolder from '@Pimcore/assets/icons/add-folder.inline.svg?react'
@@ -23,6 +26,7 @@ import addPackage from '@Pimcore/assets/icons/add-package.inline.svg?react'
 import addSomething from '@Pimcore/assets/icons/add-something.inline.svg?react'
 import addUser from '@Pimcore/assets/icons/add-user.inline.svg?react'
 import alert from '@Pimcore/assets/icons/alert.inline.svg?react'
+import arrowNarrowRight from '@Pimcore/assets/icons/arrow-narrow-right.inline.svg?react'
 import asset from '@Pimcore/assets/icons/asset.inline.svg?react'
 import attachment from '@Pimcore/assets/icons/attachment.inline.svg?react'
 import audio from '@Pimcore/assets/icons/audio.inline.svg?react'
@@ -146,7 +150,8 @@ import newIcon from '@Pimcore/assets/icons/new.inline.svg?react'
 import news from '@Pimcore/assets/icons/news.inline.svg?react'
 import noContent from '@Pimcore/assets/icons/no-content.inline.svg?react'
 import notesEvents from '@Pimcore/assets/icons/notes-events.inline.svg?react'
-import notification from '@Pimcore/assets/icons/notification.inline.svg?react'
+import notificationRead from '@Pimcore/assets/icons/notification-read.inline.svg?react'
+import notificationUnread from '@Pimcore/assets/icons/notification-unread.inline.svg?react'
 import numberField from '@Pimcore/assets/icons/number-field.inline.svg?react'
 import openFolder from '@Pimcore/assets/icons/open-folder.inline.svg?react'
 import packageIcon from '@Pimcore/assets/icons/package.inline.svg?react'
@@ -214,7 +219,7 @@ import xlsxCsv from '@Pimcore/assets/icons/xlsx-csv.inline.svg?react'
 
 moduleSystem.registerModule({
   onInit: () => {
-    const iconLibrary = container.get<IconLibrary>(serviceIds.iconLibrary)
+    const iconLibrary = container.get<IconLibrary>(serviceIds.iconLibrary);
     iconLibrary.register({
       name: 'accessory',
       component: accessory
@@ -744,8 +749,12 @@ moduleSystem.registerModule({
       component: notesEvents
     })
     iconLibrary.register({
-      name: 'notification',
-      component: notification
+      name: 'notification-read',
+      component: notificationRead
+    })
+    iconLibrary.register({
+      name: 'notification-unread',
+      component: notificationUnread
     })
     iconLibrary.register({
       name: 'number-field',
