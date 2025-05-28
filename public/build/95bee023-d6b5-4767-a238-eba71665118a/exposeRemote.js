a9f73c4741747bb33bc5db642ebc9208fa79d1a9
Tine <42359615+xIrusux@users.noreply.github.com>
2025-05-28 16:41:49 +0200

    fix title type break (#1546)
    * fix title type break
    
    * Automatic frontend build

--- public/build/95bee023-d6b5-4767-a238-eba71665118a/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/95bee023-d6b5-4767-a238-eba71665118a/static/js/remoteEntry.js'
-    
---
+++ public/build/d4f3bf65-ffcd-4386-b62c-24a04edbc58d/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/d4f3bf65-ffcd-4386-b62c-24a04edbc58d/static/js/remoteEntry.js'
+    
