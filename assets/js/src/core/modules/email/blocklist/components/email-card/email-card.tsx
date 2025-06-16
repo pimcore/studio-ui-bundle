faf89cc865babb67fd7c4c52ea1dd2db92612e14
Corepex <christian.feldkirchner@pimcore.com>
2025-06-16 15:04:58 +0200

    fixed linter issue

--- assets/js/src/core/modules/email/blocklist/components/email-card/email-card.tsx
+++ assets/js/src/core/modules/email/blocklist/components/email-card/email-card.tsx
@@ -1,12 +1,22 @@
-import { Card } from "@Pimcore/components/card/card"
-import { Flex } from "@Pimcore/components/flex/flex"
-import { Blocklist } from "@Pimcore/modules/email/emails-api-slice.gen"
-import { IconButton } from "@sdk/components"
-import { formatDateTime } from "@sdk/utils"
-import { Space } from "antd"
-import React, { useState } from "react"
-import { useTranslation } from "react-i18next"
-import { useEmailBlocklist } from "../../hooks/use-email-blocklist"
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { Card } from '@Pimcore/components/card/card'
+import { Flex } from '@Pimcore/components/flex/flex'
+import { type Blocklist } from '@Pimcore/modules/email/emails-api-slice.gen'
+import { IconButton } from '@sdk/components'
+import { formatDateTime } from '@sdk/utils'
+import { Space } from 'antd'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useEmailBlocklist } from '../../hooks/use-email-blocklist'
 
 interface EmailCardProps {
   entry: Blocklist
@@ -19,21 +29,24 @@ export const EmailCard = ({ entry }: EmailCardProps): React.JSX.Element => {
 
   return (
     <Card>
-      <Flex align="center" justify="space-between">
+      <Flex
+        align="center"
+        justify="space-between"
+      >
         <span>{entry.email}</span>
 
         <Space>
           <span>{formatDateTime({ timestamp: entry.modificationDate!, dateStyle: 'short', timeStyle: 'short' })}</span>
 
           <IconButton
-            icon={isLoading ? { value: 'spinner' } : { value: 'trash' }}
-            type="link"
-            loading={isLoading}
-            onClick={() => {
+            aria-label={ t('aria.email-blocklist.remove.email') }
+            icon={ isLoading ? { value: 'spinner' } : { value: 'trash' } }
+            loading={ isLoading }
+            onClick={ () => {
               setIsLoading(true)
               void removeEmail(entry.email)
-            }}
-            aria-label={t('aria.email-blocklist.remove.email')}
+            } }
+            type="link"
           />
         </Space>
       </Flex>
