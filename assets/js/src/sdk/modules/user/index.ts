d754b8399618539383aa8c2e38da2ad6dace372f
sholzer <sarah.holzer@valantic.at>
2025-06-18 12:33:23 +0200

    [User Management] Profile Page #1492

--- assets/js/src/sdk/modules/user/index.ts
+++ assets/js/src/sdk/modules/user/index.ts
@@ -15,7 +15,6 @@ if (module.hot !== undefined) {
 export * from '@Pimcore/modules/user/hooks/use-global-user-management-context'
 export * from '@Pimcore/modules/user/hooks/use-user-management-draft'
 export * from '@Pimcore/modules/user/hooks/use-user-management-helper'
-export * from '@Pimcore/modules/user/hooks/use-user-trackable-changes'
 
 export * from '@Pimcore/modules/user/roles/hooks/use-roles-draft'
 export * from '@Pimcore/modules/user/roles/hooks/use-roles-helper'
