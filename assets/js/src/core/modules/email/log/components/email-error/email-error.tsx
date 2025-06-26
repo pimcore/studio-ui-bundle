aa5cf11eb134ba4dac05c18b93f2de898488f6e8
Corepex <christian.feldkirchner@pimcore.com>
2025-06-26 13:56:27 +0200

    added email error alert

---
+++ assets/js/src/core/modules/email/log/components/email-error/email-error.tsx
@@ -0,0 +1,23 @@
+import { EmailLog, useEmailLogGetByIdQuery } from "@Pimcore/modules/email/emails-api-slice.gen"
+import { Alert } from "@sdk/components"
+import React from "react"
+
+interface EmailErrorProps {
+  email: EmailLog
+}
+
+export const EmailError = ({ email }: EmailErrorProps): React.JSX.Element => {
+  const { data, isLoading } = useEmailLogGetByIdQuery({ id: email.id })
+
+  if (isLoading) {
+    return <></>
+  }
+
+  return (
+    <Alert
+      description={data?.error || ''}
+      showIcon
+      type="error"
+    />
+  )
+}
