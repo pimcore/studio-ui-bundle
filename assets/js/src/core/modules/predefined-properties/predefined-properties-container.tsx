2ba47b16dd60cf564790d5d368af477f90fc13e9
xIrusux <42359615+xIrusux@users.noreply.github.com>
2025-06-30 11:24:32 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/predefined-properties/predefined-properties-container.tsx
+++ assets/js/src/core/modules/predefined-properties/predefined-properties-container.tsx
@@ -53,7 +53,7 @@ export const PredefinedPropertiesContainer = (): React.JSX.Element => {
   }, [predefinedProperties])
 
   useEffect(() => {
-if(!isUndefined(error)) {
+    if (!isUndefined(error)) {
       trackError(new ApiError(error))
     }
   }, [error])
