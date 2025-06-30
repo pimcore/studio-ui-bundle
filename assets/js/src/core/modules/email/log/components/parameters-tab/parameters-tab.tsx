767f15c9fa6b62020d1f4ba533eb64bf108cf554
Corepex <christian.feldkirchner@pimcore.com>
2025-06-30 10:05:54 +0200

    fixed linter bugs

--- assets/js/src/core/modules/email/log/components/parameters-tab/parameters-tab.tsx
+++ assets/js/src/core/modules/email/log/components/parameters-tab/parameters-tab.tsx
@@ -1,11 +1,21 @@
-import { Content } from "@Pimcore/components/content/content"
-import { Grid } from "@Pimcore/components/grid/grid"
-import { EmailLog, EmailLogObjectParameterData, EmailLogParameters, useEmailLogGetParamsQuery } from "@Pimcore/modules/email/emails-api-slice.gen"
-import { addColumnMeta, DefaultCell } from "@sdk/components"
-import { ColumnDef, createColumnHelper } from "@tanstack/react-table"
-import { isNil, isUndefined } from "lodash"
-import React from "react"
-import { useTranslation } from "react-i18next"
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { Content } from '@Pimcore/components/content/content'
+import { Grid } from '@Pimcore/components/grid/grid'
+import { type EmailLog, type EmailLogObjectParameterData, type EmailLogParameters, useEmailLogGetParamsQuery } from '@Pimcore/modules/email/emails-api-slice.gen'
+import { addColumnMeta, DefaultCell } from '@sdk/components'
+import { type ColumnDef, createColumnHelper } from '@tanstack/react-table'
+import { isNil, isUndefined } from 'lodash'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 interface ParametersTabProps {
   email: EmailLog
@@ -22,50 +32,52 @@ export const ParametersTab = ({ email }: ParametersTabProps): React.JSX.Element 
   const columnHelper = createColumnHelper<ExtendedEmailLogParameters>()
   const columns: Array<ColumnDef<ExtendedEmailLogParameters>> = [
     columnHelper.accessor('name', {
-      header: t('widget.email-log.grid.name'),
+      header: t('widget.email-log.grid.name')
     }),
     columnHelper.accessor('computedValue', {
       id: 'computedValue',
       header: t('widget.email-log.grid.value'),
       meta: {
-        editable: false,
+        editable: false
       },
       cell: (info) => {
         const row: ExtendedEmailLogParameters = info.row.original
 
-        return <DefaultCell
-          {...addColumnMeta(info, { type: isNil(row.objectData) ? 'text' : 'element' })}
-        />
+        return (
+          <DefaultCell
+            { ...addColumnMeta(info, { type: isNil(row.objectData) ? 'text' : 'element' }) }
+          />
+        )
       }
     })
   ]
 
   const dataToLoop = data?.data ?? []
   const extendedData: ExtendedEmailLogParameters[] = dataToLoop.map((parameter) => {
-    let computedValue;
+    let computedValue
 
     if (!isNil(parameter.objectData)) {
       computedValue = {
         ...parameter.objectData,
-        fullPath: parameter.objectData!.path
+        fullPath: parameter.objectData.path
       }
     }
 
     return {
       ...parameter,
-      computedValue: computedValue || parameter.value,
+      computedValue: computedValue ?? parameter.value
     }
   })
 
   return (
     <Content
-      loading={isLoading}
-      none={isUndefined(data?.data) || data.data.length === 0}
+      loading={ isLoading }
+      none={ isUndefined(data?.data) || data.data.length === 0 }
     >
       <Grid
         autoWidth
-        columns={columns}
-        data={extendedData}
+        columns={ columns }
+        data={ extendedData }
       />
     </Content>
   )
--- assets/js/src/core/modules/email/log/components/text-preview/text-preview.tsx
+++ assets/js/src/core/modules/email/log/components/text-preview/text-preview.tsx
@@ -1,10 +1,20 @@
-import { Content } from "@Pimcore/components/content/content"
-import { getLanguageExtensions } from "@Pimcore/components/text-editor/detect-language"
-import { EmailLog, useEmailLogGetTextQuery } from "@Pimcore/modules/email/emails-api-slice.gen"
-import ReactCodeMirror from "@uiw/react-codemirror"
-import { isUndefined } from "lodash"
-import React from "react"
-import { useStyles } from "./text-preview.styles"
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { Content } from '@Pimcore/components/content/content'
+import { getLanguageExtensions } from '@Pimcore/components/text-editor/detect-language'
+import { type EmailLog, useEmailLogGetTextQuery } from '@Pimcore/modules/email/emails-api-slice.gen'
+import ReactCodeMirror from '@uiw/react-codemirror'
+import { isUndefined } from 'lodash'
+import React from 'react'
+import { useStyles } from './text-preview.styles'
 
 interface TextPreviewProps {
   email: EmailLog
@@ -16,19 +26,19 @@ export const TextPreview = ({ email }: TextPreviewProps): React.JSX.Element => {
 
   return (
     <Content
-      loading={isLoading}
-      none={isUndefined(data?.data) || data.data.length === 0}
+      loading={ isLoading }
+      none={ isUndefined(data?.data) || data.data.length === 0 }
     >
       <ReactCodeMirror
-        basicSetup={{
+        basicSetup={ {
           lineNumbers: true,
           syntaxHighlighting: true,
           searchKeymap: true
-        }}
-        className={styles.codeEditor}
-        readOnly={true}
-        value={data?.data || ""}
-        extensions={getLanguageExtensions('html')}
+        } }
+        className={ styles.codeEditor }
+        extensions={ getLanguageExtensions('html') }
+        readOnly
+        value={ data?.data ?? '' }
       />
     </Content>
   )
