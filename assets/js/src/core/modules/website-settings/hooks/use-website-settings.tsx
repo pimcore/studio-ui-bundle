14589d88b31e6e4f375755f0c5f0f9c793062cfb
xIrusux <christine.altmann@gmx.com>
2025-07-07 15:40:33 +0200

    improve error handling

--- assets/js/src/core/modules/website-settings/hooks/use-website-settings.tsx
+++ assets/js/src/core/modules/website-settings/hooks/use-website-settings.tsx
@@ -8,9 +8,10 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
-import trackError, { GeneralError } from '@Pimcore/modules/app/error-handler'
+import trackError, { ApiError, GeneralError } from '@Pimcore/modules/app/error-handler'
 import { type WebsiteSettingRow } from '../website-settings-container'
 import { useWebsiteSettingsAddMutation, useWebsiteSettingsDeleteMutation, useWebsiteSettingsUpdateMutation, type WebsiteSetting, type WebsiteSettingsUpdate } from '../website-settings-api-slice-enhanced'
+import { isUndefined } from 'lodash'
 
 interface UseWebsiteSettingReturn {
   createNewSetting: (name: string, type: string) => Promise<{ success: boolean, data?: WebsiteSetting }>
@@ -29,11 +30,16 @@ export const useWebsiteSetting = (): UseWebsiteSettingReturn => {
   const createNewSetting = async (name: string, type: string): Promise<{ success: boolean, data?: WebsiteSetting }> => {
     try {
       const result = await createSetting({ websiteSettingsAdd: { name, type } })
+      
+      if (!isUndefined(result.error)) {
+        trackError(new ApiError(result.error))
+      }
+
       if ('data' in result) {
         return { success: true, data: result.data }
       }
     } catch {
-      trackError(new GeneralError('Was not able to create Website Setting'))
+      trackError(new GeneralError('Error creating Website Settings'))
     }
     return { success: false }
   }
@@ -41,9 +47,14 @@ export const useWebsiteSetting = (): UseWebsiteSettingReturn => {
   const deleteSettingById = async (id: number): Promise<{ success: boolean }> => {
     try {
       const result = await deleteSetting({ id })
+
+      if (!isUndefined(result.error)) {
+        trackError(new ApiError(result.error))
+      }
+
       return { success: 'data' in result }
     } catch {
-      trackError(new GeneralError('Was not able to delete Website Setting'))
+      trackError(new GeneralError('Error deleting Website Settings'))
       return { success: false }
     }
   }
@@ -58,9 +69,14 @@ export const useWebsiteSetting = (): UseWebsiteSettingReturn => {
   const updateSettingById = async (id: number, row: WebsiteSettingRow): Promise<{ success: boolean }> => {
     try {
       const result = await updateSetting({ id, websiteSettingsUpdate: toApiSetting(row) })
+
+      if (!isUndefined(result.error)) {
+        trackError(new ApiError(result.error))
+      }
+
       return { success: 'data' in result }
     } catch {
-      trackError(new GeneralError('Was not able to update Website Setting'))
+      trackError(new GeneralError('Error updating Website Settings'))
       return { success: false }
     }
   }
