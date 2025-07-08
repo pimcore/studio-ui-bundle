d385fbe365ed17b7b8f72a6204800fdb6b823243
Corepex <16717695+Corepex@users.noreply.github.com>
2025-07-08 07:06:15 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/email/blocklist/email-blocklist-container.tsx
+++ assets/js/src/core/modules/email/blocklist/email-blocklist-container.tsx
@@ -45,7 +45,7 @@ export const EmailBlocklistContainer = (): React.JSX.Element => {
   }
 
   useEffect(() => {
-    if (isFetching === false) {
+    if (!isFetching) {
       setIsLoading(false)
     }
   }, [isFetching])
@@ -58,69 +58,69 @@ export const EmailBlocklistContainer = (): React.JSX.Element => {
           theme='secondary'
         >
           <IconButton
-            disabled={isLoading || isRTKLoading}
-            icon={{ value: 'refresh' }}
-            onClick={() => {
+            disabled={ isLoading || isRTKLoading }
+            icon={ { value: 'refresh' } }
+            onClick={ () => {
               setIsLoading(true)
               dispatch(
                 api.util.invalidateTags(
                   invalidatingTags.EMAIL_BLOCKLIST()
                 )
               )
-            }}
+            } }
           />
           <Pagination
-            current={currentPage}
-            defaultPageSize={pageSize}
-            onChange={onPagerChange}
+            current={ currentPage }
+            defaultPageSize={ pageSize }
+            onChange={ onPagerChange }
             showSizeChanger
-            showTotal={(total) => t('pagination.show-total', { total })}
-            total={total}
+            showTotal={ (total) => t('pagination.show-total', { total }) }
+            total={ total }
           />
         </Toolbar>
       }
       renderTopBar={
         <Toolbar
           justify='space-between'
-          margin={{
+          margin={ {
             x: 'mini',
             y: 'none'
-          }}
+          } }
           theme='secondary'
         >
-          <Flex gap={'small'}>
+          <Flex gap={ 'small' }>
             <Title
-              icon={<Icon value="users-x" />}
+              icon={ <Icon value="users-x" /> }
             >
               {t('widget.email-blocklist')}
             </Title>
             <IconTextButton
-              disabled={isLoading || isRTKLoading}
-              icon={{ value: 'new' }}
-              onClick={async () => {
+              disabled={ isLoading || isRTKLoading }
+              icon={ { value: 'new' } }
+              onClick={ async () => {
                 await addNewEmail(() => {
                   setIsLoading(false)
                 })
-              }}
+              } }
             >{t('email-blocklist.add')}</IconTextButton>
           </Flex>
         </Toolbar>
       }
     >
       <Content
-        loading={isLoading}
-        none={isUndefined(data?.items) || data.items.length === 0}
+        loading={ isLoading }
+        none={ isUndefined(data?.items) || data.items.length === 0 }
         padded
       >
         {data?.items !== undefined && data?.items?.length > 0
           ? (
-            data.items.map((item) => (
-              <EmailCard
-                entry={item}
-                key={item.email}
-              />
-            ))
-          )
+              data.items.map((item) => (
+                <EmailCard
+                  entry={ item }
+                  key={ item.email }
+                />
+              ))
+            )
           : ''}
       </Content>
     </ContentLayout>
