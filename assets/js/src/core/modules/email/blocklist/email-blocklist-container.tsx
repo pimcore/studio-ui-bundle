1a23feb4c26b4d627522056f6121230912a1da67
Corepex <christian.feldkirchner@pimcore.com>
2025-07-08 09:02:15 +0200

    streamlines loading

--- assets/js/src/core/modules/email/blocklist/email-blocklist-container.tsx
+++ assets/js/src/core/modules/email/blocklist/email-blocklist-container.tsx
@@ -19,7 +19,7 @@ import { Toolbar } from '@Pimcore/components/toolbar/toolbar'
 import { api } from '@Pimcore/modules/email/emails-api-slice-enhanced'
 import { invalidatingTags } from '@sdk/api'
 import { Icon, Pagination } from '@sdk/components'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useEmailBlocklistGetCollectionQuery } from '../emails-api-slice.gen'
 import { EmailCard } from './components/email-card/email-card'
@@ -33,7 +33,7 @@ export const EmailBlocklistContainer = (): React.JSX.Element => {
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [pageSize, setPageSize] = useState<number>(20)
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const { data, isLoading: isRTKLoading } = useEmailBlocklistGetCollectionQuery({
+  const { data, isLoading: isRTKLoading, isFetching } = useEmailBlocklistGetCollectionQuery({
     page: currentPage,
     pageSize
   })
@@ -44,6 +44,12 @@ export const EmailBlocklistContainer = (): React.JSX.Element => {
     setPageSize(pageSize)
   }
 
+  useEffect(() => {
+    if (isFetching === false) {
+      setIsLoading(false)
+    }
+  }, [isFetching])
+
   return (
     <ContentLayout
       renderToolbar={
@@ -52,71 +58,69 @@ export const EmailBlocklistContainer = (): React.JSX.Element => {
           theme='secondary'
         >
           <IconButton
-            disabled={ isLoading || isRTKLoading }
-            icon={ { value: 'refresh' } }
-            onClick={ () => {
+            disabled={isLoading || isRTKLoading}
+            icon={{ value: 'refresh' }}
+            onClick={() => {
               setIsLoading(true)
               dispatch(
                 api.util.invalidateTags(
                   invalidatingTags.EMAIL_BLOCKLIST()
                 )
               )
-              setIsLoading(false)
-            }
-            }
+            }}
           />
           <Pagination
-            current={ currentPage }
-            defaultPageSize={ pageSize }
-            onChange={ onPagerChange }
+            current={currentPage}
+            defaultPageSize={pageSize}
+            onChange={onPagerChange}
             showSizeChanger
-            showTotal={ (total) => t('pagination.show-total', { total }) }
-            total={ total }
+            showTotal={(total) => t('pagination.show-total', { total })}
+            total={total}
           />
         </Toolbar>
       }
       renderTopBar={
         <Toolbar
           justify='space-between'
-          margin={ {
+          margin={{
             x: 'mini',
             y: 'none'
-          } }
+          }}
           theme='secondary'
         >
-          <Flex gap={ 'small' }>
+          <Flex gap={'small'}>
             <Title
-              icon={ <Icon value="users-x" /> }
+              icon={<Icon value="users-x" />}
             >
               {t('widget.email-blocklist')}
             </Title>
             <IconTextButton
-              disabled={ isLoading || isRTKLoading }
-              icon={ { value: 'new' } }
-              onClick={ async () => {
+              disabled={isLoading || isRTKLoading}
+              icon={{ value: 'new' }}
+              onClick={async () => {
                 await addNewEmail(() => {
                   setIsLoading(false)
                 })
-              } }
+              }}
             >{t('email-blocklist.add')}</IconTextButton>
           </Flex>
         </Toolbar>
       }
     >
       <Content
-        loading={ isLoading }
-        none={ isUndefined(data?.items) || data.items.length === 0 }
+        loading={isLoading}
+        none={isUndefined(data?.items) || data.items.length === 0}
         padded
       >
         {data?.items !== undefined && data?.items?.length > 0
           ? (
-              data.items.map((item) => (
-                <EmailCard
-                  entry={ item }
-                  key={ item.email }
-                />
-              ))
-            )
+            data.items.map((item) => (
+              <EmailCard
+                entry={item}
+                key={item.email}
+              />
+            ))
+          )
           : ''}
       </Content>
     </ContentLayout>
