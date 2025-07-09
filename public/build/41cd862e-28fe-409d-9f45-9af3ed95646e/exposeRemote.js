796798d14acab0e869b14c26569986eae9163bb7
markus-moser <markus.moser@pimcore.com>
2025-07-09 17:49:38 +0200

    Add Copilot instructions (#1684)

--- public/build/41cd862e-28fe-409d-9f45-9af3ed95646e/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/41cd862e-28fe-409d-9f45-9af3ed95646e/static/js/remoteEntry.js'
-    
---
+++ public/build/625707a7-9df3-43a4-a2f8-92895c0ad5ce/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/625707a7-9df3-43a4-a2f8-92895c0ad5ce/static/js/remoteEntry.js'
+    
