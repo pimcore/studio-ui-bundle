4d23ab35ee4811588dbb82b92b434c4a2911713f
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-07-10 09:35:58 +0200

    added the test code

--- assets/js/src/core/modules/reports/reports-view/components/report-chart/components/pie-chart/pie-chart.tsx
+++ assets/js/src/core/modules/reports/reports-view/components/report-chart/components/pie-chart/pie-chart.tsx
@@ -9,14 +9,64 @@
  */
 
 import React, { useEffect, useState } from 'react'
-import { Flex } from '@Pimcore/components/flex/flex'
 import { Pie } from '@ant-design/plots'
 import { isEmpty } from 'lodash'
-import { ChartLegend } from '@Pimcore/modules/reports/reports-view/components/report-chart/components/pie-chart/components/chart-legend/chart-legend'
+// import { ChartLegend } from '@Pimcore/modules/reports/reports-view/components/report-chart/components/pie-chart/components/chart-legend/chart-legend'
 import { generateRandomColors } from '@Pimcore/modules/reports/reports-view/components/report-chart/utils/helpers'
 import type { CustomReportChartData, CustomReportDetails } from '@Pimcore/modules/reports/custom-reports-api-slice.gen'
 import type { IChartDataItem } from '@Pimcore/modules/reports/reports-view/components/report-chart/types'
-
+const MOCK_CHART_DATA = [
+  { type: 'document', value: 7 },
+  { type: 'folder', value: 48 },
+  { type: 'image', value: 337 },
+  { type: 'text', value: 2 },
+  { type: 'video', value: 1 },
+  { type: 'audio', value: 15 },
+  { type: 'pdf', value: 9 },
+  { type: 'spreadsheet', value: 4 },
+  { type: 'presentation', value: 6 },
+  { type: 'archive', value: 3 },
+  { type: 'code', value: 5 },
+  { type: 'executable', value: 2 },
+  { type: 'script', value: 4 },
+  { type: 'font', value: 1 },
+  { type: 'ebook', value: 3 },
+  { type: 'config', value: 6 },
+  { type: 'markdown', value: 7 },
+  { type: 'csv', value: 8 },
+  { type: 'xml', value: 5 },
+  { type: 'json', value: 4 },
+  { type: 'yaml', value: 2 },
+  { type: 'log', value: 9 },
+  { type: 'certificate', value: 1 },
+  { type: 'database', value: 2 },
+  { type: 'iso', value: 3 },
+  { type: 'backup', value: 4 },
+  { type: 'bin', value: 2 },
+  { type: 'apk', value: 1 },
+  { type: 'template', value: 3 },
+  { type: 'readme', value: 5 },
+  { type: 'key', value: 1 },
+  { type: 'license', value: 2 },
+  { type: 'thumbnail', value: 4 },
+  { type: 'resource', value: 3 },
+  { type: 'draft', value: 2 },
+  { type: 'note', value: 7 },
+  { type: 'svg', value: 6 },
+  { type: 'png', value: 5 },
+  { type: 'jpg', value: 11 },
+  { type: 'gif', value: 3 },
+  { type: 'webp', value: 1 },
+  { type: 'tiff', value: 1 },
+  { type: 'psd', value: 2 },
+  { type: 'ai', value: 1 },
+  { type: 'sketch', value: 1 },
+  { type: 'fig', value: 1 },
+  { type: 'csv-export', value: 2 },
+  { type: 'report', value: 5 },
+  { type: 'manual', value: 3 },
+  { type: 'faq', value: 2 }
+]
 export interface IPieChartProps {
   chartData: CustomReportChartData[]
   reportData: CustomReportDetails
@@ -34,7 +84,7 @@ export const PieChart = ({ reportData, chartData }: IPieChartProps): React.JSX.E
     [CHART_FIELD_VALUE_KEY]: item?.[pieColumn]
   }))
 
-  const [colorList] = useState<string[]>(generateRandomColors(reportChartData?.length))
+  const [colorList] = useState<string[]>(generateRandomColors(MOCK_CHART_DATA?.length))
   const [disabledItems, setDisabledItems] = useState<string[]>([])
   const [chartRef, setChartRef] = useState<any>(null)
   const [totalCount, setTotalCount] = useState<number>(0)
@@ -64,8 +114,10 @@ export const PieChart = ({ reportData, chartData }: IPieChartProps): React.JSX.E
     }
   }
 
+  console.log('reportChartData', handleLegendItemClick)
+
   const config = {
-    data: reportChartData,
+    data: MOCK_CHART_DATA,
     colorField: CHART_FIELD_TYPE_KEY,
     angleField: CHART_FIELD_VALUE_KEY,
     autoFit: true,
@@ -76,7 +128,16 @@ export const PieChart = ({ reportData, chartData }: IPieChartProps): React.JSX.E
       }
     },
     innerRadius: 0.6,
-    legend: false,
+    legend: {
+      color: {
+        position: 'right',
+        layout: {
+          justifyContent: 'center'
+        },
+        rowPadding: 10,
+        itemLabelFontSize: 14
+      }
+    },
     onReady: (plot: any) => {
       setChartRef(plot)
     },
@@ -105,16 +166,16 @@ export const PieChart = ({ reportData, chartData }: IPieChartProps): React.JSX.E
   }
 
   return (
-    <Flex align="center">
-      <Flex style={ { width: '100%' } }>
+    <div>
+      <div>
         <Pie { ...config } />
-      </Flex>
-      <ChartLegend
-        colorList={ colorList }
-        data={ reportChartData }
-        disabledItems={ disabledItems }
-        handleLegendItemClick={ handleLegendItemClick }
-      />
-    </Flex>
+      </div>
+      {/* <ChartLegend */}
+      {/*  colorList={ colorList } */}
+      {/*  data={ reportChartData } */}
+      {/*  disabledItems={ disabledItems } */}
+      {/*  handleLegendItemClick={ handleLegendItemClick } */}
+      {/* /> */}
+    </div>
   )
 }
