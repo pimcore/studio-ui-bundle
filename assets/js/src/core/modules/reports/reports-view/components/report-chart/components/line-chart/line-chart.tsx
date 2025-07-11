f6ed1a701f1609276203da6acfa14ed11c62b6a6
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-07-11 15:27:09 +0200

    update the LineChart

--- assets/js/src/core/modules/reports/reports-view/components/report-chart/components/line-chart/line-chart.tsx
+++ assets/js/src/core/modules/reports/reports-view/components/report-chart/components/line-chart/line-chart.tsx
@@ -9,8 +9,9 @@
  */
 
 import React from 'react'
-import { Flex } from '@Pimcore/components/flex/flex'
 import { Line } from '@ant-design/plots'
+import { toNumber } from 'lodash'
+import { Flex } from '@Pimcore/components/flex/flex'
 import type { ChartProps, ReportDataProps } from '@Pimcore/modules/reports/reports-view/components/report-chart/types'
 
 export interface ILineChartProps {
@@ -18,22 +19,44 @@ export interface ILineChartProps {
   reportData: ReportDataProps
 }
 
+const CHART_FIELD_NAME_KEY = 'name'
+const CHART_FIELD_VALUE_KEY = 'value'
+
 export const LineChart = ({ chartData, reportData }: ILineChartProps): React.JSX.Element => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const xAxis: string = reportData?.xAxis ?? ''
+  const formattedChartData = chartData.flatMap((item: object) => {
+    return Object.entries(item)
+      .filter(([key]) => key !== xAxis)
+      .map(([key, value]) => ({
+        [xAxis]: item?.[xAxis],
+        [CHART_FIELD_NAME_KEY]: key,
+        [CHART_FIELD_VALUE_KEY]: toNumber(value)
+      }))
+  })
+
   const config = {
-    data: chartData,
-    xField: reportData?.xAxis,
-    yField: reportData?.yAxis,
+    data: formattedChartData,
+    xField: xAxis,
+    yField: CHART_FIELD_VALUE_KEY,
+    seriesField: CHART_FIELD_NAME_KEY,
+    colorField: CHART_FIELD_NAME_KEY,
     point: {
-      shapeField: 'square',
+      shapeField: 'circle',
       sizeField: 4
     },
-    interaction: {
-      tooltip: {
-        marker: false
+    legend: {
+      color: {
+        position: 'bottom',
+        labelFormatter: (text: any) => {
+          const customLabels: any = {
+            'count(*)': 'Count',
+            'sum(id)': 'Sum'
+          }
+          return customLabels[text] ?? text
+        }
       }
-    },
-    style: {
-      lineWidth: 2
     }
   }
 
