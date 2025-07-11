d676ab0d27fc78343546063c4eef80336eeeba8f
Corepex <christian.feldkirchner@pimcore.com>
2025-07-11 12:42:21 +0200

    cleanup

--- assets/js/src/core/modules/application-logger/components/detail-modal/detail-modal.tsx
+++ assets/js/src/core/modules/application-logger/components/detail-modal/detail-modal.tsx
@@ -1,32 +1,41 @@
-import { Flex } from "@Pimcore/components/flex/flex";
-import { Form } from "@Pimcore/components/form/form";
-import { Input } from "@Pimcore/components/input/input";
-import { Modal } from "@Pimcore/components/modal/modal";
-import { ModalTitle } from "@Pimcore/components/modal/modal-title/modal-title";
-import { TextArea } from "@Pimcore/components/textarea/textarea";
-import { isNil } from "lodash";
-import React from "react";
-import { useTranslation } from "react-i18next";
-import { BundleApplicationLoggerLogEntryWithActions } from "../table/table";
-import { ManyToOneRelation, ManyToOneRelationValueType } from "@Pimcore/modules/element/dynamic-types/definitions/objects/data-related/components/many-to-one-relation/many-to-one-relation";
-import { FieldWidthProvider } from "@Pimcore/modules/element/dynamic-types/definitions/objects/data-related/providers/field-width/field-width-provider";
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { Form } from '@Pimcore/components/form/form'
+import { Input } from '@Pimcore/components/input/input'
+import { Modal } from '@Pimcore/components/modal/modal'
+import { ModalTitle } from '@Pimcore/components/modal/modal-title/modal-title'
+import { TextArea } from '@Pimcore/components/textarea/textarea'
+import { ManyToOneRelation, type ManyToOneRelationValueType } from '@Pimcore/modules/element/dynamic-types/definitions/objects/data-related/components/many-to-one-relation/many-to-one-relation'
+import { FieldWidthProvider } from '@Pimcore/modules/element/dynamic-types/definitions/objects/data-related/providers/field-width/field-width-provider'
+import { isNil } from 'lodash'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { type BundleApplicationLoggerLogEntryWithActions } from '../table/table'
 
 interface DetailModalProps {
-  data: BundleApplicationLoggerLogEntryWithActions | null;
-  open: boolean;
-  setOpen: (open: boolean) => void;
+  data: BundleApplicationLoggerLogEntryWithActions | null
+  open: boolean
+  setOpen: (open: boolean) => void
 }
 
 export const DetailModal = (props: DetailModalProps): React.JSX.Element => {
-  const { t } = useTranslation();
-  const [form] = Form.useForm();
+  const { t } = useTranslation()
+  const [form] = Form.useForm()
 
   if (isNil(props.data)) {
     return <></>
   }
 
   const closeModel = (): void => {
-    props.setOpen(false);
+    props.setOpen(false)
   }
 
   const formData = {
@@ -36,71 +45,71 @@ export const DetailModal = (props: DetailModalProps): React.JSX.Element => {
     component: props.data.component ?? '',
     source: props.data.source ?? '',
     fileObject: {
-      fullPath: "/Product Data/Body-Styles/2-door roadster",
+      fullPath: '/Product Data/Body-Styles/2-door roadster',
       id: 6,
       isPublished: true,
-      subtype: "object",
-      type: "object"
-    } as ManyToOneRelationValueType,
+      subtype: 'object',
+      type: 'object'
+    } as ManyToOneRelationValueType
   }
 
   return (
     <Modal
-      open={props.open}
-      onClose={closeModel}
-      onCancel={closeModel}
-      onOk={closeModel}
-      title={(
+      onCancel={ closeModel }
+      onClose={ closeModel }
+      onOk={ closeModel }
+      open={ props.open }
+      title={ (
         <ModalTitle>
           {t('application-logger.detail-modal.title')}
         </ModalTitle>
-      )}
+      ) }
     >
       <FieldWidthProvider>
         <Form
+          form={ form }
+          initialValues={ formData }
           layout="vertical"
-          form={form}
-          initialValues={formData}
         >
           <Form.Item
-            label={t('application-logger.columns.timestamp')}
+            label={ t('application-logger.columns.timestamp') }
             name="date"
           >
             <Input readOnly />
           </Form.Item>
 
           <Form.Item
-            label={t('application-logger.columns.message')}
+            label={ t('application-logger.columns.message') }
             name="message"
           >
             <TextArea readOnly />
           </Form.Item>
 
           <Form.Item
-            label={t('application-logger.columns.type')}
+            label={ t('application-logger.columns.type') }
             name="priority"
           >
             <Input readOnly />
           </Form.Item>
 
           <Form.Item
-            label={t('application-logger.columns.component')}
+            label={ t('application-logger.columns.component') }
             name="component"
           >
             <Input readOnly />
           </Form.Item>
 
           <Form.Item
-            label={t('application-logger.columns.source')}
+            label={ t('application-logger.columns.source') }
             name="source"
           >
             <Input readOnly />
           </Form.Item>
 
-          {props.data.fileObject && (
+          {props.data.fileObject !== null && (
             <Form.Item
-              label={t('application-logger.columns.related-object')}
-              name={'fileObject'}
+              label={ t('application-logger.columns.related-object') }
+              name={ 'fileObject' }
             >
               <ManyToOneRelation
                 assetsAllowed
--- assets/js/src/core/modules/application-logger/components/sidebar/tabs/filter/filter-tab-container.tsx
+++ assets/js/src/core/modules/application-logger/components/sidebar/tabs/filter/filter-tab-container.tsx
@@ -1,9 +1,19 @@
-import { Button, Content, ContentLayout, DatePicker, Form, IconTextButton, Input, Space, Title, Toolbar } from "@sdk/components"
-import React from "react"
-import { useTranslation } from "react-i18next"
-import { ComponentSelect } from "./components/component-select/component-select"
-import { PrioritySelect } from "./components/priority-select/priority-select"
-import { useFilter } from "./provider/filter-provider/use-filter"
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { Button, Content, ContentLayout, DatePicker, Form, IconTextButton, Input, Space, Title, Toolbar } from '@sdk/components'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { ComponentSelect } from './components/component-select/component-select'
+import { PrioritySelect } from './components/priority-select/priority-select'
+import { useFilter } from './provider/filter-provider/use-filter'
 
 const DATE_FORMAT = 'YYYY-MM-DD HH:mm'
 
@@ -35,15 +45,15 @@ export const FilterTabContainer = (): React.JSX.Element => {
       renderToolbar={
         <Toolbar theme='secondary'>
           <IconTextButton
-            icon={{ value: 'close' }}
-            onClick={handleResetFilters}
+            icon={ { value: 'close' } }
+            onClick={ handleResetFilters }
             type='link'
           >
             {t('sidebar.clear-all-filters')}
           </IconTextButton>
 
           <Button
-            onClick={updateFilters}
+            onClick={ updateFilters }
             type='primary'
           >
             {t('button.apply')}
@@ -53,104 +63,104 @@ export const FilterTabContainer = (): React.JSX.Element => {
     >
       <Content padded>
         <Form
-          form={form}
+          form={ form }
           layout="vertical"
         >
           <Space
-            size="none"
             direction='vertical'
-            style={{ width: '100%' }}
+            size="none"
+            style={ { width: '100%' } }
           >
             <Title>{t('application-logger.sidebar.search-parameter')}</Title>
 
             <Form.Item
-              label={t('application-logger.filter.date-from')}
+              label={ t('application-logger.filter.date-from') }
               name="dateFrom"
             >
               <DatePicker
                 className="w-full"
-                format={DATE_FORMAT}
-                onChange={(value: string) => {
+                format={ DATE_FORMAT }
+                onChange={ (value: string) => {
                   setDateFrom(value)
-                }}
+                } }
                 outputType="dateString"
-                showTime={{ format: 'HH:mm' }}
-                value={dateFrom}
+                showTime={ { format: 'HH:mm' } }
+                value={ dateFrom }
               />
             </Form.Item>
 
             <Form.Item
-              label={t('application-logger.filter.date-to')}
+              label={ t('application-logger.filter.date-to') }
               name="dateTo"
             >
               <DatePicker
                 className="w-full"
-                format={DATE_FORMAT}
-                onChange={(value: string) => {
+                format={ DATE_FORMAT }
+                onChange={ (value: string) => {
                   setDateTo(value)
-                }}
+                } }
                 outputType="dateString"
-                showTime={{ format: 'HH:mm' }}
-                value={dateTo}
+                showTime={ { format: 'HH:mm' } }
+                value={ dateTo }
               />
             </Form.Item>
 
             <Form.Item
-              label={t('application-logger.filter.priority')}
+              label={ t('application-logger.filter.priority') }
               name="priority"
             >
               <PrioritySelect />
             </Form.Item>
 
             <Form.Item
-              label={t('application-logger.filter.component')}
+              label={ t('application-logger.filter.component') }
               name="component"
             >
               <ComponentSelect />
             </Form.Item>
 
             <Form.Item
-              label={t('application-logger.filter.related-object-id')}
+              label={ t('application-logger.filter.related-object-id') }
               name="relatedObjectId"
             >
               <Input
-                type="number"
                 min="0"
-                step="1"
-                value={relatedObjectId ?? undefined}
-                onChange={(e) => {
+                onChange={ (e) => {
                   const value = e.target.value
-                  setRelatedObjectId(value ? parseInt(value) : null)
-                }}
+                  setRelatedObjectId(value !== '' ? parseInt(value) : null)
+                } }
+                step="1"
+                type="number"
+                value={ relatedObjectId ?? undefined }
               />
             </Form.Item>
 
             <Form.Item
-              label={t('application-logger.filter.message')}
+              label={ t('application-logger.filter.message') }
               name="message"
             >
               <Input
-                value={message ?? undefined}
-                onChange={(e) => {
+                onChange={ (e) => {
                   const value = e.target.value
                   setMessage(value ?? null)
-                }}
+                } }
+                value={ message ?? undefined }
               />
             </Form.Item>
 
             <Form.Item
-              label={t('application-logger.filter.pid')}
+              label={ t('application-logger.filter.pid') }
               name="pid"
             >
               <Input
-                type="number"
                 min="0"
-                step="1"
-                value={pid ?? undefined}
-                onChange={(e) => {
+                onChange={ (e) => {
                   const value = e.target.value
-                  setPid(value ? parseInt(value) : null)
-                }}
+                  setPid(value !== '' ? parseInt(value) : null)
+                } }
+                step="1"
+                type="number"
+                value={ pid ?? undefined }
               />
             </Form.Item>
           </Space>
