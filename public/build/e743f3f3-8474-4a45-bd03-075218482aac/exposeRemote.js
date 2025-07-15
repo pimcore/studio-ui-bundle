1184f1b36ba32817234005c0bf7ee1dfbeb8a304
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-07-15 12:33:27 +0200

    merged changes from the main branch

--- public/build/e743f3f3-8474-4a45-bd03-075218482aac/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-<<<<<<<< HEAD:public/build/d792a722-9f4e-440c-a6ea-a6db55167660/exposeRemote.js
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/d792a722-9f4e-440c-a6ea-a6db55167660/static/js/remoteEntry.js'
-========
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/e743f3f3-8474-4a45-bd03-075218482aac/static/js/remoteEntry.js'
->>>>>>>> origin/1.x:public/build/e743f3f3-8474-4a45-bd03-075218482aac/exposeRemote.js
-    
