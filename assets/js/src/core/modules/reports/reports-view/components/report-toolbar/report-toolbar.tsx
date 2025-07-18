ef06ec6f616e2bd1605bf156eca5918920e6c66c
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-07-18 12:30:54 +0200

    added the CSV dropdown

--- assets/js/src/core/modules/reports/reports-view/components/report-toolbar/report-toolbar.tsx
+++ assets/js/src/core/modules/reports/reports-view/components/report-toolbar/report-toolbar.tsx
@@ -9,8 +9,14 @@
  */
 
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Toolbar } from '@Pimcore/components/toolbar/toolbar'
 import { Pagination } from '@Pimcore/modules/reports/components/pagination/pagination'
+import { Dropdown } from '@Pimcore/components/dropdown/dropdown'
+import { DropdownButton } from '@Pimcore/components/dropdown-button/dropdown-button'
+import { Icon } from '@Pimcore/components/icon/icon'
+import { Flex } from '@Pimcore/components/flex/flex'
+import { useStyles } from '@Pimcore/modules/reports/reports-view/reports-view.styles'
 
 interface IReportToolbarProps {
   page: number
@@ -21,11 +27,40 @@ interface IReportToolbarProps {
 }
 
 export const ReportToolbar = ({ page, setPage, pageSize, setPageSize, totalItems }: IReportToolbarProps): React.JSX.Element | null => {
+  const { t } = useTranslation()
+  const { styles } = useStyles()
+
+  const renderDropdownLabel = (translationKey: string): React.JSX.Element => (
+    <Flex
+      align={ 'center' }
+      className={ styles.dropdownLabel }
+      gap="extra-small"
+    >
+      <Icon value={ 'export' } />
+      {t(translationKey)}
+    </Flex>
+  )
+
+  const dropdownItems = [
+    {
+      key: 'csv-export-with-headers',
+      label: renderDropdownLabel('reports.csv-export-with-headers'),
+      onClick: () => {
+        console.log('Item 1 clicked')
+      }
+    }
+  ]
+
   return (
     <Toolbar
-      justify="flex-end"
+      justify="space-between"
       theme="secondary"
     >
+      <Dropdown menu={ { items: dropdownItems } }>
+        <DropdownButton>
+          {renderDropdownLabel('reports.csv-export')}
+        </DropdownButton>
+      </Dropdown>
       <Pagination
         page={ page }
         pageSize={ pageSize }
