a22ef1e02da89900db3ec8064669b447ea9b2790
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-07-30 11:25:32 +0200

    updated the comparison view

--- assets/js/src/core/modules/document/editor/shared-tab-manager/tabs/versions/comparison-view/comparison-view.tsx
+++ assets/js/src/core/modules/document/editor/shared-tab-manager/tabs/versions/comparison-view/comparison-view.tsx
@@ -13,21 +13,26 @@ import { type VersionComparisonViewProps } from '@Pimcore/modules/element/editor
 import { DocumentVersionsView } from '@Pimcore/modules/document/editor/shared-tab-manager/tabs/versions/components/document-versions-view/document-versions-view'
 import { isEmptyValue } from '@Pimcore/utils/type-utils'
 import { currentDomain } from '@Pimcore/app/config/app-config'
+import { useVersionUrl } from '@Pimcore/modules/document/editor/shared-tab-manager/tabs/versions/hooks/useVersionUrl'
 
 export const ComparisonView = ({ versionIds }: VersionComparisonViewProps): React.JSX.Element => {
   const [versionUrl, setVersionUrl] = useState<string | null>(null)
   const versionsIdList = versionIds.map((version) => version.count)
 
+  const mainVersionId = versionIds?.[0]?.id
+  const comparedVersionId = versionIds?.[1]?.id
+
+  const { url: mainVersionUrl } = useVersionUrl({ versionId: mainVersionId })
+
   useEffect(() => {
-    const mainVersionId = versionIds[0].id
-    const comparedVersionId = versionIds?.[1]?.id
+    if (!isEmptyValue(comparedVersionId)) {
+      const comparisonVersionUrl = `${currentDomain}/admin/document/diff-versions/from/${mainVersionId}/to/${comparedVersionId}`
 
-    if (!isEmptyValue(mainVersionId) && !isEmptyValue(comparedVersionId)) {
-      const url = `${currentDomain}/admin/document/diff-versions/from/${mainVersionId}/to/${comparedVersionId}`
-
-      setVersionUrl(url)
+      setVersionUrl(comparisonVersionUrl)
+    } else {
+      setVersionUrl(mainVersionUrl)
     }
-  }, [versionIds])
+  }, [versionIds, mainVersionUrl])
 
   return (
     <DocumentVersionsView
