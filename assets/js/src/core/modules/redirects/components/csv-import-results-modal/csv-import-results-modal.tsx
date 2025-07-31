53f39729f80a33e318f3cdae5755a3c6dbffc594
xIrusux <42359615+xIrusux@users.noreply.github.com>
2025-07-31 14:45:36 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/redirects/components/csv-import-results-modal/csv-import-results-modal.tsx
+++ assets/js/src/core/modules/redirects/components/csv-import-results-modal/csv-import-results-modal.tsx
@@ -14,7 +14,7 @@ import { t } from 'i18next'
 import { Icon } from '@Pimcore/components/icon/icon'
 import { useStyle } from './csv-import-results-modal.styles'
 import cn from 'classnames'
-import { BundleSeoRedirectsImportStatistics } from '../../seo-api-slice.gen'
+import { type BundleSeoRedirectsImportStatistics } from '../../seo-api-slice.gen'
 
 interface CsvImportResultsModalProps {
   open: boolean
