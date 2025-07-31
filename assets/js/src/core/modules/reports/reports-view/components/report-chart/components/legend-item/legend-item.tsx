40ee9e5de354cf29299a82e591cc359cf02a6ce3
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-07-31 11:01:42 +0200

    created the LegendItem component and updated the Line Chart

---
+++ assets/js/src/core/modules/reports/reports-view/components/report-chart/components/legend-item/legend-item.tsx
@@ -0,0 +1,44 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import React from 'react'
+import cn from 'classnames'
+import { Flex } from '@Pimcore/components/flex/flex'
+import { Text } from '@Pimcore/components/text/text'
+import { useStyles } from './legend-item.styles'
+
+interface ILegendItemProps {
+  key: string
+  handleClick: (data: any) => void
+  disabled: boolean
+  markerColor: string
+  label: string
+}
+
+export const LegendItem = ({ key, label, disabled, markerColor, handleClick }: ILegendItemProps): React.JSX.Element => {
+  const { styles } = useStyles()
+
+  return (
+    <Flex
+      align="center"
+      className={ cn(styles.legendItem, { [styles.legendItemDisabled]: disabled }) }
+      key={ key }
+      onClick={ handleClick }
+    >
+      <div
+        className={ styles.circle }
+        style={ { background: markerColor } }
+      />
+      <Flex gap="small">
+        <Text type="secondary">{label}</Text>
+      </Flex>
+    </Flex>
+  )
+}
