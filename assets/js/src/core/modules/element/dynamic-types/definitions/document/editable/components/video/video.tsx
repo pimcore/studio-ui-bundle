45270724917c7eaa54a4bffedde6d7acea339640
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-08-04 10:02:47 +0200

    fixed the Eslint error

--- assets/js/src/core/modules/element/dynamic-types/definitions/document/editable/components/video/video.tsx
+++ assets/js/src/core/modules/element/dynamic-types/definitions/document/editable/components/video/video.tsx
@@ -62,7 +62,7 @@ export const VideoComponent = ({
   useEffect(() => {
     if (!isNull(containerRef?.current)) {
       // Look for the empty video element
-      const emptyVideoElement = containerRef.current.querySelector('.pimcore_editable_video_empty')
+      const emptyVideoElement: HTMLDivElement | null = containerRef.current.querySelector('.pimcore_editable_video_empty')
       if (!isNull(emptyVideoElement)) {
         setEmptyElement(emptyVideoElement)
       }
