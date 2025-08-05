5259cda8e0d3e51723eba62647d7d3a8f5c14fcb
Tine <42359615+xIrusux@users.noreply.github.com>
2025-08-05 18:46:56 +0200

    Update redirects-container.tsx

--- assets/js/src/core/modules/redirects/redirects-container.tsx
+++ assets/js/src/core/modules/redirects/redirects-container.tsx
@@ -8,79 +8,168 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
-import type { DefaultCellProps } from '@Pimcore/components/grid/columns/default-cell'
-import React from 'react'
-import { Droppable } from '@Pimcore/components/drag-and-drop/droppable'
-import { type DragAndDropInfo } from '@sdk/components'
-import { ElementCellContent } from './element-cell-content'
-import { useStyle } from './element-cell.styles'
-import { type ElementType } from '@Pimcore/types/enums/element/element-type'
-import { isEmpty } from 'lodash'
-import { convertDragAndDropInfoToElementReference } from '@Pimcore/modules/element/element-helper'
+import React, { useEffect, useMemo, useState } from 'react'
+import { ContentLayout } from '@Pimcore/components/content-layout/content-layout'
+import { Content } from '@Pimcore/components/content/content'
+import { Table } from './table/table'
+import { Box } from '@sdk/components'
+import { api, useBundleSeoRedirectsGetCollectionQuery } from './seo-api-slice-enhanced'
+import trackError, { ApiError } from '../app/error-handler'
+import { uuid } from '@sdk/utils'
+import { type RedirectRow, useRedirects } from './hooks/use-redirects'
+import { isUndefined } from 'lodash'
+import { useAppDispatch } from '@sdk/app'
+import { invalidatingTags } from '@sdk/api'
+import { RedirectsToolbar } from './components/redirects-toolbar'
+import { RedirectsTopBar } from './components/redirects-top-bar'
+import { BeginnerRedirectModal } from './components/beginner-redirect-modal'
 
-export interface ElementInfo {
-  elementType?: ElementType
-  id?: number
-  fullPath: string | false
-  published?: boolean
-  disabled?: boolean
-}
+export const RedirectsContainer = (): React.JSX.Element => {
+  const dispatch = useAppDispatch()
 
-export interface ElementCellConfig {
-  allowedTypes?: ElementType[] | ((props: DefaultCellProps) => ElementType[])
-  getElementInfo?: (props: DefaultCellProps) => ElementInfo
-  expectsStringValue?: boolean
-  allowTextInput?: boolean
-}
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [pageSize, setPageSize] = useState<number>(50)
+  const [filter, setFilter] = useState<string>('')
+  const [isBeginnerModalOpen, setIsBeginnerModalOpen] = useState<boolean>(false)
 
-export const ElementCell = (props: DefaultCellProps): React.JSX.Element => {
-  const styles = useStyle().styles
-  const { column } = props
-  const editable = Boolean(props.column.columnDef.meta?.editable ?? true)
-  const clearable = Boolean(props.column.columnDef.meta?.clearable ?? true)
-  const showPublishedState = Boolean(props.column.columnDef.meta?.showPublishedState ?? true)
-  const config = column.columnDef.meta?.config as ElementCellConfig | null ?? {
-    allowedTypes: ['asset', 'data-object', 'document']
-  }
+  const {
+    createNewRedirect,
+    createLoading
+  } = useRedirects()
 
-  const allowedTypes = typeof config.allowedTypes === 'function' ? config.allowedTypes(props) : (config.allowedTypes ?? [])
+  const [redirectRows, setRedirectRows] = useState<RedirectRow[]>([])
 
-  function isValidContext (info: DragAndDropInfo): boolean {
-    return allowedTypes.includes(info.type as ElementType) && editable
-  }
-
-  function onDrop (info: DragAndDropInfo): void {
-    if (props.column.columnDef.meta?.editable !== undefined && props.table.options.meta?.onUpdateCellData !== undefined) {
-      const expectsStringValue = Boolean(props.column.columnDef.meta?.config?.expectsStringValue)
-
-      const value = expectsStringValue
-        ? info.data.fullPath
-        : convertDragAndDropInfoToElementReference(info, showPublishedState)
-
-      props.table.options.meta?.onUpdateCellData({
-        rowIndex: props.row.index,
-        columnId: props.column.id,
-        value,
-        rowData: props.row.original
-      })
+  const queryArgs = useMemo(() => ({
+    body: {
+      filters: {
+        page: currentPage,
+        pageSize,
+        columnFilters: filter !== '' ? { search: filter } : undefined
+      }
     }
+  }), [currentPage, pageSize, filter])
+
+  const {
+    data,
+    isLoading: redirectsLoading,
+    isFetching: redirectsFetching,
+    error
+  } = useBundleSeoRedirectsGetCollectionQuery(queryArgs)
+  const redirects = data?.items
+
+  const reload = (): void => {
+    dispatch(api.util.invalidateTags(invalidatingTags.REDIRECTS()))
   }
 
-  const dropDisabled = !editable || isEmpty(allowedTypes)
+  const handleCreateRedirect = async (redirectData?: { type: string, source: string, target: string }): Promise<boolean> => {
+    const tempId = uuid()
+    const optimisticRedirect: RedirectRow = {
+      id: Date.now(),
+      type: redirectData?.type ?? 'entire_uri',
+      source: redirectData?.source ?? null,
+      target: redirectData?.target ?? null,
+      sourceSite: null,
+      targetSite: null,
+      statusCode: 301,
+      priority: 1,
+      regex: false,
+      active: true,
+      passThroughParameters: false,
+      expiry: null,
+      creationDate: Date.now(),
+      modificationDate: Date.now(),
+      userOwner: null,
+      userModification: null,
+      additionalAttributes: undefined,
+      rowId: tempId
+    }
+
+    setRedirectRows(prev => [optimisticRedirect, ...prev])
+
+    const { success } = await createNewRedirect(redirectData)
+
+    if (!success) {
+      setRedirectRows(prev => prev.filter(row => row.rowId !== tempId))
+    }
+
+    return success
+  }
+
+  useEffect(() => {
+    if (!isUndefined(redirects)) {
+      setRedirectRows(
+        redirects.map(item => ({ ...item, rowId: uuid() }))
+      )
+    }
+  }, [redirects])
+
+  useEffect(() => {
+    if (!isUndefined(error)) {
+      trackError(new ApiError(error))
+    }
+  }, [error])
+
+  const handleSearch = (value: string): void => {
+    setFilter(value)
+    setCurrentPage(1)
+  }
+
+  const handlePageChange = (page: number, newPageSize: number): void => {
+    setCurrentPage(page)
+    setPageSize(newPageSize)
+  }
 
   return (
-    <Droppable
-      className={ [styles['element-cell'], 'default-cell__content'].join(' ') }
-      disabled={ dropDisabled }
-      isValidContext={ isValidContext }
-      onDrop={ onDrop }
+    <ContentLayout
+      renderToolbar={
+        <RedirectsToolbar
+          currentPage={ currentPage }
+          onPageChange={ handlePageChange }
+          onRefresh={ reload }
+          redirectRowsLength={ redirectRows.length }
+          redirectsFetching={ redirectsFetching }
+          totalItems={ data?.totalItems ?? 0 }
+        />
+      }
+      renderTopBar={
+        <RedirectsTopBar
+          createLoading={ createLoading }
+          onBeginnerClick={ () => { setIsBeginnerModalOpen(true) } }
+          onExpertClick={ async () => {
+            await handleCreateRedirect()
+          } }
+          onSearch={ handleSearch }
+          redirectsFetching={ redirectsFetching }
+          redirectsLoading={ redirectsLoading }
+        />
+      }
     >
-      <ElementCellContent
-        { ...props }
-        clearDisabled={ !clearable }
-        dropDisabled={ dropDisabled }
-        getElementInfo={ config.getElementInfo }
+      <Content
+        loading={ redirectsLoading || redirectsFetching }
+        margin={ {
+          x: 'extra-small',
+          y: 'none'
+        } }
+        none={ isUndefined(redirects) || redirects.length === 0 }
+      >
+        <Box
+          margin={ {
+            x: 'extra-small',
+            y: 'none'
+          } }
+        >
+          <Table
+            redirectRows={ redirectRows }
+            setRedirectRows={ setRedirectRows }
+          />
+        </Box>
+      </Content>
+
+      <BeginnerRedirectModal
+        createRedirect={ handleCreateRedirect }
+        open={ isBeginnerModalOpen }
+        setOpen={ setIsBeginnerModalOpen }
       />
-    </Droppable>
+    </ContentLayout>
   )
 }
