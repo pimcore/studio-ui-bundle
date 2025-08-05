24d80818efd33ccd36abc117ec9f058a51adfba0
vin0401 <marvin.bertram@pimcore.com>
2025-08-05 16:40:38 +0200

    Add missing translation

--- assets/js/src/core/components/pipeline/item/dynamic-group/dynamic-group-item.tsx
+++ assets/js/src/core/components/pipeline/item/dynamic-group/dynamic-group-item.tsx
@@ -20,6 +20,7 @@ import { CSS } from '@dnd-kit/utilities'
 import { useSortable } from '@dnd-kit/sortable'
 import { Box } from '@Pimcore/components/box/box'
 import { useStyles } from './dynamic-group-item.styles'
+import { useTranslation } from 'react-i18next'
 
 export interface DynamicGroupItemProps {
   id: number
@@ -30,6 +31,7 @@ const DynamicGroupItem = ({ id, dynamicTypeRegistryId }: DynamicGroupItemProps):
   const { operations, getValueByKey } = useNumberedList()
   const { styles } = useStyles()
   const { listeners, setNodeRef, setActivatorNodeRef, transform, transition } = useSortable({ id: id + 1 })
+  const { t } = useTranslation()
 
   const style = useMemo(() => ({
     transform: CSS.Transform.toString(transform),
@@ -69,7 +71,7 @@ const DynamicGroupItem = ({ id, dynamicTypeRegistryId }: DynamicGroupItemProps):
               variant="minimal"
               { ...listeners }
             />
-            <Text strong>{keyValue}</Text>
+            <Text strong>{t(`grid.advanced-column.advancedColumns.${keyValue}`)}</Text>
           </Flex>
 
           <IconButton
