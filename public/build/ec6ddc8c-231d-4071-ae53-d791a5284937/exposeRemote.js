d2b7b5ccc5eabe1ffe2b90d63bc19fb71f41637f
ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>
2025-08-07 11:17:35 +0000

    Automatic frontend build

---
+++ public/build/ec6ddc8c-231d-4071-ae53-d791a5284937/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/ec6ddc8c-231d-4071-ae53-d791a5284937/static/js/remoteEntry.js'
+    
---
+++ public/build/ec6ddc8c-231d-4071-ae53-d791a5284937/static/js/async/6585.f57553ec.js
@@ -0,0 +1,2 @@
+/*! For license information please see 6585.f57553ec.js.LICENSE.txt */
+"use strict";(self.webpackChunkpimcore_studio_ui_bundle=self.webpackChunkpimcore_studio_ui_bundle||[]).push([["6585"],{77975:function(e,i,l){l.r(i),l.d(i,{default:()=>t});var s=l(85893);l(81004);let t=e=>(0,s.jsx)("svg",{xmlns:"http://www.w3.org/2000/svg",width:"1em",height:"1em",viewBox:"0 0 640 480",...e,children:(0,s.jsxs)("g",{fillRule:"evenodd",strokeWidth:"1pt",children:[(0,s.jsx)("path",{fill:"#e70011",d:"M0 0h639.958v248.947H0z"}),(0,s.jsx)("path",{fill:"#fff",d:"M0 240h639.958v240H0z"})]})})}}]);
