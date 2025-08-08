92869f33723d7d23d4452d43b1fd62ddc3474c36
Corepex <16717695+Corepex@users.noreply.github.com>
2025-08-08 10:09:25 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/components/code-editor/code-editor.stories.tsx
+++ assets/js/src/core/components/code-editor/code-editor.stories.tsx
@@ -1,5 +1,15 @@
-import { Meta } from "@storybook/react/*";
-import { CodeEditor } from "./code-editor";
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { type Meta } from '@storybook/react/*'
+import { CodeEditor } from './code-editor'
 
 const config: Meta = {
   title: 'Components/Others/CodeEditor',
