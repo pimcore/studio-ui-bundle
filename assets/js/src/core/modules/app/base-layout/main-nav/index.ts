1bbb3c40bae07164e5cea854e4f279af1e779351
xIrusux <christine.altmann@gmx.com>
2025-08-12 15:40:18 +0200

    order main nav

--- assets/js/src/core/modules/app/base-layout/main-nav/index.ts
+++ assets/js/src/core/modules/app/base-layout/main-nav/index.ts
@@ -20,49 +20,57 @@ moduleSystem.registerModule({
     mainNavRegistryService.registerMainNavItem({
       path: 'QuickAccess',
       label: 'navigation.quick-access',
-      icon: 'quick-access'
+      icon: 'quick-access',
+      order: 100
     })
 
     mainNavRegistryService.registerMainNavItem({
       path: 'DataManagement',
       label: 'navigation.data-management',
-      icon: 'data-object'
+      icon: 'data-object',
+      order: 200
     })
 
     mainNavRegistryService.registerMainNavItem({
       path: 'ExperienceEcommerce',
       label: 'navigation.experience-ecommerce',
-      icon: 'experience-commerce'
+      icon: 'experience-commerce',
+      order: 300
     })
 
     mainNavRegistryService.registerMainNavItem({
       path: 'AssetManagement',
       label: 'navigation.asset-management',
-      icon: 'asset'
+      icon: 'asset',
+      order: 400
     })
 
     mainNavRegistryService.registerMainNavItem({
       path: 'AutomationIntegration',
       label: 'navigation.automation-integration',
-      icon: 'automation-integration'
+      icon: 'automation-integration',
+      order: 500
     })
 
     mainNavRegistryService.registerMainNavItem({
       path: 'Translations',
       label: 'navigation.translations',
-      icon: 'translate'
+      icon: 'translate',
+      order: 600
     })
 
     mainNavRegistryService.registerMainNavItem({
       path: 'Reporting',
       label: 'navigation.reporting',
-      icon: 'reporting'
+      icon: 'reporting',
+      order: 700
     })
 
     mainNavRegistryService.registerMainNavItem({
       path: 'System',
       label: 'navigation.system',
-      icon: 'shield'
+      icon: 'shield',
+      order: 800
     })
   }
 })
