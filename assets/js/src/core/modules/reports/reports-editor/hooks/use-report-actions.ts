c074631354c6685b66ed6d2337cda4dd0c025444
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-08-13 13:10:41 +0200

    refactored the useReportActions hook

--- assets/js/src/core/modules/reports/reports-editor/hooks/use-report-actions.ts
+++ assets/js/src/core/modules/reports/reports-editor/hooks/use-report-actions.ts
@@ -10,6 +10,7 @@
 
 import { useEffect } from 'react'
 import trackError, { ApiError } from '@Pimcore/modules/app/error-handler'
+import { type ApiErrorData } from '@Pimcore/modules/app/error-handler/classes/api-error'
 import {
   type CustomReportsConfigCloneApiArg,
   type CustomReportsConfigDeleteApiArg,
@@ -25,40 +26,34 @@ interface IUseReportActionsReturn {
   deleteReport: (arg: CustomReportsConfigDeleteApiArg) => Promise<void>
 }
 
+const useTrackMutationError = (isError: boolean, error: ApiErrorData | undefined): void => {
+  useEffect(() => {
+    if (isError) {
+      trackError(new ApiError(error!))
+    }
+  }, [isError, error])
+}
+
 export const useReportActions = (): IUseReportActionsReturn => {
   const [addReportMutation, { isError: isAddReportError, error: addReportError }] = useCustomReportsConfigAddMutation()
   const [cloneReportMutation, { isError: isCloneReportError, error: cloneReportError }] = useCustomReportsConfigCloneMutation()
   const [deleteReportMutation, { isError: isDeleteReportError, error: deleteReportError }] = useCustomReportsConfigDeleteMutation()
 
+  useTrackMutationError(isAddReportError, addReportError)
+  useTrackMutationError(isCloneReportError, cloneReportError)
+  useTrackMutationError(isDeleteReportError, deleteReportError)
+
   const addReport = async (arg: CustomReportsConfigAddApiArg): Promise<void> => {
     await addReportMutation(arg)
   }
-
-  useEffect(() => {
-    if (isAddReportError) {
-      trackError(new ApiError(addReportError))
-    }
-  }, [isAddReportError])
 
   const cloneReport = async (arg: CustomReportsConfigCloneApiArg): Promise<void> => {
     await cloneReportMutation(arg)
   }
 
-  useEffect(() => {
-    if (isCloneReportError) {
-      trackError(new ApiError(cloneReportError))
-    }
-  }, [isCloneReportError])
-
   const deleteReport = async (arg: CustomReportsConfigDeleteApiArg): Promise<void> => {
     await deleteReportMutation(arg)
   }
-
-  useEffect(() => {
-    if (isDeleteReportError) {
-      trackError(new ApiError(deleteReportError))
-    }
-  }, [isDeleteReportError])
 
   return {
     addReport,
