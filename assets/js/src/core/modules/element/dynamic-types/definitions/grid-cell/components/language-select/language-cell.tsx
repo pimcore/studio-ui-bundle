956f58c70e27cf3b9fa64bf6330b16b20a182fc0
xIrusux <christine.altmann@gmx.com>
2025-08-14 09:46:18 +0200

    use new allowed for viewing languages for languages available  in language cell

--- assets/js/src/core/modules/element/dynamic-types/definitions/grid-cell/components/language-select/language-cell.tsx
+++ assets/js/src/core/modules/element/dynamic-types/definitions/grid-cell/components/language-select/language-cell.tsx
@@ -9,32 +9,42 @@
  */
 
 import { type DefaultCellProps } from '@Pimcore/components/grid/columns/default-cell'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SelectCell, type SelectCellConfig } from '@Pimcore/modules/element/dynamic-types/definitions/grid-cell/components/select/select-cell'
 import { useSettings } from '@Pimcore/modules/app/settings/hooks/use-settings'
 import { addColumnConfig } from '@Pimcore/components/grid/columns/helpers'
-import { GeneralError, trackError } from '@sdk/modules/app'
-import { isUndefined } from 'lodash'
+import { ApiError, GeneralError, trackError } from '@sdk/modules/app'
+import { isUndefined, isArray } from 'lodash'
+import { useUserGetCurrentInformationQuery } from '@Pimcore/modules/user/user-api-slice-enhanced'
 
 export const LanguageCell = (props: DefaultCellProps): React.JSX.Element => {
   const settings = useSettings()
+  const { data: userInfo, error, } = useUserGetCurrentInformationQuery()
+
+  useEffect(() => {
+    if (!isUndefined(error)) {
+      trackError(new ApiError(error))
+    }
+  }, [error])
+
 
   const availableLanguages = settings.availableAdminLanguages
-  const validLanguages: string[] = settings.validLanguages
-  const languageSelectionOptions = validLanguages.map(validLang => {
-    const match = availableLanguages.find(lang => lang.language === validLang)
+  const allowedLanguagesForViewingTranslations = userInfo?.allowedLanguagesForViewingWebsiteTranslations as Array<{ language: string; display: string }> | undefined
+    const languageSelectionOptions = (isArray(allowedLanguagesForViewingTranslations) ? allowedLanguagesForViewingTranslations : [])?.map(lang => {
+    const match = availableLanguages.find(availableLang => availableLang.language === lang.language)
     if (isUndefined(match)) {
-      trackError(new GeneralError(`Language "${validLang}" not found in availableLanguages`))
+      trackError(new GeneralError(`Language "${lang.language}" not found in availableLanguages`))
     }
     return ({
-      value: match.language,
-      label: `${match.display} [${match.language}]`,
-      displayValue: match.language
+      value: match?.language ?? lang.language,
+      label: `${match?.display ?? lang.display} [${lang.language}]`,
+      displayValue: lang.language
     })
-  })
+  }) ?? []
 
+  
   const columnConfig: SelectCellConfig = {
-    options: languageSelectionOptions ?? []
+    options: languageSelectionOptions
   }
 
   return (
