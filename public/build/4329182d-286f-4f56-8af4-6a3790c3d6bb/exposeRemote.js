87a922c1647798af6db6fa1d20b969caf4810c67
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-08-18 12:36:28 +0200

    updated the API

--- public/build/4329182d-286f-4f56-8af4-6a3790c3d6bb/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-<<<<<<<< HEAD:public/build/f1b044f4-1a3b-4ab3-812f-d2f05e030dff/exposeRemote.js
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/f1b044f4-1a3b-4ab3-812f-d2f05e030dff/static/js/remoteEntry.js'
-========
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/4329182d-286f-4f56-8af4-6a3790c3d6bb/static/js/remoteEntry.js'
->>>>>>>> origin/1.x:public/build/4329182d-286f-4f56-8af4-6a3790c3d6bb/exposeRemote.js
-    
