fc6ee632ef92fd9271a43fdb243efc6c87638990
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-08-21 12:13:32 +0200

    [Reports] implement sorting for columns (#1934)
    * implemented sorting
    
    * fixed the api behavior
    
    * refactored the code
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

--- assets/js/src/core/modules/reports/reports-view/context/report-data-context.tsx
+++ assets/js/src/core/modules/reports/reports-view/context/report-data-context.tsx
@@ -18,15 +18,24 @@ interface IReportDataProviderProps {
   children: ReactNode
 }
 
+export interface ISorting {
+  sortBy: string
+  sortOrder: 'ASC' | 'DESC'
+}
+
 interface IReportDataContext extends IUseReportDataReturn {
   page: number
   setPage: (page: number) => void
   pageSize: number
   setPageSize: (pageSize: number) => void
+  sorting: ISorting | undefined
+  setSorting: (sorting: ISorting | undefined) => void
+  resetData: () => void
 }
 
 export const PAGE_INITIAL = 1
 export const PAGE_SIZE_INITIAL = 50
+export const SORTING_INITIAL = undefined
 
 const ReportDataContext = createContext<IReportDataContext | undefined>(undefined)
 
@@ -36,18 +45,28 @@ export const ReportDataProvider = ({
 }: IReportDataProviderProps): React.JSX.Element => {
   const [page, setPage] = useState(PAGE_INITIAL)
   const [pageSize, setPageSize] = useState(PAGE_SIZE_INITIAL)
+  const [sorting, setSorting] = useState<ISorting | undefined>(SORTING_INITIAL)
 
   const { filters } = useGridFilterContext()
 
-  const reportDataValue = useReportData({ name, filters, page, pageSize })
+  const resetData = (): void => {
+    setPage(PAGE_INITIAL)
+    setPageSize(PAGE_SIZE_INITIAL)
+    setSorting(SORTING_INITIAL)
+  }
+
+  const reportDataValue = useReportData({ name, filters, page, pageSize, sorting })
 
   const contextValue: IReportDataContext = useMemo(() => ({
     ...reportDataValue,
     page,
     setPage,
     pageSize,
-    setPageSize
-  }), [reportDataValue, page, setPage, pageSize, setPageSize])
+    setPageSize,
+    sorting,
+    setSorting,
+    resetData
+  }), [reportDataValue, page, setPage, pageSize, setPageSize, sorting, setSorting])
 
   return (
     <ReportDataContext.Provider value={ contextValue }>
