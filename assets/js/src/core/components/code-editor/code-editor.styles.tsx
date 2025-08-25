d1ee557a641b14808ed54605b6f64f0f35116691
vin0401 <marvin.bertram@pimcore.com>
2025-08-25 17:18:43 +0200

    Add code editor

--- assets/js/src/core/components/code-editor/code-editor.styles.tsx
+++ assets/js/src/core/components/code-editor/code-editor.styles.tsx
@@ -14,13 +14,39 @@ export const useStyles = createStyles(({ token, css }) => {
   return {
     editor: css`
       .cm-editor {
-        border: 1px solid #d9d9d9;
+        border: 1px solid ${token.colorBorder};
         border-radius: ${token.borderRadius}px;
         outline: none;
         overflow: auto;
+        font-family: ${token.fontFamilyCode};
+        font-size: ${token.fontSize}px;
+        line-height: 1.6;
 
         &.cm-focused {
           border-color: ${token.colorPrimary};
+          box-shadow: 0 0 0 2px ${token.colorPrimary}1A;
+        }
+
+        .cm-content {
+          padding: ${token.paddingXS}px ${token.paddingSM}px;
+          min-height: 120px;
+        }
+
+        .cm-gutters {
+          background-color: ${token.colorFillQuaternary};
+          border-right: 1px solid ${token.colorBorderSecondary};
+        }
+
+        .cm-lineNumbers {
+          color: ${token.colorTextTertiary};
+        }
+
+        .cm-activeLine {
+          background-color: ${token.colorFillTertiary};
+        }
+
+        .cm-selectionMatch {
+          background-color: ${token.colorPrimaryBg};
         }
       }
     `
