7da4674747b2525a5c7f6d6e5257d337fa68c2a4
xIrusux <christine.altmann@gmx.com>
2025-08-26 10:20:41 +0200

    make wysiwyg string agian

--- assets/js/src/core/modules/element/dynamic-types/definitions/objects/data-related/types/dynamic-type-object-data-wysiwyg.tsx
+++ assets/js/src/core/modules/element/dynamic-types/definitions/objects/data-related/types/dynamic-type-object-data-wysiwyg.tsx
@@ -48,7 +48,7 @@ export class DynamicTypeObjectDataWysiwyg extends DynamicTypeObjectDataAbstract 
     formLayout: 'vertical'
   }
 
-  protected dynamicTypeFieldFilterType: DynamicTypeFieldFilterAbstract = container.get(serviceIds['DynamicTypes/FieldFilter/Fulltext'])
+  protected dynamicTypeFieldFilterType: DynamicTypeFieldFilterAbstract = container.get(serviceIds['DynamicTypes/FieldFilter/String'])
 
   getObjectDataComponent (props: WysiwygObjectDataDefinition): React.ReactElement<AbstractObjectDataDefinition> {
     const parseConfig = (config?: string | null): Record<string, any> => {
