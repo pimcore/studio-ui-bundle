0b2e244615d17ebdb2ad5d7d711a16b2506a7140
vin0401 <marvin.bertram@pimcore.com>
2025-09-01 12:52:52 +0200

    Remove unused import

--- assets/js/src/core/modules/element/listing/batch-actions/xlsx-modal/xlsx-modal.tsx
+++ assets/js/src/core/modules/element/listing/batch-actions/xlsx-modal/xlsx-modal.tsx
@@ -16,7 +16,6 @@ import { createJob as createDownloadJob } from '@Pimcore/modules/execution-engin
 import { defaultTopics, topics } from '@Pimcore/modules/execution-engine/topics'
 import { ModalTitle } from '@Pimcore/components/modal/modal-title/modal-title'
 import { useTranslation } from 'react-i18next'
-import { appConfig } from '@Pimcore/app/config/app-config'
 import { useRowSelection } from '@Pimcore/modules/element/listing/decorators/row-selection/context-layer/provider/use-row-selection'
 import { useSelectedColumns } from '@Pimcore/modules/element/listing/abstract/configuration-layer/provider/selected-columns/use-selected-columns'
 import { useSettings } from '@Pimcore/modules/element/listing/abstract/settings/use-settings'
