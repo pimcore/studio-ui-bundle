88aa856bc055f4ebd434c76699a6aebfba2184c1
vin0401 <26813978+vin0401@users.noreply.github.com>
2025-09-01 11:07:03 +0000

    Apply eslint-fixer changes

--- assets/js/src/sdk/api/class-definition/index.ts
+++ assets/js/src/sdk/api/class-definition/index.ts
@@ -12,4 +12,4 @@ if (module.hot !== undefined) {
   module.hot.accept()
 }
 
-export * from '@Pimcore/modules/class-definition/class-definition-slice-enhanced';
+export * from '@Pimcore/modules/class-definition/class-definition-slice-enhanced'
