095dd66c9ab4d44356039e4d688fb4c708905009
markus-moser <markus.moser@pimcore.com>
2025-09-01 17:57:14 +0200

    Do not make the select editable editable by default (#2000)

---
+++ public/build/03cfeb94-ab7c-4d15-b872-3bc14e24bed6/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/03cfeb94-ab7c-4d15-b872-3bc14e24bed6/static/js/remoteEntry.js'
+    
--- public/build/dc275981-3b4b-49fd-b22e-b7fb1e35ba72/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/dc275981-3b4b-49fd-b22e-b7fb1e35ba72/static/js/remoteEntry.js'
-    
