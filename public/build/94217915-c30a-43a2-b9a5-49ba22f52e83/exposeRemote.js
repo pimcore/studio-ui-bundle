f33149ddb6c128e5941f360f7052f99b1c32f252
MSchinwaldEl <126564636+MSchinwaldEl@users.noreply.github.com>
2025-09-04 14:53:26 +0000

    Automatic frontend build

--- public/build/94217915-c30a-43a2-b9a5-49ba22f52e83/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/94217915-c30a-43a2-b9a5-49ba22f52e83/static/js/remoteEntry.js'
-    
---
+++ public/build/d8517b1d-0157-4557-acda-a94beab88925/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/d8517b1d-0157-4557-acda-a94beab88925/static/js/remoteEntry.js'
+    
