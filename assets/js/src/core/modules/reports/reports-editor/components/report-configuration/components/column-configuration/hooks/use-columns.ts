8d7d3527d4afa44192e7bb5653ea74bd376c18ca
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-09-05 15:41:28 +0200

    updated the useColumns

--- assets/js/src/core/modules/reports/reports-editor/components/report-configuration/components/column-configuration/hooks/use-columns.ts
+++ assets/js/src/core/modules/reports/reports-editor/components/report-configuration/components/column-configuration/hooks/use-columns.ts
@@ -16,29 +16,49 @@ export const useColumns = (): Array<AccessorKeyColumnDef<unknown, never>> => {
   return [
     columnHelper.accessor('name', {
       header: 'Name',
-      meta: {
-        type: 'text-cell'
-      }
+      meta: { type: 'text-cell' }
     }),
     columnHelper.accessor('display', {
       header: 'Display',
-      meta: {
-        type: 'checkbox',
-        editable: true
-      }
+      meta: { type: 'checkbox', editable: true }
     }),
     columnHelper.accessor('export', {
       header: 'Export',
-      meta: {
-        type: 'checkbox',
-        editable: true
-      }
+      meta: { type: 'checkbox', editable: true }
     }),
     columnHelper.accessor('order', {
       header: 'Order',
+      meta: { type: 'checkbox', editable: true }
+    }),
+    columnHelper.accessor('filterType', {
+      header: 'Filter Type',
       meta: {
-        type: 'checkbox',
-        editable: true
+        type: 'select',
+        editable: true,
+        config: {
+          options: [
+            { label: 'Empty', value: '' },
+            { label: 'Text', value: 'string' },
+            { label: 'Number', value: 'numeric' },
+            { label: 'Date', value: 'date' },
+            { label: 'Bool', value: 'boolean' }
+          ]
+        }
+      }
+    }),
+    columnHelper.accessor('displayType', {
+      header: 'Display Type',
+      meta: {
+        type: 'select',
+        editable: true,
+        config: {
+          options: [
+            { label: 'None', value: '' },
+            { label: 'Text', value: 'text' },
+            { label: 'Date', value: 'date' },
+            { label: 'Hide', value: 'hide' }
+          ]
+        }
       }
     })
   ]
