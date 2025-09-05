e5ba35ca73628dbc1d7e4fabfeb68c05ef383318
Christian F. <christian.feldkirchner@pimcore.com>
2025-09-05 09:05:21 +0200

    Widgets editor base form setup (#2014)
    * fixed loading issues, improved caching, added focus into create modal
    
    * renamed form interface
    
    * fixed cache tag issues
    
    * fixed perspective cache clear
    
    * linter fixes
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: Corepex <16717695+Corepex@users.noreply.github.com>

--- assets/js/src/core/modules/widget-editor/custom-view-editor/widget-editor-container-inner.tsx
+++ assets/js/src/core/modules/widget-editor/custom-view-editor/widget-editor-container-inner.tsx
@@ -9,7 +9,6 @@
  */
 
 import { ConfigLayout } from '@Pimcore/components/predefined-layouts/config/config-layout'
-import { ContentLayout } from '@sdk/components'
 import React from 'react'
 import { TreeContainer } from './components/tree/tree-container'
 import { WidgetDetailContainer } from './components/widget-detail/widget-detail-container'
@@ -34,11 +33,9 @@ export const WidgetEditorContainerInner = (): React.JSX.Element => {
   }
 
   return (
-    <ContentLayout>
-      <ConfigLayout
-        leftItem={ sidebar }
-        rightItem={ main }
-      />
-    </ContentLayout>
+    <ConfigLayout
+      leftItem={ sidebar }
+      rightItem={ main }
+    />
   )
 }
