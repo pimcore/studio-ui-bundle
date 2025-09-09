885de47fc8d0887001bc72753bebb0f0f5bb854f
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-09-09 09:32:10 +0200

    updated the SourceDefinition

--- assets/js/src/core/modules/reports/reports-editor/components/report-configuration/components/source-definition/source-definition.tsx
+++ assets/js/src/core/modules/reports/reports-editor/components/report-configuration/components/source-definition/source-definition.tsx
@@ -8,17 +8,60 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { isNull } from 'lodash'
 import { FormKit } from '@Pimcore/components/form/form-kit'
+import { Text } from '@Pimcore/components/text/text'
 import { type IReportConfigurationSectionProps } from '@Pimcore/modules/reports/reports-editor/types'
+import { Dropdown } from '@Pimcore/components/dropdown/dropdown'
+import { IconTextButton } from '@Pimcore/components/icon-text-button/icon-text-button'
+import { useStyles } from '@Pimcore/modules/reports/reports-editor/reports-editor.styles'
 
 export const SourceDefinition = ({ currentData }: IReportConfigurationSectionProps): React.JSX.Element => {
   const { t } = useTranslation()
+  const { styles } = useStyles()
+
+  const dataSourceConfig = currentData.dataSourceConfig
+  const isEmptySourceDefinitionConfig = isNull(dataSourceConfig)
+
+  const [currentSourceDefinition, setCurrentSourceDefinition] = useState<string | null>(null)
+
+  const dropdownItems = [
+    {
+      key: 'sql',
+      label: 'Sql',
+      onClick: () => { setCurrentSourceDefinition('sql') }
+    }
+  ]
+
+  const renderAddButton = (): React.JSX.Element => {
+    return (
+      <Dropdown
+        menu={ { items: dropdownItems } }
+        trigger={ ['click'] }
+      >
+        <IconTextButton
+          className={ styles.dropdownButton }
+          icon={ { value: 'plus-circle' } }
+        >
+          {t('add')}
+        </IconTextButton>
+      </Dropdown>
+    )
+  }
 
   return (
-    <FormKit.Panel title={ t('reports.editor.source-definition.title') }>
-      Content
+    <FormKit.Panel
+      extra={ isEmptySourceDefinitionConfig && renderAddButton() }
+      extraPosition="start"
+      title={ t('reports.editor.source-definition.title') }
+    >
+      {isEmptySourceDefinitionConfig && (
+        <Text type="secondary">
+          {t('reports.editor.source-definition.no-content')}
+        </Text>
+      )}
     </FormKit.Panel>
   )
 }
