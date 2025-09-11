c698f1e8e80436506c2897ccdbd4f673e2eab79f
xIrusux <christine.altmann@gmx.com>
2025-09-11 17:14:29 +0200

    fix up stories

--- assets/js/src/core/components/icon-selector/icon-selector.stories.tsx
+++ assets/js/src/core/components/icon-selector/icon-selector.stories.tsx
@@ -11,8 +11,10 @@
 import { type Meta, type StoryObj } from '@storybook/react'
 import { IconSelector } from './icon-selector'
 import React, { useState } from 'react'
-import { IconButton } from '@Pimcore/components/icon-button/icon-button'
 import { type ElementIcon } from '@Pimcore/modules/asset/asset-api-slice.gen'
+import { Form } from '@sdk/components'
+import { FormKit } from '@Pimcore/components/form/form-kit'
+import { Content } from '@Pimcore/components/content/content'
 
 const config: Meta = {
   title: 'Components/General/IconSelector',
@@ -27,31 +29,34 @@ export default config
 
 type Story = StoryObj<typeof config>
 
-const IconSelectorDemo = (): React.JSX.Element => {
-  const [selectedIcon, setSelectedIcon] = useState<ElementIcon | undefined>(undefined)
+interface IconFormData {
+  selectedIcon?: ElementIcon
+}
 
-  const handleChange = (icon: ElementIcon | undefined): void => {
-    setSelectedIcon(icon)
-  }
+const IconSelectorDemo = (): React.JSX.Element => {
+  const [form] = Form.useForm<IconFormData>()
+  const currentIcon = Form.useWatch('selectedIcon', form)
 
   return (
     <div style={ { padding: '20px' } }>
       <div style={ { marginBottom: '20px' } }>
-        <p>Current selected icon: <strong>{selectedIcon?.value ?? 'None'}</strong></p>
+        <p>Current selected icon: <strong>{currentIcon?.value ?? 'None'}</strong></p>
 
-        <IconSelector
-          onChange={ handleChange }
-          value={ selectedIcon }
+        <FormKit
+          formProps={ {
+            form,
+            layout: 'vertical'
+          } }
         >
-          {(openModal) => (
-            <IconButton
-              icon={ { value: selectedIcon?.value ?? 'edit' } }
-              onClick={ openModal }
+          <Content padded>
+            <Form.Item
+              label="Select Icon"
+              name="selectedIcon"
             >
-              Select Icon
-            </IconButton>
-          )}
-        </IconSelector>
+              <IconSelector />
+            </Form.Item>
+          </Content>
+        </FormKit>
       </div>
     </div>
   )
