fb62572deb45b886f6690afbee0e670a8d1a1833
vin0401 <26813978+vin0401@users.noreply.github.com>
2025-09-11 10:16:41 +0000

    Automatic frontend build

---
+++ public/build/5ba6d810-7d4e-4ebf-a9e0-8cfecf05964d/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/5ba6d810-7d4e-4ebf-a9e0-8cfecf05964d/static/js/remoteEntry.js'
+    
--- public/build/fb468e0a-177d-47a5-a3ec-4a8586ed18dc/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/fb468e0a-177d-47a5-a3ec-4a8586ed18dc/static/js/remoteEntry.js'
-    
