74a1b8e9ed99e2540eb588b7081842cacacc8066
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-09-15 16:22:24 +0200

    Studio UI Development - Custom Reports Editor - Editor Detail (#1931)
    * updated the ReportsEditor
    
    * updated the ReportConfiguration
    
    * created the useReportFormState hook
    
    * added the main functionality
    
    * updated approach by using the portal
    
    * refactored the code
    
    * updated the portal component
    
    * fixed the Sonar error
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    * updated the ReportConfiguration
    
    * restructured the code
    
    * updated the form approach
    
    * made the tabs sticky
    
    * Automatic frontend build
    
    * updated ChartSettings
    
    * updated the ChartSettings
    
    * updated the updated method
    
    * updated onValuesChange by handling empty chart state
    
    * updated handleSave
    
    * added helper file with the normalizers
    
    * Automatic frontend build
    
    * merged changes from the main branch
    
    * updated the UsersRolesDropdown component
    
    * updated the handleApplyChanges
    
    * fixed types
    
    * added styles
    
    * updated styles
    
    * fixed types
    
    * added the basic implementatiion of the ColumnConfiguration
    
    * created the useColumns hook
    
    * updated the useColumns
    
    * updated the useColumns
    
    * added the reordering
    
    * fixed styles
    
    * added the Refetch btn
    
    * updated the permission section
    
    * updated the GeneralSettings by adding translations and tooltips
    
    * added translations to the ColumnConfiguration
    
    * added translations to the ChartSettings
    
    * added loader state
    
    * fixed styles
    
    * fixed styles
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

---
+++ assets/js/src/core/modules/reports/reports-editor/components/report-configuration/components/chart-settings/chart-settings.tsx
@@ -0,0 +1,91 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import React, { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { FormKit } from '@Pimcore/components/form/form-kit'
+import { Select, type SelectProps } from '@Pimcore/components/select/select'
+import { Form } from '@Pimcore/components/form/form'
+import { Conditional } from '@Pimcore/components/form/conditional/conditional'
+import { CHART_TYPE_BAR, CHART_TYPE_LINE, CHART_TYPE_PIE } from '@Pimcore/modules/reports/constants'
+import type { IReportConfigurationSectionProps } from '@Pimcore/modules/reports/reports-editor/types'
+
+export const ChartSettings = ({ currentData }: IReportConfigurationSectionProps): React.JSX.Element => {
+  const { t } = useTranslation()
+
+  const CHART_OPTIONS = [
+    { value: '', label: t('reports.editor.chart-settings.chart-type.none') },
+    { value: CHART_TYPE_PIE, label: t('reports.editor.chart-settings.chart-type.pie-chart') },
+    { value: CHART_TYPE_LINE, label: t('reports.editor.chart-settings.chart-type.line-chart') },
+    { value: CHART_TYPE_BAR, label: t('reports.editor.chart-settings.chart-type.bar-chart') }
+  ]
+
+  const chartSelectOptions = useMemo(() => {
+    return currentData.columnConfigurations.map((column) => ({
+      value: column.name,
+      label: column.name
+    }))
+  }, [currentData])
+
+  const renderSelectItem = ({ label, name, mode }: { label: string, name: string, mode?: SelectProps['mode'] }): React.JSX.Element => (
+    <Form.Item
+      label={ label }
+      name={ name }
+    >
+      <Select
+        className="w-full"
+        mode={ mode }
+        options={ chartSelectOptions }
+      />
+    </Form.Item>
+  )
+
+  return (
+    <FormKit.Panel title={ t('reports.editor.chart-settings.title') }>
+      <Form.Item
+        label={ t('reports.editor.chart-settings.chart-type') }
+        name="chartType"
+      >
+        <Select options={ CHART_OPTIONS } />
+      </Form.Item>
+
+      <Conditional condition={ (formValues) => formValues.chartType === CHART_TYPE_PIE }>
+        <FormKit.Panel
+          border
+          theme="fieldset"
+          title={ t('reports.editor.chart-settings.settings') }
+        >
+          {renderSelectItem({ label: t('reports.editor.chart-settings.x-axis'), name: 'xAxis' })}
+          {renderSelectItem({ label: t('reports.editor.chart-settings.y-axis'), name: 'yAxis' })}
+        </FormKit.Panel>
+      </Conditional>
+      <Conditional condition={ (formValues) => formValues.chartType === CHART_TYPE_LINE }>
+        <FormKit.Panel
+          border
+          theme="fieldset"
+          title={ t('reports.editor.chart-settings.settings') }
+        >
+          {renderSelectItem({ label: t('reports.editor.chart-settings.x-axis'), name: 'xAxis' })}
+          {renderSelectItem({ label: t('reports.editor.chart-settings.y-axis'), name: 'yAxis', mode: 'multiple' })}
+        </FormKit.Panel>
+      </Conditional>
+      <Conditional condition={ (formValues) => formValues.chartType === CHART_TYPE_BAR }>
+        <FormKit.Panel
+          border
+          theme="fieldset"
+          title={ t('reports.editor.chart-settings.settings') }
+        >
+          {renderSelectItem({ label: t('reports.editor.chart-settings.x-axis'), name: 'xAxis' })}
+          {renderSelectItem({ label: t('reports.editor.chart-settings.y-axis'), name: 'yAxis', mode: 'multiple' })}
+        </FormKit.Panel>
+      </Conditional>
+    </FormKit.Panel>
+  )
+}
