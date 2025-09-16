3ba9a6f17bd7e2dc2b0086513483a418a69179e0
Tine <42359615+xIrusux@users.noreply.github.com>
2025-09-16 22:41:27 +0200

    [Website Settings] dont send filter unless there is a search applied (#2012)
    * small improv dont send filter when not needed
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build

--- assets/js/src/core/modules/website-settings/website-settings-container.tsx
+++ assets/js/src/core/modules/website-settings/website-settings-container.tsx
@@ -61,16 +61,20 @@ export const WebsiteSettingsContainer = (): React.JSX.Element => {
       filters: {
         page,
         pageSize,
-        columnFilters: [
-          {
-            key: 'name',
-            type: 'like',
-            filterValue: nameFilter
-          }
-        ]
+        columnFilters: nameFilter.length > 0
+          ? [
+              {
+                key: 'name',
+                type: 'like',
+                filterValue: nameFilter
+              }
+            ]
+          : []
       }
     }
   }), [nameFilter, page, pageSize])
+
+  console.log('queryArgs', queryArgs)
 
   const { data, isLoading: websiteSettingsLoading, isFetching: websiteSettingsFetching, error } = useWebsiteSettingsGetCollectionQuery(queryArgs, {
     refetchOnMountOrArgChange: true
---
+++ public/build/edf30d7b-aa97-4fd2-9bc3-c1ada87d4f0c/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/edf30d7b-aa97-4fd2-9bc3-c1ada87d4f0c/static/js/remoteEntry.js'
+    
