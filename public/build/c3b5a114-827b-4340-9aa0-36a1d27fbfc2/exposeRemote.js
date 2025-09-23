cf2014eaaf3d1ea6de69f46befd2847967906449
vrobert78 <15088490+vrobert78@users.noreply.github.com>
2025-09-23 07:50:13 +0000

    Automatic frontend build

---
+++ public/build/c3b5a114-827b-4340-9aa0-36a1d27fbfc2/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/c3b5a114-827b-4340-9aa0-36a1d27fbfc2/static/js/remoteEntry.js'
+    
--- public/build/f80fa918-c2be-4f59-bd2b-2e03af7bb463/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/f80fa918-c2be-4f59-bd2b-2e03af7bb463/static/js/remoteEntry.js'
-    
