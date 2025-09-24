d23f37d05e9f3342feef0f4a894862279d765c6f
markus-moser <markus.moser@pimcore.com>
2025-09-24 17:10:09 +0200

    Add translation related document detail context menu actions (#2104)

--- assets/js/src/core/modules/document/document-api-slice-enhanced.ts
+++ assets/js/src/core/modules/document/document-api-slice-enhanced.ts
@@ -68,6 +68,18 @@ const api = baseApi.enhanceEndpoints({
 
     documentsListAvailableSites: {
       providesTags: () => providingTags.DOCUMENT_SITE()
+    },
+
+    documentGetTranslations: {
+      providesTags: (result, error, args) => providingTags.DOCUMENT_DETAIL_ID(args.id)
+    },
+
+    documentAddTranslation: {
+      invalidatesTags: (result, error, args) => invalidatingTags.DOCUMENT_DETAIL_ID(args.id)
+    },
+
+    documentDeleteTranslation: {
+      invalidatesTags: (result, error, args) => invalidatingTags.DOCUMENT_DETAIL_ID(args.id)
     }
   }
 }).injectEndpoints({
@@ -134,7 +146,12 @@ export const {
   useDocumentGetSiteQuery,
   useLazyDocumentGetSiteQuery,
   useDocumentUpdateSiteMutation,
-  useDocumentDeleteSiteMutation
+  useDocumentDeleteSiteMutation,
+  useDocumentGetTranslationsQuery,
+  useLazyDocumentGetTranslationsQuery,
+  useDocumentAddTranslationMutation,
+  useDocumentDeleteTranslationMutation,
+  useDocumentGetTranslationParentByLanguageQuery
 } = api
 
 export { api }
