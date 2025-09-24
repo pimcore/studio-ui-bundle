013913208198171baef2e1178f43114d3b8f74ce
sholzer <sarah.holzer@valantic.at>
2025-09-24 13:36:15 +0200

    [User management] delete avatar is missing #1696

--- assets/js/src/core/modules/user/user-management-slice.tsx
+++ assets/js/src/core/modules/user/user-management-slice.tsx
@@ -65,7 +65,7 @@ export const slice = createSlice({
     updateUserImage: (state, action: PayloadAction<{ id: any, image: any }>): void => {
       const update: Update<any, any> = {
         id: action.payload.id,
-        changes: { image: action.payload.image, ...action.payload.image !== undefined ? { hasImage: true } : { hasImage: false } }
+        changes: { image: action.payload.image, hasImage: action.payload.image !== undefined}
       }
       userAdapter.updateOne(state, update)
     },
