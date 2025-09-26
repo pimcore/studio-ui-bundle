a1b553e1b6fce221207f1554364695fd3ba9c80a
markus-moser <markus.moser@pimcore.com>
2025-09-26 12:55:32 +0200

    Upgrade axios (#2128)

--- public/build/288085a5-4485-47f7-a1a4-490e0982e9c8/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/288085a5-4485-47f7-a1a4-490e0982e9c8/static/js/remoteEntry.js'
-    
---
+++ public/build/6029c821-0270-4c84-9d03-d3d943eae35f/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/6029c821-0270-4c84-9d03-d3d943eae35f/static/js/remoteEntry.js'
+    
