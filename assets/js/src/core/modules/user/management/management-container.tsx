c3f34edbbec9e491ebb297f2b67b2db78e8847ca
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-10-03 11:02:55 +0200

    updated the ManagementContainer by opening a user with certain id

--- assets/js/src/core/modules/user/management/management-container.tsx
+++ assets/js/src/core/modules/user/management/management-container.tsx
@@ -21,11 +21,9 @@ import { Spin } from '@Pimcore/components/spin/spin'
 import { createTreeNodeTestId } from '@Pimcore/utils/test-id-generator'
 import { ConfigLayout } from '@Pimcore/components/predefined-layouts/config/config-layout'
 
-const ManagementContainer = ({ ...props }): React.JSX.Element => {
+const ManagementContainer = ({ userId, ...props }: Record<string, any>): React.JSX.Element => {
   const { t } = useTranslation()
   const { getUserTree } = useUserManagementHelper()
-
-  console.log('====== PROPS: ', props)
 
   const [expandedKeys, setExpandedKeys] = React.useState<any[]>([0])
 
@@ -142,6 +140,7 @@ const ManagementContainer = ({ ...props }): React.JSX.Element => {
         } }
         onUpdateTreeData={ updateTreeData }
         treeData={ treeData }
+        userId={ userId }
       />
     ]
   }
