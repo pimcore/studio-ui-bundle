35eff88b7b7008dcfe9900a740be65e70480f7b7
Corepex <16717695+Corepex@users.noreply.github.com>
2025-10-03 10:34:18 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/widget-editor/components/widget-type-form/components/many-to-one-relation/many-to-one-relation.tsx
+++ assets/js/src/core/modules/widget-editor/components/widget-type-form/components/many-to-one-relation/many-to-one-relation.tsx
@@ -1,5 +1,15 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
 import { Form } from '@Pimcore/components/form/form'
-import { ManyToOneRelation as BaseManyToOneRelation, ManyToOneRelationProps as BaseManyToOneRelationProps } from '@Pimcore/components/many-to-one-relation/many-to-one-relation'
+import { ManyToOneRelation as BaseManyToOneRelation, type ManyToOneRelationProps as BaseManyToOneRelationProps } from '@Pimcore/components/many-to-one-relation/many-to-one-relation'
 import { elementTypes } from '@Pimcore/types/enums/element/element-type'
 import React, { useEffect } from 'react'
 import { useWidgetFormContext } from '../../context/hooks/use-widget-form-context'
@@ -11,12 +21,14 @@ export const ManyToOneRelation = (props: BaseManyToOneRelationProps): React.JSX.
     form.setFieldValue('rootFolder', null)
   }, [elementType, form])
 
-  return <BaseManyToOneRelation
-    {...props}
-    key={elementType}
-    allowToClearRelation
-    dataObjectsAllowed={elementType === elementTypes.dataObject}
-    assetsAllowed={elementType === elementTypes.asset}
-    documentsAllowed={elementType === elementTypes.document}
-  />
+  return (
+    <BaseManyToOneRelation
+      { ...props }
+      allowToClearRelation
+      assetsAllowed={ elementType === elementTypes.asset }
+      dataObjectsAllowed={ elementType === elementTypes.dataObject }
+      documentsAllowed={ elementType === elementTypes.document }
+      key={ elementType }
+    />
+  )
 }
