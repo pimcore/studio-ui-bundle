abddd8584eeb4dab48b114206eb868dab049302a
markus-moser <markus.moser@pimcore.com>
2025-10-09 14:59:31 +0200

    [Document Editor] Avoid unnecessary re-renderings when editable data changes (#2272)

--- assets/js/src/core/modules/element/dynamic-types/definitions/document/editable/components/content-editable/content-editable.tsx
+++ assets/js/src/core/modules/element/dynamic-types/definitions/document/editable/components/content-editable/content-editable.tsx
@@ -217,4 +217,14 @@ const ContentEditable = ({
   )
 }
 
-export default ContentEditable
+export default React.memo(ContentEditable, (prevProps, nextProps) => {
+  const propsToCompare = Object.keys(nextProps).filter(key => key !== 'value')
+
+  for (const key of propsToCompare) {
+    if (prevProps[key as keyof ContentEditableProps] !== nextProps[key as keyof ContentEditableProps]) {
+      return false
+    }
+  }
+
+  return true
+})
