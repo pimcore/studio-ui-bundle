7ad9bc6cf2a9195e4928cb942e90f8415bffe590
vin0401 <26813978+vin0401@users.noreply.github.com>
2025-10-20 13:52:23 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/element/dynamic-types/definitions/grid-cell/components/data-object-adapter/data-object-adapter-cell.tsx
+++ assets/js/src/core/modules/element/dynamic-types/definitions/grid-cell/components/data-object-adapter/data-object-adapter-cell.tsx
@@ -57,7 +57,7 @@ export const DataObjectAdapterCell = (props: DataObjectAdapterCellProps): React.
     if (column?.type === 'dataobject.classificationstore') {
       const apiColumnKey = column.key!.split('.')[0]
 
-      if (column?.localizable === true) {
+      if (column?.localizable) {
         return column.key === apiColumnKey && (column.locale ?? currentLanguage) === apiColumn.locale && apiColumn.additionalAttributes.groupId === column.config.groupId && apiColumn.additionalAttributes.keyId === column.config.keyId
       }
 
