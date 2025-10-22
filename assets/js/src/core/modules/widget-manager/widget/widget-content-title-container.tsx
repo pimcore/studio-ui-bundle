eba8d5b117bf7efbdcd2bf7eec856e623e53ee39
markus-moser <4639428+markus-moser@users.noreply.github.com>
2025-10-22 14:18:04 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/widget-manager/widget/widget-content-title-container.tsx
+++ assets/js/src/core/modules/widget-manager/widget/widget-content-title-container.tsx
@@ -32,7 +32,6 @@ export const WidgetContentTitleContainer = ({
   icon,
   title
 }: WidgetContentTitleProps): React.JSX.Element => {
-
   return (
     <>
       {ContentTitleComponent !== undefined
