e0aa32e6b4f6d22112c9caeabf6e3a4cdcf4903e
Corepex <christian.feldkirchner@pimcore.com>
2025-10-23 12:08:29 +0200

    export `tabpanel` to sdk

--- assets/js/src/sdk/components/index.ts
+++ assets/js/src/sdk/components/index.ts
@@ -153,7 +153,7 @@ export * from '@Pimcore/components/toolstrip/box/tool-strip-box'
 export * from '@Pimcore/components/tree-element/tree-element'
 export * from '@Pimcore/components/users-roles-dropdown/users-roles-dropdown'
 export * from '@Pimcore/components/vertical-timeline/vertical-timeline'
-export type { TabpanelItem } from '@Pimcore/components/tabpanel/tabpanel'
+export * from '@Pimcore/components/tabpanel/tabpanel'
 
 export * from '@Pimcore/modules/auth/hooks/use-user'
 
