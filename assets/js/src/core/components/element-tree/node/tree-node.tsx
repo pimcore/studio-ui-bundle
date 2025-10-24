991752bbae8919dd726a5db68554391654b006b0
Marvin Bertram <marvin.bertram@pimcore.com>
2025-10-24 12:25:55 +0200

    Fix selection style in tree (#2417)
    * Fix selection style in tree
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <26813978+vin0401@users.noreply.github.com>

--- assets/js/src/core/components/element-tree/node/tree-node.tsx
+++ assets/js/src/core/components/element-tree/node/tree-node.tsx
@@ -22,6 +22,7 @@ import { scrollToNodeElement } from '@Pimcore/modules/widget-manager/widget/util
 import { type ElementType } from '@Pimcore/types/enums/element/element-type'
 import { createNodeTestId } from '@Pimcore/utils/test-id-generator'
 import { ComponentRenderer } from '@Pimcore/modules/app/component-registry/component-renderer'
+import cn from 'classnames'
 
 export type TreeNodeWrapper = (children: React.ReactNode) => React.ReactNode
 export interface TreeNodeProps {
@@ -125,10 +126,6 @@ const TreeNode = forwardRef(function ForwardedTreeNode ({
   function getClasses (): string {
     const classes = ['tree-node', styles.treeNode]
 
-    if (isSelected) {
-      classes.push('tree-node--selected')
-    }
-
     if (danger) {
       classes.push('tree-node--danger')
     }
@@ -220,7 +217,7 @@ const TreeNode = forwardRef(function ForwardedTreeNode ({
   const nodeContent = (
     <Flex
       align="center"
-      className="tree-node__content-inner"
+      className={ cn('tree-node__content-inner') }
       gap="small"
       justify="center"
       onClick={ onClick }
@@ -268,7 +265,7 @@ const TreeNode = forwardRef(function ForwardedTreeNode ({
             props={ {
               node: treeNodeProps,
               children: (
-                <div className="tree-node__content">
+                <div className={ cn('tree-node__content', { 'tree-node__content--selected': isSelected }) }>
                   {wrapNode(nodeContent)}
                 </div>
               )
@@ -276,7 +273,7 @@ const TreeNode = forwardRef(function ForwardedTreeNode ({
           />
           )
         : (
-          <div className="tree-node__content">
+          <div className={ cn('tree-node__content', { 'tree-node__content--selected': isSelected }) }>
             {wrapNode(nodeContent)}
           </div>
           )}
