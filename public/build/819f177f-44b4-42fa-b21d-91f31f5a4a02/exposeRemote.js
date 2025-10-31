abc446afe47227822aecdec78547eebc4692eb94
markus-moser <markus.moser@pimcore.com>
2025-10-31 13:33:55 +0100

    Update RTK query API clients (#2471)

--- public/build/819f177f-44b4-42fa-b21d-91f31f5a4a02/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/819f177f-44b4-42fa-b21d-91f31f5a4a02/static/js/remoteEntry.js'
-    
---
+++ public/build/d9898e02-85b3-4805-af2f-7ee1c6edf100/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/d9898e02-85b3-4805-af2f-7ee1c6edf100/static/js/remoteEntry.js'
+    
