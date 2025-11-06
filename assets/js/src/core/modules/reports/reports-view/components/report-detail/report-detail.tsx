981f3c7f9fbd1f3995b5e33f9b706da456ce73b8
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-11-06 18:34:22 +0100

    updated the safeColumnId getting

--- assets/js/src/core/modules/reports/reports-view/components/report-detail/report-detail.tsx
+++ assets/js/src/core/modules/reports/reports-view/components/report-detail/report-detail.tsx
@@ -119,7 +119,7 @@ export const ReportDetail = ({ isLoading, currentReport, reportDetailData, chart
       if (isShowColumn) {
         const columnId = item?.name ?? `id-${index}`
         // Handle cases where the column name contains special characters like `.` and is interpreted as a path
-          const safeColumnId = columnId.replace(/\W/g, '_')
+          const safeColumnId = columnId.replaceAll(/\W/g, '_')
 
         if (item.displayType !== 'hide') {
           list.push(
