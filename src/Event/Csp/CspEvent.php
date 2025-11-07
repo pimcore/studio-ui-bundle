2520467a4544465cc16a6132090da7827b59d940
markus-moser <markus.moser@pimcore.com>
2025-11-07 17:13:43 +0100

    Simplify php doc

--- src/Event/Csp/CspEvent.php
+++ src/Event/Csp/CspEvent.php
@@ -20,41 +20,6 @@ use Symfony\Contracts\EventDispatcher\Event;
 /**
  * Event dispatched before CSP headers are set, allowing listeners to add additional
  * build origins or modify CSP directives.
- * 
- * Extension developers can subscribe to this event to add their own origins:
- * 
- * ```php
- * use Pimcore\Bundle\StudioUiBundle\Event\Csp\CspEvent;
- * use Symfony\Component\EventDispatcher\EventSubscriberInterface;
- * 
- * final class MyExtensionCspSubscriber implements EventSubscriberInterface
- * {
- *     public static function getSubscribedEvents(): array
- *     {
- *         // Use priority < 50 to run after core subscribers
- *         // Use priority > 100 to run before core subscribers
- *         return [
- *             CspEvent::class => ['onCspEvent', 0],
- *         ];
- *     }
- *     
- *     public function onCspEvent(CspEvent $event): void
- *     {
- *         $event->addBuildOrigins(['https://my-cdn.example.com']);
- *         
- *         // Or modify CSP handler directly
- *         $event->getCspHandler()->addAllowedUrls(
- *             ContentSecurityPolicyHandlerInterface::FONT_OPT,
- *             ['https://fonts.googleapis.com']
- *         );
- *     }
- * }
- * ```
- * 
- * Core subscriber priorities:
- * - BuildRemoteEntryCspSubscriber: 100 (remote entry files)
- * - BuildEntryPointCspSubscriber: 50 (static resources)
- * - Extension subscribers should typically use priority 0 or negative values
  */
 final class CspEvent extends Event
 {
