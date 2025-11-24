f52868e2ff295ccf015bd7dc413cb900340ab771
Corepex <16717695+Corepex@users.noreply.github.com>
2025-11-24 10:06:43 +0000

    Apply latest automatic api client updates

--- assets/js/src/core/modules/element/editor/shared-tab-manager/tabs/workflow/workflow-api-slice.gen.ts
+++ assets/js/src/core/modules/element/editor/shared-tab-manager/tabs/workflow/workflow-api-slice.gen.ts
@@ -129,6 +129,6 @@ export type SubmitAction = {
     /** Id of the transition */
     transitionId: string;
     /** workflowOptions */
-    workflowOptions: object;
+    workflowOptions: object | null;
 };
 export const { useWorkflowGetDetailsQuery, useWorkflowActionSubmitMutation } = injectedRtkApi;
