1b8c6be5d2b7ac89bf05114e5d541d972abaa1c6
Corepex <16717695+Corepex@users.noreply.github.com>
2025-11-25 10:33:42 +0000

    Apply latest automatic api client updates

--- assets/js/src/core/modules/asset/asset-api-slice.gen.ts
+++ assets/js/src/core/modules/asset/asset-api-slice.gen.ts
@@ -514,13 +514,13 @@ export type AssetDocumentStreamCustomApiArg = {
     dpi?: number;
     cropPercent?: boolean;
     /** CropWidth of image thumbnail */
-    cropWidth?: number;
+    cropWidth?: any;
     /** CropHeight of image thumbnail */
-    cropHeight?: number;
+    cropHeight?: any;
     /** CropTop of image thumbnail */
-    cropTop?: number;
+    cropTop?: any;
     /** CropLeft of image thumbnail */
-    cropLeft?: number;
+    cropLeft?: any;
 };
 export type AssetDocumentStreamDynamicApiResponse =
     /** status 200 Document image stream based on dynamic thumbnail configuration */ Blob;
@@ -554,13 +554,13 @@ export type AssetDocumentStreamByThumbnailApiArg = {
     thumbnailName: string;
     cropPercent?: boolean;
     /** CropWidth of image thumbnail */
-    cropWidth?: number;
+    cropWidth?: any;
     /** CropHeight of image thumbnail */
-    cropHeight?: number;
+    cropHeight?: any;
     /** CropTop of image thumbnail */
-    cropTop?: number;
+    cropTop?: any;
     /** CropLeft of image thumbnail */
-    cropLeft?: number;
+    cropLeft?: any;
 };
 export type AssetDownloadZipApiResponse = /** status 200 ZIP archive as attachment */ Blob;
 export type AssetDownloadZipApiArg = {
@@ -764,13 +764,13 @@ export type AssetImageStreamCustomApiArg = {
     forceResize?: boolean;
     cropPercent?: boolean;
     /** CropWidth of image thumbnail */
-    cropWidth?: number;
+    cropWidth?: any;
     /** CropHeight of image thumbnail */
-    cropHeight?: number;
+    cropHeight?: any;
     /** CropTop of image thumbnail */
-    cropTop?: number;
+    cropTop?: any;
     /** CropLeft of image thumbnail */
-    cropLeft?: number;
+    cropLeft?: any;
 };
 export type AssetImageStreamDynamicApiResponse =
     /** status 200 Image asset stream based on dynamic thumbnail configuration */ Blob;
@@ -814,13 +814,13 @@ export type AssetImageStreamByThumbnailApiArg = {
     thumbnailName: string;
     cropPercent?: boolean;
     /** CropWidth of image thumbnail */
-    cropWidth?: number;
+    cropWidth?: any;
     /** CropHeight of image thumbnail */
-    cropHeight?: number;
+    cropHeight?: any;
     /** CropTop of image thumbnail */
-    cropTop?: number;
+    cropTop?: any;
     /** CropLeft of image thumbnail */
-    cropLeft?: number;
+    cropLeft?: any;
     /** Mime type of steamed image. */
     mimeType?: "JPEG" | "PNG" | "source" | "original" | "print";
 };
