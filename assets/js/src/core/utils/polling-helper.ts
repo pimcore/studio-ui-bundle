57bec1f9390f36679d397e420f3225792c252f75
markus-moser <markus.moser@pimcore.com>
2025-11-26 14:56:52 +0100

    [Assets] Fix unsafe asset handling when pdf preview polling needs multiple requests (#2622)

--- assets/js/src/core/utils/polling-helper.ts
+++ assets/js/src/core/utils/polling-helper.ts
@@ -14,24 +14,43 @@ interface FetchBlobWithPollingParams {
   url: string
   onSuccess: (blob: Blob) => void
   interval?: number
+  throwOnError?: boolean
 }
 
 export async function fetchBlobWithPolling ({
   url,
   onSuccess,
-  interval = 3000
+  interval = 3000,
+  throwOnError = false
 }: FetchBlobWithPollingParams): Promise<void> {
-  const fetchUrl = async (): Promise<void> => {
-    const response = await fetch(url)
-    if (response.status === 200) {
-      const blob = await response.blob()
-      onSuccess(blob)
-    } else if (response.status === 202) {
-      setTimeout(fetchUrl, interval)
-    } else {
-      trackError(new GeneralError(`Unexpected response status: ${response.status}`))
+  await new Promise((resolve, reject) => {
+    const fetchUrl = async (): Promise<void> => {
+      try {
+        const response = await fetch(url)
+        if (response.status === 200) {
+          const blob = await response.blob()
+          onSuccess(blob)
+          resolve(undefined)
+        } else if (response.status === 202) {
+          setTimeout(() => { void fetchUrl() }, interval)
+        } else {
+          if (throwOnError) {
+            reject(new Error(`Unexpected response status: ${response.status}`))
+          } else {
+            trackError(new GeneralError(`Unexpected response status: ${response.status}`))
+            resolve(undefined)
+          }
+        }
+      } catch (error) {
+        if (throwOnError) {
+          reject(error)
+        } else {
+          trackError(new GeneralError('An error occurred while fetching the blob'))
+          resolve(undefined)
+        }
+      }
     }
-  }
 
-  await fetchUrl()
+    void fetchUrl()
+  })
 }
