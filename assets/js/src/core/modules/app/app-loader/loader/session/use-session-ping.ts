c5c3d4f17a36c1ab581df9aae6ae4a5dc231f244
markus-moser <markus.moser@pimcore.com>
2025-11-26 16:16:41 +0100

    Integrate session ping approach

---
+++ assets/js/src/core/modules/app/app-loader/loader/session/use-session-ping.ts
@@ -0,0 +1,36 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { useEffect } from 'react'
+import { useSettings } from '@Pimcore/modules/app/settings/hooks/use-settings'
+import { useLazyPingActionQuery } from '@Pimcore/modules/app/settings/settings-slice.gen'
+
+import { isNil } from 'lodash'
+
+export const useSessionPing = (): void => {
+  const { session_gc_maxlifetime: sessionGcMaxLifetime } = useSettings()
+  const [triggerPing] = useLazyPingActionQuery()
+
+  useEffect(() => {
+    if (isNil(sessionGcMaxLifetime)) {
+      return
+    }
+
+    const intervalTime = (Number(sessionGcMaxLifetime) - 60) * 1000
+
+    const interval = setInterval(() => {
+      void triggerPing()
+    }, intervalTime)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [sessionGcMaxLifetime, triggerPing])
+}
