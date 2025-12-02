6bee92569a885a2e5be1a75a3fe4f18e672a99b7
Marvin Bertram <marvin.bertram@pimcore.com>
2025-12-02 17:49:48 +0100

    Handle context while having multiple tabsets (#2645)
    * Handle context while having multiple tabsets
    
    * Cleanup code
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <26813978+vin0401@users.noreply.github.com>

--- assets/js/src/core/modules/widget-manager/widget-manager-slice.ts
+++ assets/js/src/core/modules/widget-manager/widget-manager-slice.ts
@@ -225,6 +225,39 @@ export const slice = createSlice({
       if (isOuterModelNode) {
         state.outerModel = { ...model.toJson() }
       } else {
+        const currentTabset = model.getActiveTabset()
+        let hasValidNode = false
+
+        if (currentTabset !== undefined) {
+          const currentNode = currentTabset.getChildren()?.[0]
+
+          if (currentNode !== undefined) {
+            model.doAction(Actions.selectTab(currentNode.getId()))
+            hasValidNode = true
+          }
+        }
+
+        if (!hasValidNode) {
+          const firstTabset = model.getFirstTabSet()
+          const parent = firstTabset.getParent()
+
+          const tabsets = parent!.getChildren()
+          let validChildNode: Node | undefined
+
+          for (const tabset of tabsets) {
+            const childNodes = tabset.getChildren()
+
+            if (childNodes.length > 0) {
+              validChildNode = childNodes[0]
+              break
+            };
+          }
+
+          if (validChildNode !== undefined) {
+            model.doAction(Actions.selectTab(validChildNode.getId()))
+          }
+        }
+
         state.innerModel = { ...model.toJson() }
       }
     }
---
+++ public/build/0280151a-ac8c-4cca-a66b-f3d67185db11/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/0280151a-ac8c-4cca-a66b-f3d67185db11/static/js/remoteEntry.js'
+    
