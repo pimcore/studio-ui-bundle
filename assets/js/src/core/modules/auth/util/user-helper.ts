0c894e409509ead21619d79444e2c29d2f8dd74f
Corepex <16717695+Corepex@users.noreply.github.com>
2025-12-09 10:27:19 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/auth/util/user-helper.ts
+++ assets/js/src/core/modules/auth/util/user-helper.ts
@@ -1,6 +1,16 @@
-import { store } from "@sdk/app";
-import { UserInformation } from "../user/user-api-slice.gen";
-import { selectCurrentUser } from "../user/user-slice";
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { store } from '@sdk/app'
+import { type UserInformation } from '../user/user-api-slice.gen'
+import { selectCurrentUser } from '../user/user-slice'
 
 export const getCurrentUser = (): UserInformation => {
   return selectCurrentUser(store.getState())
