ad88c22d6795d39e0dd759602ec4a09699f42287
Christian F. <christian.feldkirchner@pimcore.com>
2025-12-09 11:36:52 +0100

    Fix import (#2695)
    * fix import
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: Corepex <16717695+Corepex@users.noreply.github.com>

--- assets/js/src/core/modules/auth/util/user-helper.ts
+++ assets/js/src/core/modules/auth/util/user-helper.ts
@@ -8,7 +8,7 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
-import { store } from '@sdk/app'
+import { store } from '@Pimcore/app/store'
 import { type UserInformation } from '../user/user-api-slice.gen'
 import { selectCurrentUser } from '../user/user-slice'
 
---
+++ public/build/c5d93c9c-df50-4825-9eca-0daaf066640e/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/c5d93c9c-df50-4825-9eca-0daaf066640e/static/js/remoteEntry.js'
+    
