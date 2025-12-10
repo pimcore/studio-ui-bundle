d727100d966800787a2e0024e73c5d048ba6335c
berfinyuksel <99557970+berfinyuksel@users.noreply.github.com>
2025-12-10 12:22:18 +0000

    Automatic frontend build

---
+++ public/build/ab02c35f-686d-4fe9-a80c-48f45f5e9201/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/ab02c35f-686d-4fe9-a80c-48f45f5e9201/static/js/remoteEntry.js'
+    
--- public/build/cd807d6d-3a04-4b02-a1e4-d6d95d586657/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/cd807d6d-3a04-4b02-a1e4-d6d95d586657/static/js/remoteEntry.js'
-    
