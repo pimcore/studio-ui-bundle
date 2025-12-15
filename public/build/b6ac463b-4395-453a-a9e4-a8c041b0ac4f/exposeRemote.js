ff8488762959f830a6d4142d496a58a699c6fe22
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-12-15 12:08:16 +0100

    added the event publish on 401 error

---
+++ public/build/b6ac463b-4395-453a-a9e4-a8c041b0ac4f/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/b6ac463b-4395-453a-a9e4-a8c041b0ac4f/static/js/remoteEntry.js'
+    
--- public/build/b765b6fc-2151-4f1d-9cb5-e3177be7bb2d/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/b765b6fc-2151-4f1d-9cb5-e3177be7bb2d/static/js/remoteEntry.js'
-    
