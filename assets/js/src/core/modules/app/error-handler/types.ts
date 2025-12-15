1a0e0fc8eb53fd7630b571384c30d4ffadaec6d3
markus-moser <markus.moser@pimcore.com>
2025-12-15 10:40:01 +0100

    [Asset Upload] Improve (version) upload handling (#2710)

--- assets/js/src/core/modules/app/error-handler/types.ts
+++ assets/js/src/core/modules/app/error-handler/types.ts
@@ -15,7 +15,7 @@ export interface IErrorGetContent {
   data: string | { errorKey: string, title?: string }
 }
 
-export type ApiErrorData = FetchBaseQueryError | SerializedError
+export type ApiErrorData = FetchBaseQueryError | SerializedError | { data: IApiErrorDetails }
 
 export interface IApiErrorDetails {
   detail?: string
