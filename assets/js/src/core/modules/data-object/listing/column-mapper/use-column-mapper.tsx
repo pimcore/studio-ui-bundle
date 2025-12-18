9b901e756b76832c3ca348838e1a71a0de6e0696
vin0401 <marvin.bertram@pimcore.com>
2025-12-18 10:50:59 +0100

    Fix behavior of classification store in grid

--- assets/js/src/core/modules/data-object/listing/column-mapper/use-column-mapper.tsx
+++ assets/js/src/core/modules/data-object/listing/column-mapper/use-column-mapper.tsx
@@ -27,13 +27,17 @@ export const useDataObjectColumnMapper = (): UseColumnMapperReturn => {
 
       if (column.localizable && column.locale === 'default') {
         return dataKey === column.key && data.additionalAttributes?.keyId === column.config?.keyId && data.additionalAttributes?.groupId === column.config?.groupId && data.locale === null
-      }
+      } 
 
       if (column.localizable && (column.locale === null || column.locale === undefined)) {
         return dataKey === column.key && data.additionalAttributes?.keyId === column.config?.keyId && data.additionalAttributes?.groupId === column.config?.groupId && data.locale === currentLanguage
       }
 
-      return dataKey === column.key && data.additionalAttributes?.keyId === column.config?.keyId && data.additionalAttributes?.groupId === column.config?.groupId && data.locale === column.locale
+      if (column.localizable) {
+        return dataKey === column.key && data.additionalAttributes?.keyId === column.config?.keyId && data.additionalAttributes?.groupId === column.config?.groupId && data.locale === column.locale
+      }
+
+      return dataKey === column.key && data.additionalAttributes?.keyId === column.config?.keyId && data.additionalAttributes?.groupId === column.config?.groupId
     }
 
     if (column.localizable && (column.locale === null || column.locale === undefined)) {
