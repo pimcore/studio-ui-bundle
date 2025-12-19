60436056c514abe6e897fe8e882a4803942e53c7
Corepex <christian.feldkirchner@pimcore.com>
2025-12-19 12:02:02 +0100

    added antdesign `Input` to sdk export

--- assets/js/src/sdk/components/index.ts
+++ assets/js/src/sdk/components/index.ts
@@ -199,3 +199,5 @@ export * from '@Pimcore/components/data-object-preview/data-object-preview'
 export * from '@Pimcore/components/creatable-select/creatable-select'
 
 export * from '@Pimcore/components/dynamic-filter/provider/use-dynamic-filter'
+
+export * from 'antd/es/input'
