9a06eed9c927bca698a82cdecb11333c2cd5169b
Corepex <christian.feldkirchner@pimcore.com>
2025-12-19 12:20:21 +0100

    export only `InputRef`

--- assets/js/src/sdk/components/index.ts
+++ assets/js/src/sdk/components/index.ts
@@ -200,4 +200,4 @@ export * from '@Pimcore/components/creatable-select/creatable-select'
 
 export * from '@Pimcore/components/dynamic-filter/provider/use-dynamic-filter'
 
-export * from 'antd/es/input'
+export { type InputRef } from 'antd'
