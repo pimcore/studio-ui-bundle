de3403f57c19811851e86ad8eb6c107ce024e35c
chilladelia <1760961+chilladelia@users.noreply.github.com>
2026-01-20 16:05:13 +0000

    Automatic frontend build

--- public/build/0c3d7959-db96-4218-be5d-c446ace567b9/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/0c3d7959-db96-4218-be5d-c446ace567b9/static/js/remoteEntry.js'
-    
---
+++ public/build/e36ec463-31a6-4671-b9fe-a65041b32a40/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/e36ec463-31a6-4671-b9fe-a65041b32a40/static/js/remoteEntry.js'
+    
