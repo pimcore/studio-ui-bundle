a247aa1f3935ae3d180e15c69633d297cc26ac4f
AlephLau <70108013+AlephLau@users.noreply.github.com>
2026-01-22 17:13:33 +0000

    Automatic frontend build

---
+++ public/build/ac715f4e-4886-4a21-b8c8-8d9b1f56b1bb/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/ac715f4e-4886-4a21-b8c8-8d9b1f56b1bb/static/js/remoteEntry.js'
+    
--- public/build/d9ff520a-2b56-42bd-87ca-772209b565ea/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/d9ff520a-2b56-42bd-87ca-772209b565ea/static/js/remoteEntry.js'
-    
