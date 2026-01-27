ea635b0468840952bd78c7dca8dffb707a748789
Andreas Grünwald <andreas.gruenwald@valantic.at>
2026-01-27 19:39:22 +0100

    Remove unused `Switch` import from DateRange field definition form fields

--- assets/js/src/core/modules/field-definitions/dynamic-types/types/dateRange/field-definition-date-range-form-fields.tsx
+++ assets/js/src/core/modules/field-definitions/dynamic-types/types/dateRange/field-definition-date-range-form-fields.tsx
@@ -9,7 +9,7 @@
  */
 
 import { type FieldDefinitionAbstractFormFieldsProps } from '@Pimcore/modules/field-definitions/dynamic-types/dynamic-type-field-definition-abstract'
-import { Form, FormKit, Input, Switch } from '@sdk/components'
+import { Form, FormKit, Input } from '@sdk/components'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
