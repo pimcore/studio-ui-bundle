1fe8e5fa9d077770bf7c3bbae4a43b8c3e06bd2f
Corepex <christian.feldkirchner@pimcore.com>
2026-01-27 14:28:51 +0100

    added sdk exports

---
+++ assets/js/src/sdk/modules/gdpr-data-extractor/index.ts
@@ -0,0 +1,4 @@
+export * from '@Pimcore/modules/gdpr-data-extractor/components/export-button/export-button'
+export * from '@Pimcore/modules/gdpr-data-extractor/dynamic-types/definitions/dynamic-type-abstract-gdpr-provider'
+
+export { type GDPRProviderTabProps } from '@Pimcore/modules/gdpr-data-extractor/components/tab-panel/tab-panel'
