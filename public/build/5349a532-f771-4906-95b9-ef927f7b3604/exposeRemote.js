427280ac74e56e591a37826f946f57754c7092a2
Sarah Holzer <sarah.holzer@valantic.at>
2026-01-28 14:50:58 +0100

    [Document Editor] set outline none for editables (#2852)

--- public/build/5349a532-f771-4906-95b9-ef927f7b3604/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/5349a532-f771-4906-95b9-ef927f7b3604/static/js/remoteEntry.js'
-    
---
+++ public/build/84376274-e00a-4f35-861c-7f695a69e288/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/84376274-e00a-4f35-861c-7f695a69e288/static/js/remoteEntry.js'
+    
