cb4c2a4b5766653e83229bd5a756257165a41d33
sholzer <10495949+sholzer@users.noreply.github.com>
2026-01-29 07:20:37 +0000

    Automatic frontend build

---
+++ public/build/0fdcc1fe-4c01-481b-be83-593fd9b52885/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/0fdcc1fe-4c01-481b-be83-593fd9b52885/static/js/remoteEntry.js'
+    
--- public/build/b279db90-7679-482e-ac87-887c7989cd2a/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/b279db90-7679-482e-ac87-887c7989cd2a/static/js/remoteEntry.js'
-    
