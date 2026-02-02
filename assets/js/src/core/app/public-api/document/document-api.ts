e803a9e38e5e48616bc255f6c00309a6873e3cc7
markus-moser <markus.moser@pimcore.com>
2026-02-02 18:21:31 +0100

    Remove console.log

--- assets/js/src/core/app/public-api/document/document-api.ts
+++ assets/js/src/core/app/public-api/document/document-api.ts
@@ -103,7 +103,6 @@ class DocumentApiImpl implements DocumentApi {
   }
 
   notifyAreablockTypes (documentId: number, editableTypeId: string, areablockTypes: AreablockGroupedTypes): void {
-    console.log('Notifying areablock types for document', documentId, editableTypeId, areablockTypes)
     store.dispatch(setDocumentAreablockTypes({ documentId, editableTypeId, areablockTypes }))
   }
 
