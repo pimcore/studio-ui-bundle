89db1bb38c95c00fc38b58aff8f7e634918df922
markus-moser <markus.moser@pimcore.com>
2026-02-02 12:37:30 +0100

    [Class Editor] Icon fixes (#2876)

--- public/build/6b470513-72d7-4fcd-bb47-5d796a4b83ee/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/6b470513-72d7-4fcd-bb47-5d796a4b83ee/static/js/remoteEntry.js'
-    
---
+++ public/build/ce418d85-a07f-473a-a30c-0d2e219cfc1a/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/ce418d85-a07f-473a-a30c-0d2e219cfc1a/static/js/remoteEntry.js'
+    
