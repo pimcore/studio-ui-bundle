5e44d8198a1293dffc3954909c4809402317f768
Andreas Grünwald <andreas.gruenwald@valantic.at>
2026-02-02 13:18:16 +0100

    Add `FieldDefinitionAbstractFormFieldsProps` to geo field type components.

--- assets/js/src/core/modules/field-definitions/dynamic-types/types/geobounds/field-definition-geobounds-form-fields.tsx
+++ assets/js/src/core/modules/field-definitions/dynamic-types/types/geobounds/field-definition-geobounds-form-fields.tsx
@@ -8,10 +8,11 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
+import { type FieldDefinitionAbstractFormFieldsProps } from '@Pimcore/modules/field-definitions/dynamic-types/dynamic-type-field-definition-abstract'
 import { FieldDefinitionGeoSettings } from '@Pimcore/modules/field-definitions/dynamic-types/components/field-definition-geo-settings/field-definition-geo-settings'
 import React from 'react'
 
-export const FieldDefinitionGeoboundsFormFields = (): React.JSX.Element => {
+export const FieldDefinitionGeoboundsFormFields = (props: FieldDefinitionAbstractFormFieldsProps): React.JSX.Element => {
   return (
     <FieldDefinitionGeoSettings />
   )
--- assets/js/src/core/modules/field-definitions/dynamic-types/types/geopolyline/field-definition-geopolyline-form-fields.tsx
+++ assets/js/src/core/modules/field-definitions/dynamic-types/types/geopolyline/field-definition-geopolyline-form-fields.tsx
@@ -8,10 +8,11 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
+import { type FieldDefinitionAbstractFormFieldsProps } from '@Pimcore/modules/field-definitions/dynamic-types/dynamic-type-field-definition-abstract'
 import { FieldDefinitionGeoSettings } from '@Pimcore/modules/field-definitions/dynamic-types/components/field-definition-geo-settings/field-definition-geo-settings'
 import React from 'react'
 
-export const FieldDefinitionGeopolylineFormFields = (): React.JSX.Element => {
+export const FieldDefinitionGeopolylineFormFields = (props: FieldDefinitionAbstractFormFieldsProps): React.JSX.Element => {
   return (
     <FieldDefinitionGeoSettings />
   )
