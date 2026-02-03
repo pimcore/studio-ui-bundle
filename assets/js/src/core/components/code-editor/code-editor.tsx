0b28bbb45b6a78dca73244eb07dabe2705c21024
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-02-03 16:43:34 +0100

    refactored the code

--- assets/js/src/core/components/code-editor/code-editor.tsx
+++ assets/js/src/core/components/code-editor/code-editor.tsx
@@ -8,13 +8,9 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
-import { isEmpty } from 'lodash'
-import ReactCodeMirror, { type EditorView, type ReactCodeMirrorProps } from '@uiw/react-codemirror'
-import { yaml as codeMirrorYaml } from '@codemirror/lang-yaml'
-import yaml from 'js-yaml'
-import { type Extension } from '@codemirror/state'
-import { type Diagnostic, linter, lintGutter } from '@codemirror/lint'
 import React from 'react'
+import ReactCodeMirror, { type ReactCodeMirrorProps } from '@uiw/react-codemirror'
+import { getPresetExtensions } from '@Pimcore/components/code-editor/helpers'
 import { useStyles } from './code-editor.styles'
 
 export type CodeEditorPreset = 'text' | 'yaml'
@@ -24,52 +20,6 @@ export interface CodeEditorProps extends Omit<ReactCodeMirrorProps, 'extensions'
   extensions?: ReactCodeMirrorProps['extensions']
   value?: string
   onChange?: (value: string) => void
-}
-
-const yamlLinter = linter((view: EditorView): Diagnostic[] => {
-  const diagnostics: Diagnostic[] = []
-  const content = view.state.doc.toString()
-
-  try {
-    yaml.load(content)
-  } catch (error) {
-    if (error instanceof Error) {
-      const yamlError = error as any
-      let from = 0
-      let to = 0
-
-      // Get error line and column
-      if (!isEmpty(yamlError.mark)) {
-        const line: number = yamlError.mark.line
-        const column = yamlError.mark.column
-        const lineObj = view.state.doc.line(line + 1)
-        from = lineObj.from + column
-        to = Math.min(from + 1, lineObj.to)
-      } else if (!isEmpty(yamlError.linePos)) {
-        from = yamlError.linePos[0]?.start ?? 0
-        to = yamlError.linePos[0]?.end ?? from + 1
-      }
-
-      diagnostics.push({
-        from,
-        to,
-        severity: 'error',
-        message: error.message
-      })
-    }
-  }
-
-  return diagnostics
-})
-
-const getPresetExtensions = (preset: CodeEditorPreset): Extension[] => {
-  switch (preset) {
-    case 'yaml':
-      return [codeMirrorYaml(), yamlLinter, lintGutter()]
-    case 'text':
-    default:
-      return []
-  }
 }
 
 export const CodeEditor = ({ preset, extensions, value, onChange, ...props }: CodeEditorProps): React.JSX.Element => {
