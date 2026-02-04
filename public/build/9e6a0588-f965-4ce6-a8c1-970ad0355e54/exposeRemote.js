ff3711184aabbbc1c99e7a26933e51a41db54164
drietsch <62896+drietsch@users.noreply.github.com>
2026-02-04 16:25:54 +0000

    Automatic frontend build

--- public/build/9e6a0588-f965-4ce6-a8c1-970ad0355e54/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/9e6a0588-f965-4ce6-a8c1-970ad0355e54/static/js/remoteEntry.js'
-    
---
+++ public/build/f673bb49-2067-4244-af34-b284ee5b9d82/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/f673bb49-2067-4244-af34-b284ee5b9d82/static/js/remoteEntry.js'
+    
