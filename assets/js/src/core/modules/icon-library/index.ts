5fa31d0887a7d1ab14b532b332a04335d685a17e
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-02-06 12:42:58 +0100

    [Icons] Add Object Merger icons (#2904)
    * added the icons for the object merger
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

--- assets/js/src/core/modules/icon-library/index.ts
+++ assets/js/src/core/modules/icon-library/index.ts
@@ -31,6 +31,7 @@ import appearanceBranding from '@Pimcore/assets/icons/appearance-branding.inline
 import applicationLogger from '@Pimcore/assets/icons/application-logger.inline.svg?react'
 import areaBrick from '@Pimcore/assets/icons/area-brick.inline.svg?react'
 import arrowNarrowRight from '@Pimcore/assets/icons/arrow-narrow-right.inline.svg?react'
+import arrowSquareRight from '@Pimcore/assets/icons/arrow-square-right.inline.svg?react'
 import asset from '@Pimcore/assets/icons/asset.inline.svg?react'
 import attachment from '@Pimcore/assets/icons/attachment.inline.svg?react'
 import audio from '@Pimcore/assets/icons/audio.inline.svg?react'
@@ -75,13 +76,16 @@ import collection from '@Pimcore/assets/icons/collection.inline.svg?react'
 import color from '@Pimcore/assets/icons/color.inline.svg?react'
 import columns from '@Pimcore/assets/icons/columns.inline.svg?react'
 import comboboxField from '@Pimcore/assets/icons/combobox-field.inline.svg?react'
+import compare from '@Pimcore/assets/icons/compare.inline.svg?react'
 import contentDuplicate from '@Pimcore/assets/icons/content-duplicate.inline.svg?react'
 import contentSettings from '@Pimcore/assets/icons/content-settings.inline.svg?react'
 import content from '@Pimcore/assets/icons/content.inline.svg?react'
+import contrast01 from '@Pimcore/assets/icons/contrast-01.inline.svg?react'
 import convert from '@Pimcore/assets/icons/convert.inline.svg?react'
 import copilot from '@Pimcore/assets/icons/copilot.inline.svg?react'
 import copy03 from '@Pimcore/assets/icons/copy-03.inline.svg?react'
 import copy from '@Pimcore/assets/icons/copy.inline.svg?react'
+import cornerUpLeft from '@Pimcore/assets/icons/corner-up-left.inline.svg?react'
 import counter from '@Pimcore/assets/icons/counter.inline.svg?react'
 import countriesMultiple from '@Pimcore/assets/icons/countries-multiple.inline.svg?react'
 import countrySelect from '@Pimcore/assets/icons/country-select.inline.svg?react'
@@ -427,6 +431,10 @@ moduleSystem.registerModule({
       component: arrowNarrowRight
     })
     iconLibrary.register({
+      name: 'arrow-square-right',
+      component: arrowSquareRight
+    })
+    iconLibrary.register({
       name: 'asset',
       component: asset
     })
@@ -603,6 +611,10 @@ moduleSystem.registerModule({
       component: comboboxField
     })
     iconLibrary.register({
+      name: 'compare',
+      component: compare
+    })
+    iconLibrary.register({
       name: 'content-duplicate',
       component: contentDuplicate
     })
@@ -613,6 +625,10 @@ moduleSystem.registerModule({
     iconLibrary.register({
       name: 'content',
       component: content
+    })
+    iconLibrary.register({
+      name: 'contrast-01',
+      component: contrast01
     })
     iconLibrary.register({
       name: 'convert',
@@ -629,6 +645,10 @@ moduleSystem.registerModule({
     iconLibrary.register({
       name: 'copy',
       component: copy
+    })
+    iconLibrary.register({
+      name: 'corner-up-left',
+      component: cornerUpLeft
     })
     iconLibrary.register({
       name: 'counter',
--- public/build/594799a4-cce2-4cfd-bac3-f62aad686bd6/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/594799a4-cce2-4cfd-bac3-f62aad686bd6/static/js/remoteEntry.js'
-    
