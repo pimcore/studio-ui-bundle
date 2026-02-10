999c4512738c87fbe74f863f0a3faedca7868d35
kingjia90 <6014195+kingjia90@users.noreply.github.com>
2026-02-10 16:24:10 +0000

    Automatic frontend build

---
+++ public/build/d69fc075-12a9-47a8-adbc-5bc78266d08b/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/d69fc075-12a9-47a8-adbc-5bc78266d08b/static/js/remoteEntry.js'
+    
--- public/build/f68ad17c-6a6e-4eb1-bddf-3fcac84de99d/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/f68ad17c-6a6e-4eb1-bddf-3fcac84de99d/static/js/remoteEntry.js'
-    
