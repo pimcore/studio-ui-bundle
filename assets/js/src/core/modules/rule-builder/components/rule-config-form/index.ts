66712247666e6858c8d7855b5622e0d80b56c040
markus-moser <markus.moser@pimcore.com>
2026-02-11 14:51:32 +0100

    Add RuleConfigForm component

---
+++ assets/js/src/core/modules/rule-builder/components/rule-config-form/index.ts
@@ -0,0 +1,14 @@
+/**
+* Pimcore
+*
+* This source file is available under two different licenses:
+* - Pimcore Commercial License (PCL)
+* - Pimcore Enterprise License (PEL)
+* Full copyright and license information is available in
+* LICENSE.md which is distributed with this source code.
+*
+*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PEL
+*/
+
+export { RuleConfigForm } from './rule-config-form'
--- assets/js/src/sdk/modules/rule-builder/index.ts
+++ assets/js/src/sdk/modules/rule-builder/index.ts
@@ -17,6 +17,7 @@ export * from '@Pimcore/modules/rule-builder/components/rule-actions/rule-action
 export * from '@Pimcore/modules/rule-builder/components/rule-triggers/rule-triggers'
 export * from '@Pimcore/modules/rule-builder/components/rule-conditions/rule-conditions'
 export * from '@Pimcore/modules/rule-builder/components/sortable-rules-list'
+export * from '@Pimcore/modules/rule-builder/components/rule-config-form'
 
 // Dynamic Type Abstracts
 export * from '@Pimcore/modules/rule-builder/dynamic-types/rule-action/dynamic-type-rule-action-abstract'
