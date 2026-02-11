615bf109c1501521290e1113750949fea91b7314
markus-moser <4639428+markus-moser@users.noreply.github.com>
2026-02-11 13:55:30 +0000

    Apply eslint-fixer changes

--- assets/js/src/core/modules/rule-builder/components/rule-config-form/rule-config-form.tsx
+++ assets/js/src/core/modules/rule-builder/components/rule-config-form/rule-config-form.tsx
@@ -1,15 +1,12 @@
 /**
-* Pimcore
-*
-* This source file is available under two different licenses:
-* - Pimcore Commercial License (PCL)
-* - Pimcore Enterprise License (PEL)
-* Full copyright and license information is available in
-* LICENSE.md which is distributed with this source code.
-*
-*  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
-*  @license    https://github.com/pimcore/studio-ui-bundle/blob/1.x/LICENSE.md POCL and PEL
-*/
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
 
 import React, { type ReactNode } from 'react'
 import { Form, FormKit } from '@sdk/components'
@@ -48,13 +45,13 @@ export function RuleConfigForm<T extends Record<string, any>> ({
 
   return (
     <FormKit
-      formProps={{
+      formProps={ {
         form,
         component: false,
         initialValues: config,
         onValuesChange: handleValuesChange,
         disabled
-      }}
+      } }
     >
       {children}
     </FormKit>
