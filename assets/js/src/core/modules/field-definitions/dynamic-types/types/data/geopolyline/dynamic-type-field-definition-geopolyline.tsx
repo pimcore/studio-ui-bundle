d8cbcc609748bce19836dae23af441fc914c92c0
chilladelia <a.gruenw@gmail.com>
2026-02-20 19:49:20 +0100

    Class editor | Refactor folder structure. (#2987)

---
+++ assets/js/src/core/modules/field-definitions/dynamic-types/types/data/geopolyline/dynamic-type-field-definition-geopolyline.tsx
@@ -0,0 +1,43 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { type FieldDefinitionContext } from '@Pimcore/modules/field-definitions/dynamic-types/dynamic-type-field-definition-abstract'
+import { DynamicTypeFieldDefinitionDataAbstract } from '@Pimcore/modules/field-definitions/dynamic-types/types/data/_abstracts/dynamic-type-field-defintion-data-abstract'
+import { FieldDefinitionGeopolylineFormFields } from '@Pimcore/modules/field-definitions/dynamic-types/types/data/geopolyline/field-definition-geopolyline-form-fields'
+import { type ElementIcon } from '@sdk/modules/widget-manager'
+import React from 'react'
+
+export class DynamicTypeFieldDefinitionGeopolyline extends DynamicTypeFieldDefinitionDataAbstract {
+  id: string = 'geopolyline'
+
+  getIcon (): ElementIcon {
+    return { type: 'name', value: 'geographical-polyline' }
+  }
+
+  getGroup (): string[] {
+    return [...super.getGroup(), 'geo']
+  }
+
+  getFormFields (context: FieldDefinitionContext): React.JSX.Element {
+    const id = this.getId(context)
+    const fieldDefinition = context.fieldDefinitions[id]
+
+    return (
+      <>
+        {super.getFormFields(context)}
+        <FieldDefinitionGeopolylineFormFields
+          context={ context }
+          id={ fieldDefinition?.name ?? id }
+          type={ this.id }
+        />
+      </>
+    )
+  }
+}
---
+++ assets/js/src/core/modules/field-definitions/dynamic-types/types/data/user/dynamic-type-field-definition-user.tsx
@@ -0,0 +1,29 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { type FieldDefinitionContext } from '@Pimcore/modules/field-definitions/dynamic-types/dynamic-type-field-definition-abstract'
+import { DynamicTypeFieldDefinitionDataAbstract } from '@Pimcore/modules/field-definitions/dynamic-types/types/data/_abstracts/dynamic-type-field-defintion-data-abstract'
+import { type ElementIcon } from '@sdk/modules/widget-manager'
+
+export class DynamicTypeFieldDefinitionUser extends DynamicTypeFieldDefinitionDataAbstract {
+  id: string = 'user'
+
+  getIcon (): ElementIcon {
+    return { type: 'name', value: 'user' }
+  }
+
+  getGroup (): string[] {
+    return [...super.getGroup(), 'select']
+  }
+
+  getTags (props: FieldDefinitionContext): string[] {
+    return [...super.getTags(props), 'group:select']
+  }
+}
