7e3638ce2a5c80a097ea0e8257bad4288e0c584b
vin0401 <26813978+vin0401@users.noreply.github.com>
2026-02-26 08:21:38 +0000

    Automatic frontend build

---
+++ public/build/dfcef4e1-7c05-426f-ab34-144282a362fb/static/js/async/__federation_expose_modules__reports.56451ef5.js
@@ -0,0 +1,24 @@
+/*! For license information please see __federation_expose_modules__reports.56451ef5.js.LICENSE.txt */
+"use strict";(self.webpackChunkpimcore_studio_ui_bundle=self.webpackChunkpimcore_studio_ui_bundle||[]).push([["5440"],{8156:function(e,t,a){a.d(t,{Q:()=>i});var s,i=((s={}).QuickAccessHidden="quickAccess.hidden",s.OpenDocument="quickAccess.open_document",s.OpenObject="quickAccess.open_object",s.OpenAsset="quickAccess.open_asset",s.RecycleBin="quickAccess.recycle_bin",s.DataManagementHidden="dataManagement.hidden",s.NotesAndEvents="dataManagement.notesEvents",s.SearchReplaceAssignments="dataManagement.searchReplaceAssignments",s.PredefinedProperties="dataManagement.predefinedProperties",s.TagConfiguration="dataManagement.tagConfiguration",s.GDPRDataExtractor="dataManagement.gdprDataExtractor",s.ClassDefinitions="dataManagement.dataModel_classes",s.FieldCollections="dataManagement.dataModel_fieldCollections",s.AssetThumbnails="assetManagement.assetThumbnails",s.ExperienceEcommerceHidden="experienceEcommerce.hidden",s.Mails="experienceEcommerce.emails",s.DocumentTypes="experienceEcommerce.documentTypes",s.WebsiteSettings="experienceEcommerce.websiteSettings",s.Redirects="experienceEcommerce.redirects",s.TranslationsHidden="translations.hidden",s.Translations="translations.translations",s.Appearance="system.appearanceBranding",s.ReportingHidden="reporting.hidden",s.Reports="reporting.reports",s.CustomReportsConfiguration="reporting.customReportsConfiguration",s.SystemHidden="system.hidden",s.UsersHidden="system.users_hidden",s.Users="system.users_users",s.Roles="system.users_roles",s.PerspectiveEditor="system.perspectiveEditor",s.WidgetEditor="system.widgetEditor",s.ApplicationLogger="system.applicationLogger",s.About="system.about",s.SystemSettings="system.systemSettings",s.SearchHidden="search.hidden",s)},42144:function(e,t,a){a.r(t),a.d(t,{BarChart:()=>m.v,COLUMN_KEYS:()=>o.o,CUSTOM_REPORTS_WIDGET:()=>s.b,DynamicTypeCustomReportDefinitionAbstract:()=>n.D,DynamicTypeCustomReportDefinitionRegistry:()=>i.s,LineChart:()=>l.w,PieChart:()=>c.u,REPORTS_WIDGET:()=>s.E,ReportChart:()=>d.W,ReportsApiSlice:()=>p,useReportFormState:()=>r.H});var s=a(89294),i=a(41161),n=a(55501),r=a(17616),o=a(53797),d=a(68305),c=a(8733),l=a(13442),m=a(96150),p=a(28662);void 0!==(e=a.hmd(e)).hot&&e.hot.accept()},94593:function(e,t,a){a.d(t,{S:()=>n});var s=a(85893);a(81004);let i=(0,a(29202).createStyles)(e=>{let{token:t,css:a}=e;return{tabbarToolbar:a`
+      &.tabs-toolbar-layout {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
+        overflow: hidden;
+      }
+
+      .tabs-toolbar-layout__tabbar {
+        display: flex;
+        overflow: hidden;
+        height: calc(100% - ${t.sizeXXL}px);
+        width: 100%;
+      }
+
+      .tabs-toolbar-layout__toolbar {
+        display: flex;
+        overflow: hidden;
+        height: ${t.sizeXXL}px;
+        width: 100%;
+      }
+    `}},{hashPriority:"low"}),n=e=>{let{styles:t}=i();return(0,s.jsxs)("div",{className:["tabs-toolbar-layout",t.tabbarToolbar].join(" "),"data-testid":e.dataTestId,children:[(0,s.jsx)("div",{className:"tabs-toolbar-layout__tabbar",children:e.renderTabbar}),(0,s.jsx)("div",{className:"tabs-toolbar-layout__toolbar",children:e.renderToolbar})]})}}}]);
