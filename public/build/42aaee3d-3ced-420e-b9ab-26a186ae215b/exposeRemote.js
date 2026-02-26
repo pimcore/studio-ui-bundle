902f8d97ebbd2143d0f61fd691a4675c3ab5a56d
martineiber <11687066+martineiber@users.noreply.github.com>
2026-02-26 13:17:35 +0000

    Automatic frontend build

--- public/build/42aaee3d-3ced-420e-b9ab-26a186ae215b/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/42aaee3d-3ced-420e-b9ab-26a186ae215b/static/js/remoteEntry.js'
-    
--- public/build/93951ab9-2a91-428e-b643-37a8586b368b/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/93951ab9-2a91-428e-b643-37a8586b368b/static/js/remoteEntry.js'
-    
