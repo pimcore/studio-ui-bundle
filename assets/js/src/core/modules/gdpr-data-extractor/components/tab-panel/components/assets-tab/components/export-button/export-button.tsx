cc80aab83c2d6ca9ad18018549a24bb1f16b0772
Corepex <christian.feldkirchner@pimcore.com>
2026-03-02 10:34:08 +0100

    added sanitizer

--- assets/js/src/core/modules/gdpr-data-extractor/components/tab-panel/components/assets-tab/components/export-button/export-button.tsx
+++ assets/js/src/core/modules/gdpr-data-extractor/components/tab-panel/components/assets-tab/components/export-button/export-button.tsx
@@ -19,10 +19,11 @@ interface ExportButtonProps extends Omit<React.ComponentProps<typeof IconButton>
 
 export const ExportButton = ({ id, filename, onClick, ...iconButtonProps }: ExportButtonProps): React.JSX.Element => {
   const handleExport = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+    const sanitizedFilename = filename.replace(/\s/g, '_')
     const url = `${getPrefix()}/gdpr/export-data/${id}?providerKey=assets`
     const link = document.createElement('a')
     link.href = url
-    link.download = `${filename}.zip`
+    link.download = `${sanitizedFilename}.zip`
     document.body.appendChild(link)
     link.click()
     link.remove()
