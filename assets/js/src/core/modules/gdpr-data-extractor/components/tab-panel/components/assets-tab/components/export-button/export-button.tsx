adf27da765aa5f5d31911b01f0c392050c02f1f9
Corepex <christian.feldkirchner@pimcore.com>
2026-03-02 10:24:05 +0100

    fixed asset download button

---
+++ assets/js/src/core/modules/gdpr-data-extractor/components/tab-panel/components/assets-tab/components/export-button/export-button.tsx
@@ -0,0 +1,40 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+import { IconButton } from '@sdk/components'
+import React from 'react'
+import { getPrefix } from '@Pimcore/app/api/pimcore/route'
+
+interface ExportButtonProps extends Omit<React.ComponentProps<typeof IconButton>, 'id' | 'icon'> {
+  id: number
+  filename: string
+}
+
+export const ExportButton = ({ id, filename, onClick, ...iconButtonProps }: ExportButtonProps): React.JSX.Element => {
+  const handleExport = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+    const url = `${getPrefix()}/gdpr/export-data/${id}?providerKey=assets`
+    const link = document.createElement('a')
+    link.href = url
+    link.download = `${filename}.zip`
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
+
+    onClick?.(e)
+  }
+
+  return (
+    <IconButton
+      {...iconButtonProps}
+      icon={{ value: 'export' }}
+      onClick={handleExport}
+    />
+  )
+}
