46d3f1f619fb550d0698b23d58c73c7e667bb4dc
xIrusux <christine.altmann@gmx.com>
2026-03-02 17:49:01 +0100

    merge

--- public/build/63c9f049-248d-436c-87b4-1144d16271e7/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-<<<<<<<< HEAD:public/build/3f412c2c-c9ce-4d6f-9f23-fa41a531b595/exposeRemote.js
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/3f412c2c-c9ce-4d6f-9f23-fa41a531b595/static/js/remoteEntry.js'
-========
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/63c9f049-248d-436c-87b4-1144d16271e7/static/js/remoteEntry.js'
->>>>>>>> 50b99d17965601b73cb46e3e46f688d6a49f1563:public/build/63c9f049-248d-436c-87b4-1144d16271e7/exposeRemote.js
-    
