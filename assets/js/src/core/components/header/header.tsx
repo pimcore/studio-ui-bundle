c14135dd27f126fe189d15460c7af025bd28758f
Corepex <christian.feldkirchner@pimcore.com>
2026-03-04 14:52:48 +0100

    use rest operator

--- assets/js/src/core/components/header/header.tsx
+++ assets/js/src/core/components/header/header.tsx
@@ -15,34 +15,31 @@ import {
 } from '@Pimcore/components/header/header.styles'
 import cn from 'classnames'
 
-export interface HeaderProps {
+export interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
   icon?: React.JSX.Element
-  className?: string
   fullWidth?: boolean
-  style?: React.CSSProperties
   title: string
-  children?: React.ReactNode
 }
 
 export const Header = (props: HeaderProps): React.JSX.Element => {
   const { styles } = useStyles()
-  const { icon, title, children, style } = props
+  const { icon, title, children, fullWidth, ...rest } = props
   const classNames = cn(styles.header, props.className)
 
   return (
     <div
-      className={ classNames }
-      style={ style }
+      {...rest}
+      className={classNames}
     >
-      { title !== '' && (
-        <span className={ 'header__text' }>
-          <Title icon={ icon }>
+      {title !== '' && (
+        <span className={'header__text'}>
+          <Title icon={icon}>
             {title}
           </Title>
         </span>
       )}
 
-      <div className={ cn('header__content', { 'w-full': props.fullWidth === true }) }>
+      <div className={cn('header__content', { 'w-full': fullWidth === true })}>
         {children}
       </div>
     </div>
