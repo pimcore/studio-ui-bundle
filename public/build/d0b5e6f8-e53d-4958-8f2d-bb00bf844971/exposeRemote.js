49d934de64d2af7b8ae3ee5cfefe4dd57cc318d5
Marvin Bertram <marvin.bertram@pimcore.com>
2026-03-09 13:23:17 +0000

    build: rebuild assets after merge of origin/1.x

--- public/build/d0b5e6f8-e53d-4958-8f2d-bb00bf844971/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-<<<<<<<< HEAD:public/build/d0b5e6f8-e53d-4958-8f2d-bb00bf844971/exposeRemote.js
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/d0b5e6f8-e53d-4958-8f2d-bb00bf844971/static/js/remoteEntry.js'
-========
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/95093e3c-7bbd-4f2c-9516-a2c90c636505/static/js/remoteEntry.js'
->>>>>>>> origin/1.x:public/build/95093e3c-7bbd-4f2c-9516-a2c90c636505/exposeRemote.js
-    
