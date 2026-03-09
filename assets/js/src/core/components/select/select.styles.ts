03ad4fee7d5dfc14ce790bbd3fc83979e0f44727
markus-moser <markus.moser@pimcore.com>
2026-03-09 17:21:43 +0100

    Adjustments for data-importer

--- assets/js/src/core/components/select/select.styles.ts
+++ assets/js/src/core/components/select/select.styles.ts
@@ -11,10 +11,14 @@
 import { createStyles } from 'antd-style'
 import { isEmptyValue } from '@Pimcore/utils/type-utils'
 import { type SelectTheme } from './select'
+import { toCssDimension } from '@sdk/utils'
 
 interface StylesProps {
   width?: number
   theme?: SelectTheme
+  skeletonWidth?: number | string
+  skeletonMaxWidth?: number | string
+  skeletonMinWidth?: number | string
 }
 
 const ICON_WIDTH = 16
@@ -263,6 +267,21 @@ export const useStyles = createStyles(({ css, token }, props: StylesProps) => {
 
     customIconError: css`
        color: ${token.colorErrorHover};
-     `
+     `,
+
+    skeletonLoading: css`
+      width: ${toCssDimension(props.skeletonWidth, '100%')};
+      max-width: ${toCssDimension(props.skeletonMaxWidth, '100%')};
+      min-width: ${toCssDimension(props.skeletonMinWidth, 'auto')};
+      display: block;
+
+      .ant-skeleton,
+      .ant-skeleton-input {
+        width: ${toCssDimension(props.skeletonWidth, '100%')} !important;
+        max-width: ${toCssDimension(props.skeletonMaxWidth, '100%')} !important;
+        min-width: ${toCssDimension(props.skeletonMinWidth, 'auto')} !important;
+        display: block;
+      }
+    `
   }
 })
