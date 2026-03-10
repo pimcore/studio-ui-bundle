b3e546388d2df7ca5d92c57517c2f0b72b75a2e9
Christian F. <christian.feldkirchner@pimcore.com>
2026-03-10 15:08:20 +0100

    Implemented `QuantityValues` (#3059)
    * added quantity value screen
    
    * added translations
    
    * Apply latest automatic api client updates
    
    * implemented permissions
    
    * fixed perspective permission
    
    * fixed several small issues
    
    * Apply latest automatic api client updates
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    * added pagination
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    * removed `QuantityUnitModal`
    
    * added cancel button
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    * fixed sonar issue
    
    * Apply eslint-fixer changes
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: Corepex <16717695+Corepex@users.noreply.github.com>

--- assets/js/src/core/modules/perspectives/enums/nav-permission.ts
+++ assets/js/src/core/modules/perspectives/enums/nav-permission.ts
@@ -24,6 +24,7 @@ export enum NavPermission {
   ClassDefinitions = 'dataManagement.dataModel_classes',
   FieldCollections = 'dataManagement.dataModel_fieldCollections',
   ObjectBricks = 'dataManagement.dataModel_objectBricks',
+  QuantityValues = 'dataManagement.dataModel_quantityValue',
 
   AssetThumbnails = 'assetManagement.assetThumbnails',
 
