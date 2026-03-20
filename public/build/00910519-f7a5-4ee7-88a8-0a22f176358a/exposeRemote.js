621479c627fc355491aac0820254318b0e3dc4af
fashxp <8792145+fashxp@users.noreply.github.com>
2026-03-20 21:36:51 +0000

    Automatic frontend build

--- public/build/00910519-f7a5-4ee7-88a8-0a22f176358a/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/00910519-f7a5-4ee7-88a8-0a22f176358a/static/js/remoteEntry.js'
-    
---
+++ public/build/6647d3c5-6e24-4714-8565-5eaad1043caa/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/6647d3c5-6e24-4714-8565-5eaad1043caa/static/js/remoteEntry.js'
+    
