c4dea6cefc00c3696f8f2546b38f7c8fed846ed8
sholzer <sarah.holzer@valantic.at>
2026-03-24 16:11:45 +0100

    [Class Definitions] encrypted field conditions - field definitions #3202

--- assets/js/src/core/modules/field-definitions/dynamic-types/types/data/date/field-definition-date-form-fields.tsx
+++ assets/js/src/core/modules/field-definitions/dynamic-types/types/data/date/field-definition-date-form-fields.tsx
@@ -18,6 +18,7 @@ export const FieldDefinitionDateFormFields = (props: FieldDefinitionAbstractForm
   const form = Form.useFormInstance()
   const useCurrentDate = Form.useWatch('useCurrentDate')
   const isCustomLayout = props.context.area.includes('custom-layout')
+  const isEncryptedField = props.context.isEncryptedField === true
 
   useEffect(() => {
     if (useCurrentDate === true) {
@@ -57,17 +58,19 @@ export const FieldDefinitionDateFormFields = (props: FieldDefinitionAbstractForm
         </Form.Item>
       </FormKit.Panel>
 
-      <Form.Item
-        label={ t('column-type') }
-        name="columnType"
-      >
-        <Select
-          options={ [
-            { label: 'DATE', value: 'date' },
-            { label: 'BIGINT', value: 'bigint(20)' }
-          ] }
-        />
-      </Form.Item>
+        {!isEncryptedField && (
+            <Form.Item
+                label={ t('column-type') }
+                name="columnType"
+            >
+                <Select
+                    options={ [
+                        { label: 'DATE', value: 'date' },
+                        { label: 'BIGINT', value: 'bigint(20)' }
+                    ] }
+                />
+            </Form.Item>
+        )}
     </>
   )
 }
