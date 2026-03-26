7a01be696e0318c876a8745617437a028bd9d195
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-03-26 14:07:20 +0100

    [Custom Reports] Fix the Column Configuration logic (#3231)
    * fixed column configuration behavior
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

--- public/build/0d3a2026-f137-4008-b8a6-fa85e3d4bea7/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/0d3a2026-f137-4008-b8a6-fa85e3d4bea7/static/js/remoteEntry.js'
-    
---
+++ public/build/49964734-9302-462a-bfc0-bddd49bee0dd/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/49964734-9302-462a-bfc0-bddd49bee0dd/static/js/remoteEntry.js'
+    
