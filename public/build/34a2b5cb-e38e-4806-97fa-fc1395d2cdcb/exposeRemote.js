aac185f7e5a7ccb6861d3da824d355b8e54559fe
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-03-30 18:28:18 +0200

    added the FieldCollectionItemProvider and useFieldCollectionItem

---
+++ public/build/34a2b5cb-e38e-4806-97fa-fc1395d2cdcb/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/34a2b5cb-e38e-4806-97fa-fc1395d2cdcb/static/js/remoteEntry.js'
+    
---
+++ public/build/a425a9b6-0982-4854-8682-3a8b7815c3ff/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = 'http://localhost:3030/pimcore_studio_ui_bundle.83fc692a004b2f8d.hot-update.js'
+    
