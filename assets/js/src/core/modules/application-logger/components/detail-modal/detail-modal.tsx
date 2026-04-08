0c2302d58f555791bedab54d512ef32012289078
Corepex <christian.feldkirchner@pimcore.com>
2026-04-08 15:09:09 +0200

    added translation to `priority` field

--- assets/js/src/core/modules/application-logger/components/detail-modal/detail-modal.tsx
+++ assets/js/src/core/modules/application-logger/components/detail-modal/detail-modal.tsx
@@ -37,7 +37,7 @@ export const DetailModal = (props: DetailModalProps): React.JSX.Element => {
   const formData = {
     date: props.data?.date ?? '',
     message: props.data?.message ?? '',
-    priority: props.data?.priority ?? '',
+
     component: props.data?.component ?? '',
     source: props.data?.source ?? '',
     fileObject: props.data?.relatedElementData ?? null
@@ -84,9 +84,11 @@ export const DetailModal = (props: DetailModalProps): React.JSX.Element => {
 
           <Form.Item
             label={ t('application-logger.columns.type') }
-            name="priority"
           >
-            <Input readOnly />
+            <Input
+              readOnly
+              value={ t(`application-logger.filter.priority-level.${props.data?.priority ?? ''}`) }
+            />
           </Form.Item>
 
           <Form.Item
