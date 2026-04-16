60e894a43811bb08d3861fb9ec9360573dd2c68c
markus-moser <markus.moser@pimcore.com>
2026-04-16 13:33:01 +0200

    Fix empty area settings dialog for flat editable dialog items (#3343)

--- public/build/aa46475d-da56-4c18-b488-dba0fd75cfb7/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/aa46475d-da56-4c18-b488-dba0fd75cfb7/static/js/remoteEntry.js'
-    
---
+++ public/build/b4ba794e-975b-461d-bef5-c88911808aa3/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/b4ba794e-975b-461d-bef5-c88911808aa3/static/js/remoteEntry.js'
+    
