c4674674128bb8468e0846800f054de4d1611aa7
herbertroth <126679157+herbertroth@users.noreply.github.com>
2026-04-16 08:46:48 +0000

    Automatic frontend build

--- public/build/5487d5dd-e210-4f6d-b54b-e31045a19bfd/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/5487d5dd-e210-4f6d-b54b-e31045a19bfd/static/js/remoteEntry.js'
-    
---
+++ public/build/8d200149-c9dd-462b-b08a-9c168589c6aa/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/8d200149-c9dd-462b-b08a-9c168589c6aa/static/js/remoteEntry.js'
+    
