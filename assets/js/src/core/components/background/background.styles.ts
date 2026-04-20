eccf30d58d2b5b82e8737c2dc2b590ade575aa36
Marvin Bertram <marvin.bertram@pimcore.com>
2026-04-20 11:32:45 +0000

    feat: add orbit and hue-rotation animations to Background component

--- assets/js/src/core/components/background/background.styles.ts
+++ assets/js/src/core/components/background/background.styles.ts
@@ -19,6 +19,34 @@ export const useStyle = createStyles(({ token, css }) => {
       overflow: hidden; 
       opacity: 0.4;
 
+      @keyframes pimcore-bubble-orbit-tl {
+        0%   { transform: translate(0,     0)    rotate(65.637deg); }
+        25%  { transform: translate(40px, -30px) rotate(75deg); }
+        50%  { transform: translate(20px,  50px) rotate(65.637deg); }
+        75%  { transform: translate(-30px, 20px) rotate(55deg); }
+        100% { transform: translate(0,     0)    rotate(65.637deg); }
+      }
+
+      @keyframes pimcore-bubble-orbit-bl {
+        0%   { transform: translate(0,     0)    rotate(28.303deg); }
+        33%  { transform: translate(-40px, 30px) rotate(38deg); }
+        66%  { transform: translate(30px, -40px) rotate(18deg); }
+        100% { transform: translate(0,     0)    rotate(28.303deg); }
+      }
+
+      @keyframes pimcore-bubble-orbit-br {
+        0%   { transform: translate(0,     0)    rotate(65.637deg); }
+        25%  { transform: translate(-30px, 40px) rotate(55deg); }
+        50%  { transform: translate(40px,  20px) rotate(75deg); }
+        75%  { transform: translate(10px, -30px) rotate(65.637deg); }
+        100% { transform: translate(0,     0)    rotate(65.637deg); }
+      }
+
+      @keyframes pimcore-bubble-hue {
+        0%   { filter: blur(310px) hue-rotate(0deg); }
+        100% { filter: blur(310px) hue-rotate(360deg); }
+      }
+
       .background-figure {
         position: absolute;
 
@@ -27,22 +55,25 @@ export const useStyle = createStyles(({ token, css }) => {
           left: -30%;
           width: 1324px;
           height: 1324px;
-          transform: rotate(65.637deg);
           flex-shrink: 0;
           border-radius: var(--Components-Input-Component-paddingBlockSM, 1324px);
           background: rgba(55, 217, 243, 0.20);
           filter: blur(310px);
+          animation:
+            pimcore-bubble-orbit-tl 12s ease-in-out infinite,
+            pimcore-bubble-hue 16s linear infinite;
         }
-
 
         &--bottom-left {
           width: 651.152px;
           height: 1503.398px;
-          transform: rotate(28.303deg);
           flex-shrink: 0;
           border-radius: var(--Components-Input-Component-paddingBlockSM, 1503.398px);
           background: #FDFFFF;
           filter: blur(310px);
+          animation:
+            pimcore-bubble-orbit-bl 10s ease-in-out infinite,
+            pimcore-bubble-hue 20s linear infinite 2s;
         }
 
         &--bottom-right {
@@ -50,11 +81,13 @@ export const useStyle = createStyles(({ token, css }) => {
           left: 33%;
           width: 1642px;
           height: 686px;
-          transform: rotate(65.637deg);
           flex-shrink: 0;
           border-radius: var(--Components-Input-Component-paddingBlockSM, 1642px);
           background: var(--pimcore-brand-background-color, rgba(122, 58, 212, 0.42));
           filter: blur(310px);
+          animation:
+            pimcore-bubble-orbit-br 14s ease-in-out infinite 1s,
+            pimcore-bubble-hue 18s linear infinite 4s;
         }
       }
     `,
