42a9aedd31273e462bd9f7c35f1ac5df3608564d
Marvin Bertram <marvin.bertram@pimcore.com>
2026-04-20 11:33:55 +0000

    feat: fade out HTML preloader when React mounts

--- assets/js/src/core/modules/app/utils/app-runner.tsx
+++ assets/js/src/core/modules/app/utils/app-runner.tsx
@@ -26,6 +26,12 @@ export function runApp (): void {
     return undefined
   }
 
+  const preloader = document.getElementById('app-preloader')
+  if (preloader !== null) {
+    preloader.classList.add('app-preloader--fading')
+    preloader.addEventListener('transitionend', () => { preloader.remove() }, { once: true })
+  }
+
   const root = createRoot(domElement)
   root.render(<AppView />)
 }
