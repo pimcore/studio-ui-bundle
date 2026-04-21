b0f8e2f97e0e2e28450c47cfee1aa71f7b1b21b0
markus-moser <markus.moser@pimcore.com>
2026-04-21 15:32:16 +0000

    fix(clone): use StepCompletionCalculator to show progress bar on instant clone step

--- assets/js/src/core/modules/execution-engine/jobs/clone/abstract-clone-job.ts
+++ assets/js/src/core/modules/execution-engine/jobs/clone/abstract-clone-job.ts
@@ -14,6 +14,7 @@ import { setNodeFetching, refreshNodeChildren } from '@Pimcore/components/elemen
 import trackError, { GeneralError } from '@Pimcore/modules/app/error-handler'
 import { MessageBusJobHandler, type MessageBusJob } from '../../message-handlers/message-bus-job/message-bus-job-handler'
 import { ChildJobStepTracker } from '../../message-handlers/message-bus-job/step-tracker/child-job-step-tracker'
+import { StepCompletionCalculator } from '../../message-handlers/message-bus-job/progress-calculator/step-completion-calculator'
 import { type JobInterface, type JobRunOptions } from '../job-interface'
 import { type ElementType } from '@Pimcore/types/enums/element/element-type'
 import { JobStatus } from '../abstact-job'
@@ -69,6 +70,7 @@ export abstract class AbstractCloneJob implements JobInterface {
           return this.title
         },
         stepTracker: new ChildJobStepTracker(),
+        progressCalculator: new StepCompletionCalculator(),
         onJobCompletion: async (data: any) => {
           try {
             await this.handleCompletion()
