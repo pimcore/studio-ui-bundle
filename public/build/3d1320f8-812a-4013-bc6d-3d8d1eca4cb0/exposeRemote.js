9bf795234917470b859c471847d87e330c8622b2
Corepex <16717695+Corepex@users.noreply.github.com>
2026-04-22 07:35:42 +0000

    Automatic frontend build

--- public/build/3d1320f8-812a-4013-bc6d-3d8d1eca4cb0/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/3d1320f8-812a-4013-bc6d-3d8d1eca4cb0/static/js/remoteEntry.js'
-    
--- public/build/ea56b048-99fb-407c-b444-c817c3adf2a4/static/js/async/7396.da4c50f4.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 7396.da4c50f4.js.LICENSE.txt */
-"use strict";(self["chunk_pimcore_studio_ui_bundle "]=self["chunk_pimcore_studio_ui_bundle "]||[]).push([["7396"],{17503(e,i,l){l.r(i),l.d(i,{default:()=>t});var s=l(74848);l(47867);let t=e=>(0,s.jsx)("svg",{xmlns:"http://www.w3.org/2000/svg",width:"1em",height:"1em",viewBox:"0 0 640 480",...e,children:(0,s.jsxs)("g",{fillRule:"evenodd",strokeWidth:"1pt",children:[(0,s.jsx)("path",{fill:"#f31830",d:"M0 0h640v240H0z"}),(0,s.jsx)("path",{fill:"#fff",d:"M0 240h640v240H0z"})]})})}}]);
