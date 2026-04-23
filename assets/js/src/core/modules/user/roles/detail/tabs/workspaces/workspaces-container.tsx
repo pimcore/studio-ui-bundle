5defa964f186c44f600c4b0289a9d8b81d8e1662
xIrusux <christine.altmann@gmx.com>
2026-04-23 09:36:07 +0200

    fix lint error

--- assets/js/src/core/modules/user/roles/detail/tabs/workspaces/workspaces-container.tsx
+++ assets/js/src/core/modules/user/roles/detail/tabs/workspaces/workspaces-container.tsx
@@ -59,13 +59,13 @@ const WorkspacesContainer = ({ ...props }): React.JSX.Element => {
 
     switch (type) {
       case 'document':
-        setDocumentWorkspaces([...workspaces, { ...newWorkspace, save: false, unpublish: false }])
+        setDocumentWorkspaces([...workspaces, { ...newWorkspace, save: false, unpublish: false } as UserWorkspace])
         break
       case 'asset':
         setAssetWorkspaces([...workspaces, newWorkspace])
         break
       case 'object':
-        setObjectWorkspaces([...workspaces, { ...newWorkspace, save: false, unpublish: false }])
+        setObjectWorkspaces([...workspaces, { ...newWorkspace, save: false, unpublish: false } as UserWorkspace])
         break
     }
   }
