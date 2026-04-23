d2437660ead02ff173d1057733b48add0467b1c4
jcPimcore <259032526+jcPimcore@users.noreply.github.com>
2026-04-23 07:23:51 +0000

    Automatic frontend build

--- public/build/e19ef8f0-48b6-40c9-9ac8-3b398c23aef1/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-<<<<<<<< HEAD:public/build/8d762190-0709-420c-853d-54d3fa50d7c4/exposeRemote.js
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/8d762190-0709-420c-853d-54d3fa50d7c4/static/js/remoteEntry.js'
-========
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/e19ef8f0-48b6-40c9-9ac8-3b398c23aef1/static/js/remoteEntry.js'
->>>>>>>> 2026.1:public/build/e19ef8f0-48b6-40c9-9ac8-3b398c23aef1/exposeRemote.js
-    
---
+++ public/build/ed30dcda-b179-4a96-9bdc-c33dbfc0cbf9/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/ed30dcda-b179-4a96-9bdc-c33dbfc0cbf9/static/js/remoteEntry.js'
+    
