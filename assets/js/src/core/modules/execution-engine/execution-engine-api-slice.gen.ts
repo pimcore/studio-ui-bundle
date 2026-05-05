3a15b4c7aaecd4a19fee6f981f2da4c283268f59
markus-moser <4639428+markus-moser@users.noreply.github.com>
2026-05-05 13:04:11 +0000

    Apply latest automatic api client updates

--- assets/js/src/core/modules/execution-engine/execution-engine-api-slice.gen.ts
+++ assets/js/src/core/modules/execution-engine/execution-engine-api-slice.gen.ts
@@ -92,7 +92,7 @@ export type JobRun = {
     /** Current Message og the last Event */
     currentMessage: string;
     /** Id of the child Job run */
-    childJobRunId: number | null;
+    jobRunChildId: number | null;
     /** Current Step of a running Job */
     currentStep: number | null;
     /** Number of total Steps of a running Job */
@@ -101,8 +101,6 @@ export type JobRun = {
     creationDate: number | null;
     /** Modification date */
     modificationDate: number | null;
-    /** Job name */
-    jobName: string;
 };
 export const {
     useExecutionEngineAbortJobRunByIdMutation,
