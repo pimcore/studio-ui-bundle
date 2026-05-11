bdc7f632cc30d88fefef417eb6856546caf18501
Corepex <16717695+Corepex@users.noreply.github.com>
2026-05-11 09:35:53 +0000

    Automatic frontend build

--- public/build/378b276a-28f9-426b-aefd-715829aacbcb/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/378b276a-28f9-426b-aefd-715829aacbcb/static/js/remoteEntry.js'
-    
---
+++ public/build/472a585c-0bc3-47c3-9b0e-ef1127fd2862/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/472a585c-0bc3-47c3-9b0e-ef1127fd2862/static/js/remoteEntry.js'
+    
