00078c6fcc0eaf208980f3a9dbeae1c768fe8554
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-05-12 16:47:04 +0200

    added the drop target icon

---
+++ public/build/1aca8bd9-29cf-4413-bcea-bfe628bebf33/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = 'http://localhost:3030/pimcore_studio_ui_bundle.d925f0f4cb5a0b4b.hot-update.js'
+    
--- public/build/3e028919-2199-4d1b-a8ff-922b1dc7f980/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/3e028919-2199-4d1b-a8ff-922b1dc7f980/static/js/remoteEntry.js'
-    
