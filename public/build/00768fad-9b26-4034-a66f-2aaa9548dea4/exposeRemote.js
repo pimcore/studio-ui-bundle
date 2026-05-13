6d9cac2d44a4db999f74f6d574f06a3d017c8018
markus-moser <4639428+markus-moser@users.noreply.github.com>
2026-05-13 10:57:51 +0000

    Automatic frontend build

---
+++ public/build/00768fad-9b26-4034-a66f-2aaa9548dea4/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/00768fad-9b26-4034-a66f-2aaa9548dea4/static/js/remoteEntry.js'
+    
--- public/build/caed4f4a-a1f7-4ec9-96a7-e11f2b9f8a32/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/caed4f4a-a1f7-4ec9-96a7-e11f2b9f8a32/static/js/remoteEntry.js'
-    
