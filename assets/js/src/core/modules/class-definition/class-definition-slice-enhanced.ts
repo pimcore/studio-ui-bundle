cd9798feb34c4c7f6562c79827881fea700a3f04
markus-moser <markus.moser@pimcore.com>
2026-05-18 14:36:47 +0200

    [Data Object Editor] Use brick-fields endpoint for objectbrick class definition fieldname select (#3585)

--- assets/js/src/core/modules/class-definition/class-definition-slice-enhanced.ts
+++ assets/js/src/core/modules/class-definition/class-definition-slice-enhanced.ts
@@ -305,6 +305,7 @@ export const {
   useClassSelectOptionGetTreeQuery,
   useClassGetAvailableVisibleFieldsQuery,
   useClassGetSelectedVisibleFieldsQuery,
+  useClassDefinitionGetBrickFieldsQuery,
   useClassDefinitionGetBricksUsagesQuery,
   useClassFieldCollectionCollectionQuery,
   useClassFieldCollectionCreateMutation,
---
+++ public/build/86c8b9c3-3351-4dc8-8ebd-d729527ba317/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/86c8b9c3-3351-4dc8-8ebd-d729527ba317/static/js/remoteEntry.js'
+    
