69ab3af96a9048bc5f09cd3c6ad98acb43ce42be
markus-moser <markus.moser@pimcore.com>
2026-05-27 09:35:10 +0200

    Prepare 2025.4.4 Release (#3617)

---
+++ public/build/103a0e15-ee52-4916-9d8c-1414b5775285/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/103a0e15-ee52-4916-9d8c-1414b5775285/static/js/remoteEntry.js'
+    
--- public/build/5acaf935-7d29-434e-8739-66155b4248e0/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/5acaf935-7d29-434e-8739-66155b4248e0/static/js/remoteEntry.js'
-    
