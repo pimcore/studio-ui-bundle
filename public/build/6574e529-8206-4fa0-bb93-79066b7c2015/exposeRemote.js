a0893681b9dec60032d54d46511972968e56e568
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-05-27 16:36:22 +0200

    deleted the provideSplitView from the FieldDefinitionLocalizedfieldsFormFields

--- public/build/6574e529-8206-4fa0-bb93-79066b7c2015/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/6574e529-8206-4fa0-bb93-79066b7c2015/static/js/remoteEntry.js'
-    
---
+++ public/build/b54c9b32-9089-41c8-bc6b-c6d60a8987df/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = 'http://localhost:3030/pimcore_studio_ui_bundle.98dd2bdb80227516.hot-update.js'
+    
