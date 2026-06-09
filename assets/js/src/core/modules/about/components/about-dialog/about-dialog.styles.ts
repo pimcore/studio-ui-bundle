b1ddec1433a11d976c86240c7eda10a7a9124fc9
Marvin Bertram <marvin.bertram@pimcore.com>
2026-06-09 11:22:10 +0200

    Replace createStyles to reduce theme duplication in memory (#3681)
    * Replace createStyles to reduce theme duplication in memory
    
    * Cleanup code
    
    * Fix types
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <26813978+vin0401@users.noreply.github.com>

--- assets/js/src/core/modules/about/components/about-dialog/about-dialog.styles.ts
+++ assets/js/src/core/modules/about/components/about-dialog/about-dialog.styles.ts
@@ -8,7 +8,7 @@
  *  @license    Pimcore Open Core License (POCL)
  */
 
-import { createStyles } from 'antd-style'
+import { createStyles } from '@Pimcore/modules/ant-design/styles/create-styles'
 import { FastColor } from '@ant-design/fast-color'
 
 export const useStyle = createStyles(({ token, css }) => {
