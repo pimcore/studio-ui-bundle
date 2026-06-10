3ead6f888835047c0691346ffdafbdfcbc6932d9
ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>
2026-06-10 09:26:55 +0000

    Automatic frontend build

---
+++ public/build/36ccf1c5-1c85-485f-8a7d-028ee0c83351/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/36ccf1c5-1c85-485f-8a7d-028ee0c83351/static/js/remoteEntry.js'
+    
--- public/build/c3b5aa26-72b9-4e9d-a298-187d9407be38/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/c3b5aa26-72b9-4e9d-a298-187d9407be38/static/js/remoteEntry.js'
-    
