385a3a75fd35127488dcc2b5c1f6e70ba6da68ad
markus-moser <markus.moser@pimcore.com>
2026-06-11 16:45:05 +0200

    [Data Objects] Translate class names in class filter and add object menu (#3736)

---
+++ public/build/2b4e7f64-1303-4353-866c-b70804b40db4/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/2b4e7f64-1303-4353-866c-b70804b40db4/static/js/remoteEntry.js'
+    
--- public/build/580ad83c-f657-423e-999a-8e015083eaea/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/580ad83c-f657-423e-999a-8e015083eaea/static/js/remoteEntry.js'
-    
