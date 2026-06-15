1b69650a1287ff5ea504e5b99826db37f5a75d2c
xIrusux <42359615+xIrusux@users.noreply.github.com>
2026-06-15 22:07:05 +0000

    Automatic frontend build

---
+++ public/build/5298fe74-a29c-4bf2-83b3-6f250d9bed49/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/5298fe74-a29c-4bf2-83b3-6f250d9bed49/static/js/remoteEntry.js'
+    
--- public/build/52e91af8-7844-4248-8468-a48ba133ef6c/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/52e91af8-7844-4248-8468-a48ba133ef6c/static/js/remoteEntry.js'
-    
