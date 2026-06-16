9ca76ad0318f678586755985c7b610e723d7322e
astapc <210384374+astapc@users.noreply.github.com>
2026-06-16 11:18:33 +0000

    Automatic frontend build

--- public/build/26533333-3425-4ae4-91c4-148288512b69/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/26533333-3425-4ae4-91c4-148288512b69/static/js/remoteEntry.js'
-    
---
+++ public/build/ed96ea35-bdcc-418c-9281-3477ceea34d6/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/ed96ea35-bdcc-418c-9281-3477ceea34d6/static/js/remoteEntry.js'
+    
