01411c8b6a090a98c5b957830cfeab8cec1ff2c4
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-06-17 13:56:23 +0200

    added fix

---
+++ public/build/446bc1a1-0d2a-4b69-bd19-f5e0663a7906/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = 'http://localhost:3030/pimcore_studio_ui_bundle.242bd4a762420275.hot-update.js'
+    
--- public/build/47212ae1-0291-42d4-b152-b6d4ab3d9e4d/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/47212ae1-0291-42d4-b152-b6d4ab3d9e4d/static/js/remoteEntry.js'
-    
