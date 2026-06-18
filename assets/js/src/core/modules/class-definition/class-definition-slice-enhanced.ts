5d86482f4bab170399424298988c60d786d510b6
Marvin Bertram <marvin.bertram@pimcore.com>
2026-06-18 14:16:36 +0200

    Refresh field collecitons after import (#3775)
    * Refresh field collecitons after import
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: vin0401 <26813978+vin0401@users.noreply.github.com>

--- assets/js/src/core/modules/class-definition/class-definition-slice-enhanced.ts
+++ assets/js/src/core/modules/class-definition/class-definition-slice-enhanced.ts
@@ -141,7 +141,10 @@ const apiWithTags = baseApi.enhanceEndpoints({
       providesTags: (result, error, args) => providingTags.FIELD_COLLECTION_DETAIL(args.key)
     },
     classFieldCollectionUpdate: {
-      invalidatesTags: () => invalidatingTags.FIELD_COLLECTION_COLLECTION(),
+      invalidatesTags: (result, error, args) => [
+        ...invalidatingTags.FIELD_COLLECTION_COLLECTION(),
+        ...invalidatingTags.FIELD_COLLECTION_DETAIL(args.key)
+      ],
       async onQueryStarted (args, { dispatch, queryFulfilled }) {
         try {
           const { data } = await queryFulfilled
