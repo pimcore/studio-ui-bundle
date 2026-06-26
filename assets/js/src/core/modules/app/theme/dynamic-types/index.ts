9a3a97ccb6f96bb3a28ee3117741ae80489e44a0
Marvin Bertram <marvin.bertram@pimcore.com>
2026-06-26 12:36:40 +0000

    Remove dark theme

--- assets/js/src/core/modules/app/theme/dynamic-types/index.ts
+++ assets/js/src/core/modules/app/theme/dynamic-types/index.ts
@@ -13,13 +13,11 @@ import { moduleSystem } from '@Pimcore/app/module-system/module-system'
 import { serviceIds } from '@Pimcore/app/config/services/service-ids'
 import { type DynamicTypeThemeRegistry } from './registry/dynamic-type-theme-registry'
 import { type DynamicTypeThemeStudioDefaultLight } from './definitions/studio-default-light/dynamic-type-theme-studio-default-light'
-import { type DynamicTypeThemeStudioDefaultDark } from './definitions/studio-default-dark/dynamic-type-theme-studio-default-dark'
 
 moduleSystem.registerModule({
   onInit: () => {
     const themeRegistry = container.get<DynamicTypeThemeRegistry>(serviceIds['DynamicTypes/ThemeRegistry'])
 
     themeRegistry.registerDynamicType(container.get<DynamicTypeThemeStudioDefaultLight>(serviceIds['DynamicTypes/Theme/StudioDefaultLight']))
-    themeRegistry.registerDynamicType(container.get<DynamicTypeThemeStudioDefaultDark>(serviceIds['DynamicTypes/Theme/StudioDefaultDark']))
   }
 })
--- public/build/45e574be-0fab-483f-98c7-5480e74ea6f2/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/45e574be-0fab-483f-98c7-5480e74ea6f2/static/js/remoteEntry.js'
-    
