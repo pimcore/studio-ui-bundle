0546a354208e5bba846a18f801ac82645aedcc3d
idaiv <138690998+idaiv@users.noreply.github.com>
2026-06-29 14:40:37 +0000

    Automatic frontend build

--- public/build/9b87b1a6-cd16-4a74-a235-aea8e62945c1/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/9b87b1a6-cd16-4a74-a235-aea8e62945c1/static/js/remoteEntry.js'
-    
--- public/build/fb377a9d-6c7c-4c45-a643-bda20b96c13f/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/fb377a9d-6c7c-4c45-a643-bda20b96c13f/static/js/remoteEntry.js'
-    
