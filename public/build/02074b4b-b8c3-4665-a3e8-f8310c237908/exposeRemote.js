808ecf9ecc6dbb0d541a54f6b8458634d2ffd3c1
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-06-29 10:36:59 +0200

    Global search does not work (#3823)
    * fixed the useApplySavedSearch
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

--- public/build/02074b4b-b8c3-4665-a3e8-f8310c237908/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/02074b4b-b8c3-4665-a3e8-f8310c237908/static/js/remoteEntry.js'
-    
---
+++ public/build/6d7aa9fa-0018-489b-9ad1-ab0da21a0079/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/6d7aa9fa-0018-489b-9ad1-ab0da21a0079/static/js/remoteEntry.js'
+    
