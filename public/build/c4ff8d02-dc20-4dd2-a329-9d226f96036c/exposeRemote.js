8cef1b38302bed9f8ce55176aa3321941bf81e4c
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-07-07 10:12:53 +0000

    Automatic frontend build

---
+++ public/build/c4ff8d02-dc20-4dd2-a329-9d226f96036c/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/c4ff8d02-dc20-4dd2-a329-9d226f96036c/static/js/remoteEntry.js'
+    
--- public/build/da2ffc44-5be1-4fd6-a1e3-f57babd4bb35/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/da2ffc44-5be1-4fd6-a1e3-f57babd4bb35/static/js/remoteEntry.js'
-    
