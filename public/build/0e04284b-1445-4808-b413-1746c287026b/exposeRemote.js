6bd42c386287ef8ba6224062d22b52db3d4825e3
markus-moser <markus.moser@pimcore.com>
2026-07-09 18:03:41 +0200

    Remove committed frontend build artifacts from public/build/ (#3869)

--- public/build/0e04284b-1445-4808-b413-1746c287026b/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/0e04284b-1445-4808-b413-1746c287026b/static/js/remoteEntry.js'
-    
--- public/build/59b8522f-ab6f-4417-ae50-96761d485c68/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/59b8522f-ab6f-4417-ae50-96761d485c68/static/js/remoteEntry.js'
-    
